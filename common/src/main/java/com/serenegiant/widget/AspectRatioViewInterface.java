056008758dff1ba4b5561958345a3478d15c712f
saki4510t <saki4510t@gmail.com>
2016-11-27 10:46:30 +0900

    1.1.12

---
+++ common/src/main/java/com/serenegiant/widget/AspectRatioViewInterface.java
@@ -0,0 +1,10 @@
+package com.serenegiant.widget;
+
+/**
+ * アスペクト比を一定に保つView用のインターフェースを定義
+ */
+public interface AspectRatioViewInterface {
+	public void setAspectRatio(double aspectRatio);
+	public void setAspectRatio(final int width, final int height);
+	public double getAspectRatio();
+}
