9cd85af3248383beb058e157419ea0c0df68821a
saki4510t <saki4510t@gmail.com>
2017-02-01 13:37:05 +0900

    copyrightを2017に更新

--- common/src/main/java/com/serenegiant/media/AbstractAudioEncoder.java
+++ common/src/main/java/com/serenegiant/media/AbstractAudioEncoder.java
@@ -1,7 +1,7 @@
 package com.serenegiant.media;
 /*
  * Androusb
- * Copyright (c) 2014-2016 saki t_saki@serenegiant.com
+ * Copyright (c) 2014-2017 saki t_saki@serenegiant.com
  * Distributed under the terms of the GNU Lesser General Public License (LGPL v3.0) License.
  * License details are in the file license.txt, distributed as part of this software.
  */
