4c67fcc17016148a1ba7a25873bc694c322495b5
saki4510t <saki4510t@gmail.com>
2017-04-29 13:02:25 +0900

    ライセンス表記追加

---
+++ common/src/main/java/com/serenegiant/widget/ITextureViewCallback.java
@@ -0,0 +1,10 @@
+package com.serenegiant.widget;
+
+import android.view.TextureView;
+
+/**
+ * Created by saki on 2017/04/29.
+ */
+
+public interface ITextureViewCallback extends TextureView.SurfaceTextureListener {
+}
