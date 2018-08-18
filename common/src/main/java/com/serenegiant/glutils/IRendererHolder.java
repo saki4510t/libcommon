b15ea9972ed76bd44223f59231b36807ba35c785
saki4510t <saki4510t@gmail.com>
2018-08-19 04:42:52 +0900

    IRendererHolderに#getContextを足すのを忘れてた

--- common/src/main/java/com/serenegiant/glutils/IRendererHolder.java
+++ common/src/main/java/com/serenegiant/glutils/IRendererHolder.java
@@ -20,6 +20,7 @@ package com.serenegiant.glutils;
 
 import android.graphics.SurfaceTexture;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.Surface;
 
 /**
@@ -35,6 +36,9 @@ public interface IRendererHolder extends IRendererCommon {
 	 * 関係するすべてのリソースを開放する。再利用できない
 	 */
 	public void release();
+
+	@Nullable
+	public EGLBase.IContext getContext();
 
 	/**
 	 * マスター用の映像を受け取るためのSurfaceを取得
