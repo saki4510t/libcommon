66019523dfa2120bd80b25bb8aaa2fb60be9bc76
saki4510t <saki4510t@gmail.com>
2018-10-24 07:50:04 +0900

    静止画撮影フォーマット定数と制約用アノテーションを追加

--- common/src/main/java/com/serenegiant/glutils/IRendererHolder.java
+++ common/src/main/java/com/serenegiant/glutils/IRendererHolder.java
@@ -19,16 +19,27 @@ package com.serenegiant.glutils;
 */
 
 import android.graphics.SurfaceTexture;
+import android.support.annotation.IntDef;
 import android.support.annotation.IntRange;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.Surface;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
 /**
  * 分配描画インターフェース
  */
 public interface IRendererHolder extends IRendererCommon {
 	public static final int DEFAULT_CAPTURE_COMPRESSION = 80;
+
+	public static final int OUTPUT_FORMAT_JPEG = 0;
+	public static final int OUTPUT_FORMAT_PNG = 1;
+
+	@IntDef({OUTPUT_FORMAT_JPEG, OUTPUT_FORMAT_PNG})
+	@Retention(RetentionPolicy.SOURCE)
+	public @interface StillCaptureFormat {}
+
 	/**
 	 * 実行中かどうか
 	 * @return
