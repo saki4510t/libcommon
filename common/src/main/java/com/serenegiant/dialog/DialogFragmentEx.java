25afc56fee047ac1546c967a79f9aade96f74374
saki4510t <saki4510t@gmail.com>
2019-02-11 17:33:21 +0900

    DialogFragmentExにヘルパーメソッドを追加

--- common/src/main/java/com/serenegiant/dialog/DialogFragmentEx.java
+++ common/src/main/java/com/serenegiant/dialog/DialogFragmentEx.java
@@ -1,10 +1,17 @@
 package com.serenegiant.dialog;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.serenegiant.utils.BuildCheck;
+
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
 
 public abstract class DialogFragmentEx extends DialogFragment {
+	private static final String TAG = DialogFragmentEx.class.getSimpleName();
+
 	protected static final String ARGS_KEY_REQUEST_CODE = "requestCode";
 	protected static final String ARGS_KEY_ID_TITLE = "title";
 	protected static final String ARGS_KEY_ID_MESSAGE = "message";
@@ -26,5 +33,65 @@ public abstract class DialogFragmentEx extends DialogFragment {
 			throw new IllegalStateException();
 		}
 		return args;
+	}
+
+	@Override
+	public final void onStart() {
+		super.onStart();
+		if (BuildCheck.isAndroid7()) {
+			internalOnResume();
+		}
+	}
+
+	@Override
+	public final void onResume() {
+		super.onResume();
+		if (!BuildCheck.isAndroid7()) {
+			internalOnResume();
+		}
+	}
+
+	@Override
+	public final void onPause() {
+		if (!BuildCheck.isAndroid7()) {
+			internalOnPause();
+		}
+		super.onPause();
+	}
+
+	@Override
+	public final void onStop() {
+		if (BuildCheck.isAndroid7()) {
+			internalOnPause();
+		}
+		super.onStop();
+	}
+	
+	/**
+	 * Android6未満でのonResume, Android7以上でのonStartの処理
+	 * この中からonResumeやonStartを呼んでは行けない(無限ループになる)
+	 * onResumeとonStartはこのクラスでfinalにしてあるので子クラスで
+	 * 追加処理が必要な場合にはこのメソッドをoverrideすること
+	 */
+	protected void internalOnResume() {
+	}
+
+	/**
+	 * Android6未満でのonPause, Android7以上でのonStopの処理
+	 * この中からonPauseやonTopを呼んでは行けない(無限ループになる)
+	 * onPauseとonStopはこのクラスでfinalにしてあるので子クラスで
+	 * 追加処理が必要な場合にはこのメソッドをoverrideすること
+	 */
+	protected void internalOnPause() {
+	}
+
+	protected void popBackStack() {
+		final Activity activity = getActivity();
+		if ((activity == null) || activity.isFinishing()) return;
+		try {
+			getFragmentManager().popBackStack();
+		} catch (final Exception e) {
+			Log.w(TAG, e);
+		}
 	}
 }
