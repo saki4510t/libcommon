6bc88fdf08baa3a75ce77bba1b0f0524aabe02af
saki4510t <saki4510t@gmail.com>
2019-06-25 14:44:52 +0900

    CheckableImageViewを追加

---
+++ common/src/main/java/com/serenegiant/widget/CheckableImageView.java
@@ -0,0 +1,73 @@
+package com.serenegiant.widget;
+/*
+ * libcommon
+ * utility/helper classes for myself
+ *
+ * Copyright (c) 2014-2019 saki t_saki@serenegiant.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+*/
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.Checkable;
+
+import androidx.appcompat.widget.AppCompatImageView;
+
+public class CheckableImageView extends AppCompatImageView implements Checkable {
+	private static final boolean DEBUG = false; // 実同時はfalseにすること
+	private static final String TAG = CheckableImageView.class.getSimpleName();
+
+	private boolean mIsChecked;
+	private static final int[] CHECKED_STATE_SET = { android.R.attr.state_checked };
+
+	public CheckableImageView(Context context) {
+		this(context, null, 0);
+	}
+
+	public CheckableImageView(Context context, AttributeSet attrs) {
+		this(context, attrs, 0);
+	}
+
+	public CheckableImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+	}
+
+	@Override
+	public void setChecked(boolean checked) {
+		if (mIsChecked != checked) {
+			mIsChecked = checked;
+            refreshDrawableState();
+        }
+	}
+
+	@Override
+	public boolean isChecked() {
+		return mIsChecked;
+	}
+
+	@Override
+	public void toggle() {
+		setChecked(!mIsChecked);
+	}
+
+	@Override
+    public int[] onCreateDrawableState(final int extraSpace) {
+        final int[] drawableState = super.onCreateDrawableState(extraSpace + 1);
+        if (isChecked()) {
+            mergeDrawableStates(drawableState, CHECKED_STATE_SET);
+        }
+        return drawableState;
+    }
+
+}
