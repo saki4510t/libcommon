44a23b7ad97881c33c694b1f57664d69511bdb14
saki4510t <saki4510t@gmail.com>
2019-06-27 00:30:21 +0900

    NumberPickerPreferenceV7と関連クラスを追加

---
+++ common/src/main/java/com/serenegiant/preference/NumberPickerPreferenceFragment.java
@@ -0,0 +1,135 @@
+package com.serenegiant.preference;
+/*
+ * libcommon
+ * utility/helper classes for myself
+ *
+ * Copyright (c) 2014-2019 saki t_saki@serenegiant.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+*/
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.NumberPicker;
+
+import androidx.annotation.NonNull;
+import androidx.preference.PreferenceDialogFragment;
+
+public class NumberPickerPreferenceFragment extends PreferenceDialogFragment {
+	private static final boolean DEBUG = false;	// set false on production
+	private static final String TAG = NumberPickerPreferenceFragment.class.getSimpleName();
+
+	private static final String SAVE_STATE_MIN_VALUE = "NumberPickerPreferenceFragment.minValue";
+	private static final String SAVE_STATE_MAX_VALUE = "NumberPickerPreferenceFragment.maxValue";
+	private static final String SAVE_STATE_VALUE = "NumberPickerPreferenceFragment.value";
+
+	public static NumberPickerPreferenceFragment newInstance(final String key) {
+		final NumberPickerPreferenceFragment fragment = new NumberPickerPreferenceFragment();
+		final Bundle args = new Bundle(1);
+		args.putString(ARG_KEY, key);
+		fragment.setArguments(args);
+		return fragment;
+	}
+
+	private int mMinValue;
+	private int mMaxValue;
+	private int mValue;
+	private boolean changed;
+
+	public NumberPickerPreferenceFragment() {
+		super();
+		// デフォルトコンストラクタが必要
+		if (DEBUG) Log.v(TAG, "コンストラクタ:");
+	}
+
+	@Override
+	public void onCreate(final Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if (DEBUG) Log.v(TAG, "onCreate:");
+		if (savedInstanceState == null) {
+			final NumberPickerPreferenceV7 preference = getNumberPickerPreference();
+
+			mMinValue = preference.getMinValue();
+			mMaxValue = preference.getMaxValue();
+			mValue = preference.getValue();
+
+		} else {
+			mMinValue = savedInstanceState.getInt(SAVE_STATE_MIN_VALUE, 0);
+			mMaxValue = savedInstanceState.getInt(SAVE_STATE_MAX_VALUE, 100);
+			mValue = savedInstanceState.getInt(SAVE_STATE_VALUE, 0);
+		}
+	}
+
+	@Override
+	public void onSaveInstanceState(@NonNull Bundle outState) {
+		super.onSaveInstanceState(outState);
+		if (DEBUG) Log.v(TAG, "onSaveInstanceState:");
+		outState.putInt(SAVE_STATE_MIN_VALUE, mMinValue);
+		outState.putInt(SAVE_STATE_MAX_VALUE, mMaxValue);
+		outState.putInt(SAVE_STATE_VALUE, mValue);
+	}
+
+	@Override
+	public void onDialogClosed(final boolean positiveResult) {
+		if (DEBUG) Log.v(TAG, "onDialogClosed:");
+		final NumberPickerPreferenceV7 preference = getNumberPickerPreference();
+		if (positiveResult || changed) {
+			if (preference.callChangeListener(mValue)) {
+				preference.setValue(mValue);
+			}
+		 }
+	}
+
+	@Override
+	protected View onCreateDialogView(final Context context) {
+		if (DEBUG) Log.v(TAG, "onCreateDialogView:");
+		final NumberPicker picker = new NumberPicker(context);
+		picker.setOnValueChangedListener(mOnValueChangeListener);
+		picker.setMinValue(mMinValue);
+		picker.setMaxValue(mMaxValue);
+		picker.setValue(mValue);
+		changed = false;
+		return picker;
+	}
+
+	@Override
+	protected void onPrepareDialogBuilder(final AlertDialog.Builder builder) {
+		super.onPrepareDialogBuilder(builder);
+
+		/*
+		 * The typical interaction for list-based dialogs is to have
+		 * click-on-an-item dismiss the dialog instead of the user having to
+		 * press 'Ok'.
+		 */
+		builder.setPositiveButton(null, null);
+	}
+
+	protected NumberPickerPreferenceV7 getNumberPickerPreference() {
+		return (NumberPickerPreferenceV7)getPreference();
+	}
+
+	private final NumberPicker.OnValueChangeListener mOnValueChangeListener
+		= new NumberPicker.OnValueChangeListener() {
+		@Override
+		public void onValueChange(final NumberPicker picker, final int oldVal, final int newVal) {
+			if (DEBUG) Log.v(TAG, "onValueChange:newVal=" + newVal);
+			if (oldVal != newVal) {
+				changed = true;
+			}
+			mValue = newVal;
+		}
+	};
+}
---
+++ common/src/main/java/com/serenegiant/preference/NumberPickerPreferenceV7.java
@@ -0,0 +1,122 @@
+package com.serenegiant.preference;
+/*
+ * libcommon
+ * utility/helper classes for myself
+ *
+ * Copyright (c) 2014-2019 saki t_saki@serenegiant.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+*/
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.util.Log;
+
+import com.serenegiant.common.R;
+
+import java.util.Locale;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.res.TypedArrayUtils;
+import androidx.preference.DialogPreference;
+
+public class NumberPickerPreferenceV7 extends DialogPreference {
+	private static final boolean DEBUG = false;	// set false on production
+	private static final String TAG = NumberPickerPreferenceV7.class.getSimpleName();
+
+	private int mDefaultValue;
+	private final int mMinValue;
+	private final int mMaxValue;
+	private int mValue;
+
+	public NumberPickerPreferenceV7(@NonNull final Context context) {
+		this(context, null);
+	}
+
+	public NumberPickerPreferenceV7(@NonNull final Context context,
+		@Nullable final AttributeSet attrs) {
+
+		this(context, attrs, TypedArrayUtils.getAttr(context, R.attr.dialogPreferenceStyle,
+			android.R.attr.dialogPreferenceStyle));
+	}
+
+	public NumberPickerPreferenceV7(@NonNull final Context context,
+		@Nullable final AttributeSet attrs, final int defStyle) {
+
+		super(context, attrs, defStyle);
+		if (DEBUG) Log.v(TAG, "コンストラクタ:");
+
+		final TypedArray a = context.obtainStyledAttributes(
+			attrs, R.styleable.NumberPicker, defStyle, 0);
+		mDefaultValue = a.getInt(R.styleable.NumberPicker_DefaultValue, 0);
+		final int min = a.getInt(R.styleable.NumberPicker_MinValue, 0);
+		final int max = a.getInt(R.styleable.NumberPicker_MaxValue, 100);
+        a.recycle();
+
+		mMinValue = Math.min(min, max);
+		mMaxValue = Math.max(min, max);
+        setPositiveButtonText(android.R.string.ok);
+		setNegativeButtonText(android.R.string.cancel);
+	}
+
+	@Override
+	protected Object onGetDefaultValue(final TypedArray a, final int index) {
+		return (a.getString(index));
+	}
+
+	@Override
+	protected void onSetInitialValue(@Nullable final Object defaultValue) {
+
+		if (defaultValue != null) {
+			mDefaultValue = Integer.parseInt((String) defaultValue);
+		}
+		mValue = getPersistedInt(mDefaultValue);
+
+		setSummary(getSummary());
+
+		if (DEBUG) Log.v(TAG, "onSetInitialValue:" + mValue);
+	}
+
+	@Override
+	public CharSequence getSummary() {
+		return String.format(Locale.US, "%d", mValue);
+	}
+
+//--------------------------------------------------------------------------------
+
+	public int getMinValue() {
+		return mMinValue;
+	}
+
+	public int getMaxValue() {
+		return mMaxValue;
+	}
+
+	public int getValue() {
+		return mValue;
+	}
+
+	public void setValue(final int value) {
+		final boolean changed = getValue() != value;
+		if (changed) {
+			mValue = value;
+			persistInt(value);
+			if (changed) {
+				notifyChanged();
+			}
+		}
+	}
+
+}
