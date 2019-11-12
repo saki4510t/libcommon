804e54d1904cc5ae96e4f6d47e432c6ad8f729b6
saki4510t <saki4510t@gmail.com>
2019-11-12 09:59:22 +0900

    GLコンテキスト保持用のヘルパークラスを追加

---
+++ common/src/main/java/com/serenegiant/glutils/GLContext.java
@@ -0,0 +1,165 @@
+package com.serenegiant.glutils;
+
+import androidx.annotation.Nullable;
+
+/**
+ * 現在のスレッド上にGLコンテキストを生成する
+ */
+public class GLContext implements EGLConst {
+	private static final boolean DEBUG = true;	// set false on production
+	private static final String TAG = GLContext.class.getSimpleName();
+
+	private final Object mSync = new Object();
+	private final int mFlags;
+	@Nullable
+	private EGLBase mEgl = null;
+	@Nullable
+	private EGLBase.IEglSurface mEglMasterSurface;
+
+	/**
+	 * コンストラクタ
+	 * @param maxClientVersion 通常は2か3
+	 * @param sharedContext 共有コンテキストの親となるIContext, nullなら自分がマスターのコンテキストとなる
+	 * @param flags
+	 */
+	public GLContext(final int maxClientVersion,
+		@Nullable final EGLBase.IContext sharedContext, final int flags) {
+
+		mFlags = flags;
+		if ((sharedContext == null)
+			|| (sharedContext instanceof EGLBase.IContext)) {
+
+			final int stencilBits
+				= (flags & EGL_FLAG_STENCIL_1BIT) == EGL_FLAG_STENCIL_1BIT ? 1
+					: ((flags & EGL_FLAG_STENCIL_8BIT) == EGL_FLAG_STENCIL_8BIT ? 8 : 0);
+			mEgl = EGLBase.createFrom(maxClientVersion, sharedContext,
+				(flags & EGL_FLAG_DEPTH_BUFFER) == EGL_FLAG_DEPTH_BUFFER,
+				stencilBits,
+				(flags & EGL_FLAG_RECORDABLE) == EGL_FLAG_RECORDABLE);
+		}
+		if (mEgl != null) {
+			mEglMasterSurface = mEgl.createOffscreen(1, 1);
+			mEglMasterSurface.makeCurrent();
+		} else {
+			throw new RuntimeException("failed to create EglCore");
+		}
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		try {
+			release();
+		} finally {
+			super.finalize();
+		}
+	}
+
+	/**
+	 * 関連するリソースを破棄する
+	 * コンストラクタを呼び出したスレッド上で実行すること
+	 */
+	public void release() {
+		synchronized (mSync) {
+			if (mEglMasterSurface != null) {
+				mEglMasterSurface.release();
+				mEglMasterSurface = null;
+			}
+			if (mEgl != null) {
+				mEgl.release();
+				mEgl = null;
+			}
+		}
+	}
+
+	/**
+	 * EGLBaseを取得
+	 * @return
+	 * @throws IllegalStateException
+	 */
+	public EGLBase getEgl() throws IllegalStateException {
+		synchronized (mSync) {
+			if (mEgl != null) {
+				return mEgl;
+			} else {
+				throw new IllegalStateException();
+			}
+		}
+	}
+
+	/**
+	 * EGLBase生成時のIConfigを取得
+	 * @return
+	 * @throws IllegalStateException
+	 */
+	public EGLBase.IConfig getConfig() throws IllegalStateException {
+		synchronized (mSync) {
+			if (mEgl != null) {
+				return mEgl.getConfig();
+			} else {
+				throw new IllegalStateException();
+			}
+		}
+	}
+
+	/**
+	 * EGLBase生成時のflagsを取得
+	 * @return
+	 */
+	public int getFlags() {
+		return mFlags;
+	}
+
+	/**
+	 * IContextを取得
+	 * @return
+	 * @throws IllegalStateException
+	 */
+	public EGLBase.IContext getContext() throws IllegalStateException {
+		if (mEgl != null) {
+			mEgl.getContext();
+		} else {
+			throw new IllegalStateException();
+		}
+		return mEgl != null ? mEgl.getContext() : null;
+	}
+
+	/**
+	 * マスターコンテキストを選択
+	 * @throws IllegalStateException
+	 */
+	public void makeDefault() throws IllegalStateException {
+		synchronized (mSync) {
+			if (mEgl != null) {
+				mEglMasterSurface.makeCurrent();
+			} else {
+				throw new IllegalStateException();
+			}
+		}
+	}
+
+	/**
+	 * GLES2以上で初期化されているかどうか
+	 * @return
+	 */
+	public boolean isGLES2() {
+		return getGlVersion() > 1;
+	}
+
+	/**
+	 * GLES3以上で初期化されているかどうか
+	 * @return
+	 */
+	public boolean isGLES3() {
+		return getGlVersion() > 2;
+	}
+
+	/**
+	 * GLコンテキストのバージョンを取得
+	 * @return
+	 */
+	public int getGlVersion() {
+		synchronized (mSync) {
+			return  (mEgl != null) ? mEgl.getGlVersion() : 0;
+		}
+	}
+}
