72324fc563d1bbbc21ac408aa70eb688a488191c
saki4510t <saki4510t@gmail.com>
2019-11-16 14:10:56 +0900

    GLコンテキストとそれを保持するワーカースレッドを扱うためのヘルパークラスGLManagerを追加

---
+++ common/src/main/java/com/serenegiant/glutils/GLManager.java
@@ -0,0 +1,236 @@
+package com.serenegiant.glutils;
+/*
+ * libcommon
+ * utility/helper classes for myself
+ *
+ * Copyright (c) 2014-2019 saki t_saki@serenegiant.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+*/
+
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+import android.view.Choreographer;
+
+import com.serenegiant.utils.HandlerThreadHandler;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.WorkerThread;
+
+/**
+ * GLコンテキストとそれを保持するワーカースレッドを扱うためのヘルパークラス
+ */
+public class GLManager {
+	private static final boolean DEBUG = true;	// set false on production
+	private static final String TAG = GLManager.class.getSimpleName();
+
+	@NonNull
+	private final GLContext mGLContext;
+	@NonNull
+	private final Handler mGLHandler;
+	@Nullable
+	private final Handler.Callback mCallback;
+	private boolean mInitialized;
+	private boolean mReleased;
+
+	/**
+	 * コンストラクタ
+	 * @param maxClientVersion
+	 * @param sharedContext
+	 * @param flags
+	 * @param callback
+	 */
+	public GLManager(final int maxClientVersion,
+		@Nullable final EGLBase.IContext sharedContext, final int flags,
+		@Nullable final Handler.Callback callback) throws RuntimeException {
+
+		mCallback = callback;
+		mGLContext = new GLContext(maxClientVersion, sharedContext, flags);
+		mGLHandler = HandlerThreadHandler.createHandler(TAG,
+			new Handler.Callback() {
+				@Override
+				public boolean handleMessage(@NonNull final Message msg) {
+					return GLManager.this.handleMessage(msg);
+				}
+			}
+		);
+		final Semaphore sync = new Semaphore(0);
+		mGLHandler.postAtFrontOfQueue(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					mGLContext.initialize();
+					mInitialized = true;
+				} catch (final Exception e) {
+					Log.w(TAG, e);
+				}
+				sync.release();
+			}
+		});
+		try {
+			sync.tryAcquire(3000, TimeUnit.MILLISECONDS);
+		} catch (final InterruptedException e) {
+			// do nothing
+		}
+		if (!mInitialized) {
+			throw new RuntimeException("Failed to initialize GL context");
+		}
+	}
+
+	@Override
+	protected void finalize() throws Throwable {
+		try {
+			release();
+		} finally {
+			super.finalize();
+		}
+	}
+
+	/**
+	 * 関連するリソースを廃棄する
+	 * 再利用はできない
+	 */
+	public synchronized void release() {
+		if (!mReleased) {
+			mReleased = true;
+			mGLHandler.postAtFrontOfQueue(new Runnable() {
+				@Override
+				public void run() {
+					mGLContext.release();
+					mGLHandler.removeCallbacksAndMessages(null);
+					mGLHandler.getLooper().quit();
+				}
+			});
+		}
+	}
+
+	/**
+	 * GLコンテキストが有効かどうか
+	 * @return
+	 */
+	public synchronized boolean isValid() {
+		return mInitialized && !mReleased;
+	}
+
+	/**
+	 * この共有GLコンテキストがコンテキストを持つGLTaskを生成して返す
+	 * @param callback 
+	 * @return
+	 * @throws RuntimeException
+	 */
+	public synchronized GLManager createShared(
+		@Nullable final Handler.Callback callback)	throws RuntimeException {
+
+		checkValid();
+		return new GLManager(mGLContext.getMaxClientVersion(),
+			mGLContext.getContext(), mGLContext.getFlags(),
+			callback);
+	}
+
+	/**
+	 * GLコンテキストを保持しているスレッド上での実行用のHandlerを返す
+	 * @return
+	 * @throws IllegalStateException
+	 */
+	@NonNull
+	public synchronized Handler getGLHandler() throws IllegalStateException {
+		checkValid();
+		return mGLHandler;
+	}
+
+	/**
+	 * GLContextを取得する
+	 * @return
+	 * @throws IllegalStateException
+	 */
+	public synchronized GLContext getGLContext() throws IllegalStateException {
+		checkValid();
+		return mGLContext;
+	}
+
+	/**
+	 * GLコンテキストを保持しているスレッド上での実行要求
+	 * @param task
+	 */
+	public synchronized void runOnGLThread(final Runnable task)
+		throws IllegalStateException {
+
+		checkValid();
+		mGLHandler.post(task);
+	}
+
+	/**
+	 * GLコンテキストを保持しているスレッド上での実行要求
+	 * @param task
+	 * @param delayMs
+	 * @throws IllegalStateException
+	 */
+	public synchronized void runOnGLThread(final Runnable task, final long delayMs)
+		throws IllegalStateException{
+
+		checkValid();
+		if (delayMs > 0) {
+			mGLHandler.postDelayed(task, delayMs);
+		} else {
+			mGLHandler.post(task);
+		}
+	}
+
+	/**
+	 * GLコンテキスト上で実行されるChoreographer.FrameCallbackをpostする
+	 * @param callback
+	 * @param delayMs
+	 * @throws IllegalStateException
+	 */
+	public synchronized void postFrameCallbackDelayed(
+		@NonNull final Choreographer.FrameCallback callback,
+		final long delayMs) throws IllegalStateException {
+
+		checkValid();
+		mGLHandler.post(new Runnable() {
+			@Override
+			public void run() {
+				Choreographer.getInstance().postFrameCallbackDelayed(callback, delayMs);
+			}
+		});
+	}
+
+//--------------------------------------------------------------------------------
+	/**
+	 * GLコンテキストが破棄されていないかどうかを確認
+	 * @throws IllegalStateException
+	 */
+	private void checkValid() throws IllegalStateException {
+		if (!isValid()) {
+			throw new IllegalStateException("already released");
+		}
+	}
+
+	/**
+	 * GLスレッド上での処理の実体
+	 * @param msg
+	 * @return
+	 */
+	@WorkerThread
+	protected boolean handleMessage(@NonNull final Message msg) {
+		if (mCallback != null) {
+			return mCallback.handleMessage(msg);
+		}
+		return false;
+	}
+}
