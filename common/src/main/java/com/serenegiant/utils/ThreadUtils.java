87e485a8b6a1b3dc7dedcc2dd34558aaca8d8b5d
saki4510t <saki4510t@gmail.com>
2019-11-24 21:06:50 +0900

    ThreadUtilsを追加

---
+++ common/src/main/java/com/serenegiant/utils/ThreadUtils.java
@@ -0,0 +1,38 @@
+package com.serenegiant.utils;
+
+public class ThreadUtils {
+
+	/**
+	 * InterruptedExceptionを投げずにSleepする
+	 * @param  millis
+	 *         the length of time to sleep in milliseconds
+	 * @throws  IllegalArgumentException
+	 *          if the value of {@code millis} is negative, or the value of
+	 *          {@code nanos} is not in the range {@code 0-999999}
+	 */
+	public static void NoThrowSleep(final long millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (final InterruptedException e) {
+			// ignore
+		}
+	}
+
+	/**
+	 * InterruptedExceptionを投げずにSleepする
+	 * @param  millis
+	 *         the length of time to sleep in milliseconds
+	 * @param  nanos
+	 *         {@code 0-999999} additional nanoseconds to sleep
+	 * @throws  IllegalArgumentException
+	 *          if the value of {@code millis} is negative, or the value of
+	 *          {@code nanos} is not in the range {@code 0-999999}
+	 */
+	public static void NoThrowSleep(final long millis, final int nanos) {
+		try {
+			Thread.sleep(millis, nanos);
+		} catch (final InterruptedException e) {
+			// ignore
+		}
+	}
+}
