0f494c0e5dd1282b300b5b89567597cb1b67efbd
saki4510t <saki4510t@gmail.com>
2019-11-26 18:09:22 +0900

    c形式の配列をJava形式に修正

--- common/src/main/java/com/serenegiant/utils/BufferHelper.java
+++ common/src/main/java/com/serenegiant/utils/BufferHelper.java
@@ -33,7 +33,7 @@ public class BufferHelper {
 		// インスタンス化をエラーにするためにデフォルトコンストラクタをprivateに
 	}
 
-	private static final char HEX[] = {
+	private static final char[] HEX = {
 		'0', '1', '2', '3',
 		'4', '5', '6', '7',
 		'8', '9', 'a', 'b',
