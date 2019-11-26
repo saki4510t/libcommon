6def945d071193cac56a4b008e2d8e29141eb865
saki4510t <saki4510t@gmail.com>
2019-11-26 17:44:57 +0900

    テクスチャ・テクスチャ変換行列を更新した後のスレッドのスリープを0に戻した

--- common/src/main/java/com/serenegiant/glutils/AbstractDistributeTask.java
+++ common/src/main/java/com/serenegiant/glutils/AbstractDistributeTask.java
@@ -386,7 +386,7 @@ public abstract class AbstractDistributeTask {
 				if (mHasNewFrame) {
 					mHasNewFrame = false;
 					handleUpdateTexture();
-					ThreadUtils.NoThrowSleep(0, 100);
+					ThreadUtils.NoThrowSleep(0, 0);
 				}
 			} catch (final Exception e) {
 				Log.e(TAG, "handleDraw:thread id =" + Thread.currentThread().getId(), e);
