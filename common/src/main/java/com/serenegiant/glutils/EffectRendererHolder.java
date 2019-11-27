e146e9af8c4ce350e386306e6b37e45440de5ec8
saki4510t <saki4510t@gmail.com>
2019-11-27 17:41:58 +0900

    EffectRendererHolderにもES3対応を追加

--- common/src/main/java/com/serenegiant/glutils/EffectRendererHolder.java
+++ common/src/main/java/com/serenegiant/glutils/EffectRendererHolder.java
@@ -19,11 +19,12 @@ package com.serenegiant.glutils;
 */
 
 import android.annotation.SuppressLint;
-import android.opengl.GLES20;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.WorkerThread;
 
+import android.opengl.GLES20;
+import android.opengl.GLES30;
 import android.util.Log;
 import android.util.SparseArray;
 
@@ -109,6 +110,29 @@ public class EffectRendererHolder extends AbstractRendererHolder {
 		= String.format(FRAGMENT_SHADER_GRAY_BASE_ES2,
 			HEADER_OES_ES2, SAMPLER_OES);
 
+	/**
+	 * グレースケール変換のためのフラグメントシェーダーのベース文字列
+	 * header(HEADER_OESかHEADER_2D)とサンプラーの種類文字列(SAMPLER_OESかSAMPLER_2D)を渡すこと
+	 */
+	private static final String FRAGMENT_SHADER_GRAY_BASE_ES3
+		= SHADER_VERSION_ES3 +
+		"%s" +
+		"precision mediump float;\n" +
+		"in vec2 vTextureCoord;\n" +
+		"uniform %s sTexture;\n" +
+		"const vec3 conv = vec3(0.3, 0.59, 0.11);\n" +
+		"layout(location = 0) out vec4 o_FragColor;\n" +
+		"void main() {\n" +
+		"    vec4 tc = texture(sTexture, vTextureCoord);\n" +
+		"    float color = dot(tc.rgb, conv);\n" +
+		"    vec3 cl3 = vec3(color, color, color);\n" +
+		"    o_FragColor = vec4(cl3, 1.0);\n" +
+	"}\n";
+
+	private static final String FRAGMENT_SHADER_EXT_GRAY_ES3
+		= String.format(FRAGMENT_SHADER_GRAY_BASE_ES3,
+			HEADER_OES_ES3, SAMPLER_OES);
+
 //--------------------------------------------------------------------------------
 	/**
 	 * 白黒反転したグレースケール変換のためのフラグメントシェーダーのベース文字列
@@ -131,6 +155,29 @@ public class EffectRendererHolder extends AbstractRendererHolder {
 	private static final String FRAGMENT_SHADER_GRAY_EXT_REVERSE_ES2
 		= String.format(FRAGMENT_SHADER_GRAY_REVERSE_BASE_ES2,
 			HEADER_OES_ES2, SAMPLER_OES);
+
+	/**
+	 * 白黒反転したグレースケール変換のためのフラグメントシェーダーのベース文字列
+	 * header(HEADER_OESかHEADER_2D)とサンプラーの種類文字列(SAMPLER_OESかSAMPLER_2D)を渡すこと
+	 */
+	private static final String FRAGMENT_SHADER_GRAY_REVERSE_BASE_ES3
+		= SHADER_VERSION_ES3 +
+		"%s" +
+		"precision mediump float;\n" +
+		"in vec2 vTextureCoord;\n" +
+		"uniform %s sTexture;\n" +
+		"const vec3 conv = vec3(0.3, 0.59, 0.11);\n" +
+		"layout(location = 0) out vec4 o_FragColor;\n" +
+		"void main() {\n" +
+		"    vec4 tc = texture(sTexture, vTextureCoord);\n" +
+		"    float color = dot(tc.rgb, conv);\n" +
+		"    vec3 cl3 = vec3(color, color, color);\n" +
+		"    o_FragColor = vec4(clamp(vec3(1.0, 1.0, 1.0) - cl3, 0.0, 1.0), 1.0);\n" +
+	"}\n";
+
+	private static final String FRAGMENT_SHADER_GRAY_EXT_REVERSE_ES3
+		= String.format(FRAGMENT_SHADER_GRAY_REVERSE_BASE_ES3,
+			HEADER_OES_ES3, SAMPLER_OES);
 
 //--------------------------------------------------------------------------------
 	/**
@@ -165,6 +212,39 @@ public class EffectRendererHolder extends AbstractRendererHolder {
 		= String.format(FRAGMENT_SHADER_BIN_BASE_ES2,
 			HEADER_OES_ES2, SAMPLER_OES, "0.0, 1.0, 0.0");
 
+	/**
+	 * 2値化のためのフラグメントシェーダーのベース文字列
+	 * header(HEADER_OESかHEADER_2D)とサンプラーの種類文字列(SAMPLER_OESかSAMPLER_2D)、
+	 * 変換後の明るい部分用の色を指定するための文字列(R, G, Bの順)を渡すこと
+	 */
+	private static final String FRAGMENT_SHADER_BIN_BASE_ES3
+		= SHADER_VERSION_ES3 +
+		"%s" +
+		"precision mediump float;\n" +
+		"in vec2 vTextureCoord;\n" +
+		"uniform %s sTexture;\n" +
+		"const vec3 conv = vec3(0.3, 0.59, 0.11);\n" +
+		"const vec3 cl = vec3(%s);\n" +
+		"layout(location = 0) out vec4 o_FragColor;\n" +
+		"void main() {\n" +
+		"    vec4 tc = texture(sTexture, vTextureCoord);\n" +
+		"    float color = dot(tc.rgb, conv);\n" +
+		"    vec3 bin = step(0.3, vec3(color, color, color));\n" +
+		"    o_FragColor = vec4(cl * bin, 1.0);\n" +
+	"}\n";
+
+	private static final String FRAGMENT_SHADER_EXT_BIN_ES3
+		= String.format(FRAGMENT_SHADER_BIN_BASE_ES3,
+			HEADER_OES_ES3, SAMPLER_OES, "1.0, 1.0, 1.0");
+
+	private static final String FRAGMENT_SHADER_EXT_BIN_YELLOW_ES3
+		= String.format(FRAGMENT_SHADER_BIN_BASE_ES3,
+			HEADER_OES_ES3, SAMPLER_OES, "1.0, 1.0, 0.0");
+
+	private static final String FRAGMENT_SHADER_EXT_BIN_GREEN_ES3
+		= String.format(FRAGMENT_SHADER_BIN_BASE_ES3,
+			HEADER_OES_ES3, SAMPLER_OES, "0.0, 1.0, 0.0");
+
 //--------------------------------------------------------------------------------
 	/**
 	 * 反転した2値化のためのフラグメントシェーダーのベース文字列
@@ -198,6 +278,39 @@ public class EffectRendererHolder extends AbstractRendererHolder {
 		= String.format(FRAGMENT_SHADER_BIN_REVERSE_BASE_ES2,
 			HEADER_OES_ES2, SAMPLER_OES, "0.0, 1.0, 0.0");
 
+	/**
+	 * 反転した2値化のためのフラグメントシェーダーのベース文字列
+	 * header(HEADER_OESかHEADER_2D)とサンプラーの種類文字列(SAMPLER_OESかSAMPLER_2D)、
+	 * 変換後の明るい部分用の色を指定するための文字列(R, G, Bの順)を渡すこと
+	 */
+	private static final String FRAGMENT_SHADER_BIN_REVERSE_BASE_ES3
+		= SHADER_VERSION_ES3 +
+		"%s" +
+		"precision mediump float;\n" +
+		"in vec2 vTextureCoord;\n" +
+		"uniform %s sTexture;\n" +
+		"const vec3 conv = vec3(0.3, 0.59, 0.11);\n" +
+		"const vec3 cl = vec3(%s);\n" +
+		"layout(location = 0) out vec4 o_FragColor;\n" +
+		"void main() {\n" +
+		"    vec4 tc = texture(sTexture, vTextureCoord);\n" +
+		"    float color = dot(tc.rgb, conv);\n" +
+		"    vec3 bin = step(0.3, vec3(color, color, color));\n" +
+		"    o_FragColor = vec4(cl * (vec3(1.0, 1.0, 1.0) - bin), 1.0);\n" +
+	"}\n";
+
+	private static final String FRAGMENT_SHADER_EXT_BIN_REVERSE_ES3
+		= String.format(FRAGMENT_SHADER_BIN_REVERSE_BASE_ES3,
+			HEADER_OES_ES3, SAMPLER_OES, "1.0, 1.0, 1.0");
+
+	private static final String FRAGMENT_SHADER_EXT_BIN_REVERSE_YELLOW_ES3
+		= String.format(FRAGMENT_SHADER_BIN_REVERSE_BASE_ES3,
+			HEADER_OES_ES3, SAMPLER_OES, "1.0, 1.0, 0.0");
+
+	private static final String FRAGMENT_SHADER_EXT_BIN_REVERSE_GREEN_ES3
+		= String.format(FRAGMENT_SHADER_BIN_REVERSE_BASE_ES3,
+			HEADER_OES_ES3, SAMPLER_OES, "0.0, 1.0, 0.0");
+
 //--------------------------------------------------------------------------------
 	/**
 	 * 赤と黄色を強調するためのフラグメントシェーダーのベース文字列
@@ -226,6 +339,35 @@ public class EffectRendererHolder extends AbstractRendererHolder {
 	private static final String FRAGMENT_SHADER_EXT_EMPHASIZE_RED_YELLOWS_ES2
 		= String.format(FRAGMENT_SHADER_EMPHASIZE_RED_YELLOW_BASE_ES2,
 			HEADER_OES_ES2, SAMPLER_OES);
+
+	/**
+	 * 赤と黄色を強調するためのフラグメントシェーダーのベース文字列
+	 */
+	private static final String FRAGMENT_SHADER_EMPHASIZE_RED_YELLOW_BASE_ES3
+		= SHADER_VERSION_ES3 +
+		"%s" +
+		"precision mediump float;\n" +
+		"in vec2 vTextureCoord;\n" +
+		"uniform %s sTexture;\n" +
+		"uniform float uParams[" + MAX_PARAM_NUM + "];\n" +
+		"layout(location = 0) out vec4 o_FragColor;\n" +
+		FUNC_RGB2HSV +
+		FUNC_HSV2RGB +
+		"void main() {\n" +
+		"    vec3 hsv = rgb2hsv(texture(sTexture, vTextureCoord).rgb);\n" +	// RGBをHSVに変換
+		"    if ( ((hsv.g >= uParams[2]) && (hsv.g <= uParams[3]))\n" +			// s
+		"        && ((hsv.b >= uParams[4]) && (hsv.b <= uParams[5]))\n" +		// v
+		"        && ((hsv.r <= uParams[0]) || (hsv.r >= uParams[1])) ) {\n" +	// h
+		"        hsv = hsv * vec3(uParams[6], uParams[7], uParams[8]);\n" +		// 赤色と黄色の範囲
+		"    } else {\n" +
+		"        hsv = hsv * vec3(uParams[9], uParams[10], uParams[11]);\n" +	// それ以外なら
+		"    }\n" +
+		"    o_FragColor = vec4(hsv2rgb(clamp(hsv, 0.0, 1.0)), 1.0);\n" +		// HSVをRGBに戻す
+	"}\n";
+
+	private static final String FRAGMENT_SHADER_EXT_EMPHASIZE_RED_YELLOWS_ES3
+		= String.format(FRAGMENT_SHADER_EMPHASIZE_RED_YELLOW_BASE_ES3,
+			HEADER_OES_ES3, SAMPLER_OES);
 
 //--------------------------------------------------------------------------------
 	/**
@@ -257,6 +399,37 @@ public class EffectRendererHolder extends AbstractRendererHolder {
 	private static final String FRAGMENT_SHADER_EXT_EMPHASIZE_RED_YELLOW_WHITE_ES2
 		= String.format(FRAGMENT_SHADER_EMPHASIZE_RED_YELLOW_WHITE_BASE_ES2,
 			HEADER_OES_ES2, SAMPLER_OES);
+
+	/**
+	 * 赤と黄色と白色を強調するためのフラグメントシェーダーのベース文字列
+	 */
+	private static final String FRAGMENT_SHADER_EMPHASIZE_RED_YELLOW_WHITE_BASE_ES3
+		= SHADER_VERSION_ES3 +
+		"%s" +
+		"precision mediump float;\n" +
+		"in vec2 vTextureCoord;\n" +
+		"uniform %s sTexture;\n" +
+		"uniform float uParams[" + MAX_PARAM_NUM + "];\n" +
+		"layout(location = 0) out vec4 o_FragColor;\n" +
+		FUNC_RGB2HSV +
+		FUNC_HSV2RGB +
+		"void main() {\n" +
+		"    vec3 hsv = rgb2hsv(texture(sTexture, vTextureCoord).rgb);\n" +		// RGBをHSVに変換
+		"    if ( ((hsv.g >= uParams[2]) && (hsv.g <= uParams[3]))\n" +			// s
+		"        && ((hsv.b >= uParams[4]) && (hsv.b <= uParams[5]))\n" +		// v
+		"        && ((hsv.r <= uParams[0]) || (hsv.r >= uParams[1])) ) {\n" +	// h
+		"        hsv = hsv * vec3(uParams[6], uParams[7], uParams[8]);\n" +		// 赤色と黄色の範囲
+		"    } else if ((hsv.g < uParams[12]) && (hsv.b < uParams[13])) {\n" +	// 彩度が一定以下, 明度が一定以下なら
+		"        hsv = hsv * vec3(1.0, 0.0, 2.0);\n" +							// 色相そのまま, 彩度0, 明度x2
+		"    } else {\n" +
+		"        hsv = hsv * vec3(uParams[9], uParams[10], uParams[11]);\n" +	// それ以外なら
+		"    }\n" +
+		"    o_FragColor = vec4(hsv2rgb(clamp(hsv, 0.0, 1.0)), 1.0);\n" +		// HSVをRGBに戻す
+	"}\n";
+
+	private static final String FRAGMENT_SHADER_EXT_EMPHASIZE_RED_YELLOW_WHITE_ES3
+		= String.format(FRAGMENT_SHADER_EMPHASIZE_RED_YELLOW_WHITE_BASE_ES3,
+			HEADER_OES_ES3, SAMPLER_OES);
 
 //--------------------------------------------------------------------------------
 	/**
@@ -290,6 +463,39 @@ public class EffectRendererHolder extends AbstractRendererHolder {
 	private static final String FRAGMENT_SHADER_EXT_EMPHASIZE_YELLOW_WHITE_ES2
 		= String.format(FRAGMENT_SHADER_EMPHASIZE_YELLOW_WHITE_BASE_ES2,
 			HEADER_OES_ES2, SAMPLER_OES);
+
+	/**
+	 * 黄色と白を強調するためのフラグメントシェーダーのベース文字列
+	 * 今はFRAGMENT_SHADER_EMPHASIZE_RED_YELLOW_WHITE_BASEと同じ(違うパラメータ渡せば良いだけなので)
+	 */
+	private static final String FRAGMENT_SHADER_EMPHASIZE_YELLOW_WHITE_BASE_ES3
+		= SHADER_VERSION_ES3 +
+		"%s" +
+		"precision mediump float;\n" +
+		"in vec2 vTextureCoord;\n" +
+		"uniform %s sTexture;\n" +
+		"uniform float uParams[" + MAX_PARAM_NUM + "];\n" +
+		"layout(location = 0) out vec4 o_FragColor;\n" +
+		FUNC_RGB2HSV +
+		FUNC_HSV2RGB +
+		"void main() {\n" +
+		"    vec3 rgb = texture(sTexture, vTextureCoord).rgb;\n" +			// RGB
+		"    vec3 hsv = rgb2hsv(rgb);\n" +										// RGBをHSVに変換
+		"    if (   ((hsv.r >= uParams[0]) && (hsv.r <= uParams[1]))\n" +		// h
+		"        && ((hsv.g >= uParams[2]) && (hsv.g <= uParams[3]))\n" +		// s
+		"        && ((hsv.b >= uParams[4]) && (hsv.b <= uParams[5])) ) {\n" +	// v
+		"        hsv = hsv * vec3(uParams[6], uParams[7], uParams[8]);\n" +		// 黄色の範囲
+		"    } else if ((hsv.g < uParams[12]) && (hsv.b > uParams[13])) {\n" +	// 彩度が一定以下, 明度が一定以上なら
+		"        hsv = hsv * vec3(1.0, 0.0, 2.0);\n" +							// 色相そのまま, 彩度0, 明度x2
+		"    } else {\n" +
+		"        hsv = hsv * vec3(uParams[9], uParams[10], uParams[11]);\n" +	// それ以外なら
+		"    }\n" +
+		"    o_FragColor = vec4(hsv2rgb(clamp(hsv, 0.0, 1.0)), 1.0);\n" +		// HSVをRGBに戻す
+	"}\n";
+
+	private static final String FRAGMENT_SHADER_EXT_EMPHASIZE_YELLOW_WHITE_ES3
+		= String.format(FRAGMENT_SHADER_EMPHASIZE_YELLOW_WHITE_BASE_ES3,
+			HEADER_OES_ES3, SAMPLER_OES);
 
 //================================================================================
 	/**
@@ -505,55 +711,66 @@ public class EffectRendererHolder extends AbstractRendererHolder {
 			if (DEBUG) Log.v(TAG, "MyRendererTask#handleChangeEffect:" + effect);
 			mEffect = effect;
 			if (mDrawer != null) {
-				final GLDrawer2D drawer = mDrawer;
 				switch (effect) {
 				case EFFECT_NON:
-					drawer.updateShader(FRAGMENT_SHADER_EXT_ES2);
+					mDrawer.updateShader(isGLES3()
+						? FRAGMENT_SHADER_EXT_ES3 : FRAGMENT_SHADER_EXT_ES2);
 					break;
 				case EFFECT_GRAY:
-					drawer.updateShader(FRAGMENT_SHADER_EXT_GRAY_ES2);
+					mDrawer.updateShader(isGLES3()
+						? FRAGMENT_SHADER_EXT_GRAY_ES3 : FRAGMENT_SHADER_EXT_GRAY_ES2);
 					break;
 				case EFFECT_GRAY_REVERSE:
-					drawer.updateShader(FRAGMENT_SHADER_GRAY_EXT_REVERSE_ES2);
+					mDrawer.updateShader(isGLES3()
+						? FRAGMENT_SHADER_GRAY_EXT_REVERSE_ES3 : FRAGMENT_SHADER_GRAY_EXT_REVERSE_ES2);
 					break;
 				case EFFECT_BIN:
-					drawer.updateShader(FRAGMENT_SHADER_EXT_BIN_ES2);
+					mDrawer.updateShader(isGLES3()
+						? FRAGMENT_SHADER_EXT_BIN_ES3 : FRAGMENT_SHADER_EXT_BIN_ES2);
 					break;
 				case EFFECT_BIN_YELLOW:
-					drawer.updateShader(FRAGMENT_SHADER_EXT_BIN_YELLOW_ES2);
+					mDrawer.updateShader(isGLES3()
+						?FRAGMENT_SHADER_EXT_BIN_YELLOW_ES3 : FRAGMENT_SHADER_EXT_BIN_YELLOW_ES2);
 					break;
 				case EFFECT_BIN_GREEN:
-					drawer.updateShader(FRAGMENT_SHADER_EXT_BIN_GREEN_ES2);
+					mDrawer.updateShader(isGLES3()
+						? FRAGMENT_SHADER_EXT_BIN_GREEN_ES3 : FRAGMENT_SHADER_EXT_BIN_GREEN_ES2);
 					break;
 				case EFFECT_BIN_REVERSE:
-					drawer.updateShader(FRAGMENT_SHADER_EXT_BIN_REVERSE_ES2);
+					mDrawer.updateShader(isGLES3()
+						? FRAGMENT_SHADER_EXT_BIN_REVERSE_ES3 : FRAGMENT_SHADER_EXT_BIN_REVERSE_ES2);
 					break;
 				case EFFECT_BIN_REVERSE_YELLOW:
-					drawer.updateShader(FRAGMENT_SHADER_EXT_BIN_REVERSE_YELLOW_ES2);
+					mDrawer.updateShader(isGLES3()
+						? FRAGMENT_SHADER_EXT_BIN_REVERSE_YELLOW_ES3 : FRAGMENT_SHADER_EXT_BIN_REVERSE_YELLOW_ES2);
 					break;
 				case EFFECT_BIN_REVERSE_GREEN:
-					drawer.updateShader(FRAGMENT_SHADER_EXT_BIN_REVERSE_GREEN_ES2);
+					mDrawer.updateShader(isGLES3()
+						? FRAGMENT_SHADER_EXT_BIN_REVERSE_GREEN_ES3 : FRAGMENT_SHADER_EXT_BIN_REVERSE_GREEN_ES2);
 					break;
 				case EFFECT_EMPHASIZE_RED_YELLOW:
-					drawer.updateShader(FRAGMENT_SHADER_EXT_EMPHASIZE_RED_YELLOWS_ES2);
+					mDrawer.updateShader(isGLES3()
+						? FRAGMENT_SHADER_EXT_EMPHASIZE_RED_YELLOWS_ES3 : FRAGMENT_SHADER_EXT_EMPHASIZE_RED_YELLOWS_ES2);
 					break;
 				case EFFECT_EMPHASIZE_RED_YELLOW_WHITE:
-					drawer.updateShader(FRAGMENT_SHADER_EXT_EMPHASIZE_RED_YELLOW_WHITE_ES2);
+					mDrawer.updateShader(isGLES3()
+						? FRAGMENT_SHADER_EXT_EMPHASIZE_RED_YELLOW_WHITE_ES3 : FRAGMENT_SHADER_EXT_EMPHASIZE_RED_YELLOW_WHITE_ES2);
 					break;
 				case EFFECT_EMPHASIZE_YELLOW_WHITE:
-					drawer.updateShader(FRAGMENT_SHADER_EXT_EMPHASIZE_YELLOW_WHITE_ES2);
+					mDrawer.updateShader(isGLES3()
+						? FRAGMENT_SHADER_EXT_EMPHASIZE_YELLOW_WHITE_ES3 : FRAGMENT_SHADER_EXT_EMPHASIZE_YELLOW_WHITE_ES2);
 					break;
 				default:
 					try {
 						((EffectRendererHolder)getParent())
-							.handleDefaultEffect(effect, drawer);
+							.handleDefaultEffect(effect, mDrawer);
 					} catch (final Exception e) {
-						drawer.resetShader();
+						mDrawer.resetShader();
 						Log.w(TAG, e);
 					}
 					break;
 				}
-				muParamsLoc = drawer.glGetUniformLocation("uParams");
+				muParamsLoc = mDrawer.glGetUniformLocation("uParams");
 				mCurrentParams = mParams.get(effect);
 				updateParams();
 			} else if (DEBUG) Log.d(TAG, "handleChangeEffect: mDrawer is not IShaderDrawer2d");
@@ -588,7 +805,11 @@ public class EffectRendererHolder extends AbstractRendererHolder {
 				if (mDrawer != null) {
 					mDrawer.glUseProgram();
 				} else if (DEBUG) Log.d(TAG, "handleChangeEffect: mDrawer is null");
-				GLES20.glUniform1fv(muParamsLoc, n, mCurrentParams, 0);
+				if (isGLES3()) {
+					GLES30.glUniform1fv(muParamsLoc, n, mCurrentParams, 0);
+				} else {
+					GLES20.glUniform1fv(muParamsLoc, n, mCurrentParams, 0);
+				}
 			}
 		}
 
