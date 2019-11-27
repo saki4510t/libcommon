2eecec62645b95fb45032d3bb96337198d09f50e
saki4510t <saki4510t@gmail.com>
2019-11-27 16:30:51 +0900

    TWIRL効果付与のフラグメントシェーダES3版を追加

--- common/src/main/java/com/serenegiant/glutils/ShaderConst.java
+++ common/src/main/java/com/serenegiant/glutils/ShaderConst.java
@@ -392,6 +392,10 @@ public class ShaderConst {
 		= String.format(FRAGMENT_SHADER_SQUEEZE_BASE_ES3, HEADER_OES_ES3, SAMPLER_OES);
 
 //--------------------------------------------------------------------------------
+	/**
+	 * TWIRL効果付与のフラグメントシェーダ
+	 * for ES2
+	 */
 	public static final String FRAGMENT_SHADER_TWIRL_BASE_ES2
 		= SHADER_VERSION_ES2 +
 		"%s" +
@@ -414,6 +418,34 @@ public class ShaderConst {
 		= String.format(FRAGMENT_SHADER_TWIRL_BASE_ES2, HEADER_2D, SAMPLER_2D);
 	public static final String FRAGMENT_SHADER_EXT_TWIRL_ES2
 		= String.format(FRAGMENT_SHADER_TWIRL_BASE_ES2, HEADER_OES_ES2, SAMPLER_OES);
+
+	/**
+	 * TWIRL効果付与のフラグメントシェーダ
+	 * for ES3
+	 */
+	public static final String FRAGMENT_SHADER_TWIRL_BASE_ES3
+		= SHADER_VERSION_ES3 +
+		"%s" +
+		"precision mediump float;\n" +
+		"in vec2 vTextureCoord;\n" +
+		"uniform %s sTexture;\n" +
+		"uniform vec2 uPosition;\n" +
+		"layout(location = 0) out vec4 o_FragColor;\n" +
+		"void main() {\n" +
+		"    vec2 texCoord = vTextureCoord.xy;\n" +
+		"    vec2 normCoord = 2.0 * texCoord - 1.0;\n"+
+		"    float r = length(normCoord); // to polar coords \n" +
+		"    float phi = atan(normCoord.y + uPosition.y, normCoord.x + uPosition.x);\n"+	// to polar coords
+		"    phi = phi + (1.0 - smoothstep(-0.5, 0.5, r)) * 4.0;\n"+ // Twirl it
+		"    normCoord.x = r * cos(phi); \n" +
+		"    normCoord.y = r * sin(phi); \n" +
+		"    texCoord = normCoord / 2.0 + 0.5;\n"+
+		"    o_FragColor = texture(sTexture, texCoord);\n"+
+		"}\n";
+	public static final String FRAGMENT_SHADER_TWIRL_ES3
+		= String.format(FRAGMENT_SHADER_TWIRL_BASE_ES3, HEADER_2D, SAMPLER_2D);
+	public static final String FRAGMENT_SHADER_EXT_TWIRL_ES3
+		= String.format(FRAGMENT_SHADER_TWIRL_BASE_ES3, HEADER_OES_ES3, SAMPLER_OES);
 
 //--------------------------------------------------------------------------------
 	public static final String FRAGMENT_SHADER_TUNNEL_BASE_ES2
