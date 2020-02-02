1f0071dc679cb0cb6a31c36f4f172e8b4026743f
saki4510t <saki4510t@gmail.com>
2020-02-02 19:50:00 +0900

    アノテーション追加

--- common/src/main/java/com/serenegiant/graphics/ShaderDrawable.java
+++ common/src/main/java/com/serenegiant/graphics/ShaderDrawable.java
@@ -24,14 +24,19 @@ import android.graphics.ColorFilter;
 import android.graphics.DrawFilter;
 import android.graphics.Paint;
 import android.graphics.PaintFlagsDrawFilter;
+import android.graphics.PixelFormat;
 import android.graphics.RectF;
 import android.graphics.Shader;
 import android.graphics.drawable.Drawable;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class ShaderDrawable extends Drawable {
 
 	private final Paint mPaint;
     private final DrawFilter mDrawFilter;
+    @Nullable
 	private Shader mShader;
 
 	public ShaderDrawable() {
@@ -56,7 +61,7 @@ public class ShaderDrawable extends Drawable {
 	}
 
 	@Override
-	public void draw(final Canvas canvas) {
+	public void draw(@NonNull final Canvas canvas) {
 		if (mShader != null) {
 			final int count = canvas.save();
 			final DrawFilter org = canvas.getDrawFilter();
@@ -86,10 +91,10 @@ public class ShaderDrawable extends Drawable {
 
 	@Override
 	public int getOpacity() {
-		return 0;
+		return PixelFormat.UNKNOWN;
 	}
 
-	public void setBounds(final RectF bounds) {
+	public void setBounds(@NonNull final RectF bounds) {
 		super.setBounds((int)bounds.left, (int)bounds.top, (int)bounds.right, (int)bounds.bottom);
 	}
 
@@ -97,13 +102,14 @@ public class ShaderDrawable extends Drawable {
 		super.setBounds((int)left, (int)top, (int)right, (int)bottom);
 	}
 
-	public Shader setShader(final Shader shader) {
+	public Shader setShader(@Nullable final Shader shader) {
 		if (mShader != shader) {
 			mShader = shader;
 		}
 		return shader;
 	}
 
+	@Nullable
 	public Shader getShader() {
 		return mShader;
 	}
