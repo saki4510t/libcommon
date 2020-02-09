f2fdc8c574c21c845b14d2a204292d56704210d4
saki4510t <saki4510t@gmail.com>
2020-02-09 18:30:01 +0900

    対応しているOpenGL|ESのバージョンを取得するためのヘルパーメソッドを追加

--- common/src/main/java/com/serenegiant/glutils/GLUtils.java
+++ common/src/main/java/com/serenegiant/glutils/GLUtils.java
@@ -18,13 +18,66 @@ package com.serenegiant.glutils;
  *  limitations under the License.
 */
 
+import android.opengl.GLES20;
+import android.opengl.GLES30;
 import android.opengl.Matrix;
+import android.os.Build;
+import android.util.Log;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import static com.serenegiant.glutils.IRendererCommon.*;
 
 public class GLUtils {
+	private static final String TAG = GLUtils.class.getSimpleName();
+
 	private GLUtils() {
 		// インスタンス化を防ぐためにデフォルトコンストラクタをprivateに
+	}
+
+	private static int sSupportedGLVersion = -1;
+
+	/**
+	 * 対応しているGL|ESのバージョンを取得
+	 * XXX GLES30はAPI>=18以降なんだけどAPI=18でもGLコンテキスト生成に失敗する端末があるのでAP1>=21に変更
+	 * API>=21でGL_OES_EGL_image_external_essl3に対応していれば3, そうでなければ2を返す
+	 * @return
+	 */
+	public static int getSupportedGLVersion() {
+		if (sSupportedGLVersion < 1) {
+			final AtomicInteger result = new AtomicInteger(1);
+			final Semaphore sync = new Semaphore(0);
+			final GLContext context = new GLContext(3, null, 0);
+			// ダミースレッド上でEGL/GLコンテキストを生成して
+			new Thread(new Runnable() {
+				@Override
+				public void run() {
+					context.initialize();
+					String extensions = GLES20.glGetString(GLES20.GL_EXTENSIONS); // API >= 8
+//					Log.i(TAG, "getSupportedGLVersion:" + extensions);
+					if ((extensions == null) || !extensions.contains("GL_OES_EGL_image_external")) {
+						result.set(1);
+					} else if ((Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) && context.isGLES3()) {
+						extensions = GLES30.glGetString(GLES30.GL_EXTENSIONS); 	// API >= 18
+						result.set((extensions != null) && !extensions.contains("GL_OES_EGL_image_external_essl3")
+							? 3 : 2);
+					} else {
+						result.set(2);
+					}
+					context.release();
+					sync.release();
+				}
+			}).start();
+			try {
+				sync.tryAcquire(500, TimeUnit.MILLISECONDS);
+				sSupportedGLVersion = result.get();
+			} catch (final InterruptedException e) {
+				// ignore
+			}
+		}
+		return sSupportedGLVersion;
 	}
 
 	/**
