1ac14037274841879538df4a634f8b2ae4033d51
saki4510t <saki4510t@gmail.com>
2020-02-11 21:22:40 +0900

    共有コンテキスト使うときのクラッシュ対策で常時頂点座標を読み込んでいたのをやめてvalidateProgramがfalseの時に1回だけシェーダーをリセットするように変更

--- common/src/main/java/com/serenegiant/glutils/GLDrawer2D.java
+++ common/src/main/java/com/serenegiant/glutils/GLDrawer2D.java
@@ -292,16 +292,14 @@ public abstract class GLDrawer2D {
 			updateMvpMatrix(mvp_matrix, mvp_offset);
 		}
 		bindTexture(texId);
-		if (true) {
-			// XXX 共有コンテキストを使っていると頂点配列が壊れてしまうときがあるようなので都度読み込む
-			updateVertices();
-		}
 		if (validateProgram(hProgram)) {
 			drawVertices();
+			errCnt = 0;
 		} else {
 			if (errCnt++ == 0) {
 				Log.w(TAG, "draw:invalid program");
-				// FIXME シェーダーを再初期化する?
+				// シェーダーを再初期化する
+				resetShader();
 			}
 		}
 		finishDraw();
