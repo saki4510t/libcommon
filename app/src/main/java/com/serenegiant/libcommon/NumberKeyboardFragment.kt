294b1abbce03226f333b69515923255109941640
saki4510t <saki4510t@gmail.com>
2020-02-20 20:43:53 +0900

    カスタムキーボードレイアウトテストFragmentの処理を実装...キーの文字が表示されない(´・ω・｀)

--- app/src/main/java/com/serenegiant/libcommon/NumberKeyboardFragment.kt
+++ app/src/main/java/com/serenegiant/libcommon/NumberKeyboardFragment.kt
@@ -5,20 +5,55 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import com.serenegiant.system.KeyboardDelegater
 import com.serenegiant.view.ViewUtils
+import com.serenegiant.widget.KeyboardView
 
 /**
  * NumberKeyboardテスト用フラグメント
  */
 class NumberKeyboardFragment : BaseFragment() {
 
+	private var mInputEditText: EditText? = null
+	private var mKeyboardView: KeyboardView? = null
+	private var mDelegater: KeyboardDelegater? = null
+
 	override fun onCreateView(inflater: LayoutInflater,
 		container: ViewGroup?, savedInstanceState: Bundle?): View? {
 
 		if (DEBUG) Log.v(TAG, "onCreateView:")
 		val customInflater
-			= ViewUtils.createCustomLayoutInflater(requireContext(), inflater, R.style.AppTheme_Usb)
-		return customInflater.inflate(R.layout.fragment_numberkeyboard, container, false)
+			= ViewUtils.createCustomLayoutInflater(requireContext(), inflater, R.style.AppTheme_NumberKeyboard)
+		val rootView = customInflater.inflate(R.layout.fragment_numberkeyboard, container, false)
+		initView(rootView)
+		return rootView;
+	}
+
+	override fun internalOnResume() {
+		super.internalOnResume()
+		mDelegater!!.showKeyboard()
+	}
+
+	override fun internalOnPause() {
+		mDelegater!!.hideKeyboard()
+		super.internalOnPause()
+	}
+
+	fun initView(rootView: View) {
+		mInputEditText = rootView.findViewById(R.id.edittext)
+		mKeyboardView = rootView.findViewById(R.id.number_keyboardview)
+		mDelegater = object : KeyboardDelegater(
+			mInputEditText!!, mKeyboardView!!, R.xml.keyboard_number) {
+
+			override fun onCancelClick() {
+				if (DEBUG) Log.v(TAG, "onCancelClick:")
+			}
+
+			override fun onOkClick() {
+				if (DEBUG) Log.v(TAG, "onOkClick:")
+			}
+		}
 	}
 
 	companion object {
---
+++ common/src/main/java/com/serenegiant/system/KeyboardDelegater.java
@@ -0,0 +1,157 @@
+package com.serenegiant.system;
+
+import android.content.Context;
+import android.text.Editable;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import com.serenegiant.widget.Keyboard;
+import com.serenegiant.widget.KeyboardView;
+
+import java.lang.reflect.Method;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.XmlRes;
+
+public abstract class KeyboardDelegater
+	implements KeyboardView.OnKeyboardActionListener {
+
+	private static final boolean DEBUG = true;	// set false on production
+	private static final String TAG = KeyboardDelegater.class.getSimpleName();
+
+	@NonNull
+	private final EditText mEditText;
+	@NonNull
+	private final KeyboardView mKeyboardView;
+	@XmlRes
+	private final int mKeyboardLayoutRes;
+	@Nullable
+	private Keyboard mKeyboard;
+
+	public KeyboardDelegater(
+		@NonNull final EditText editText,
+		@NonNull final KeyboardView keyboardView,
+		@XmlRes final int keyboardLayoutRes) {
+
+		if (DEBUG) Log.v(TAG, "コンストラクタ:");
+		mEditText = editText;
+		mKeyboardView = keyboardView;
+		mKeyboardLayoutRes = keyboardLayoutRes;
+	}
+
+	public void showKeyboard() {
+		if (DEBUG) Log.v(TAG, "showKeyboard:");
+		hideSystemSoftKeyboard();
+		if (mKeyboard == null) {
+			mKeyboard = new Keyboard(mEditText.getContext(), mKeyboardLayoutRes);
+			mKeyboardView.setKeyboard(mKeyboard);
+		}
+		mKeyboardView.setEnabled(true);
+		mKeyboardView.setPreviewEnabled(false);
+		mKeyboardView.setOnKeyboardActionListener(this);
+		final int visibility = mKeyboardView.getVisibility();
+		if (visibility == View.GONE || visibility == View.INVISIBLE) {
+			mKeyboardView.setVisibility(View.VISIBLE);
+		}
+	}
+
+	public void hideKeyboard() {
+		if (DEBUG) Log.v(TAG, "hideKeyboard:");
+		int visibility = mKeyboardView.getVisibility();
+		if (visibility == View.VISIBLE) {
+			mKeyboardView.setVisibility(View.GONE);
+		}
+	}
+
+	public void hideSystemSoftKeyboard() {
+
+		if (DEBUG) Log.v(TAG, "hideSystemSoftKeyboard:");
+		try {
+			final Class<EditText> cls = EditText.class;
+			final Method setShowSoftInputOnFocus
+				= cls.getMethod("setShowSoftInputOnFocus", boolean.class);
+			setShowSoftInputOnFocus.setAccessible(true);
+			setShowSoftInputOnFocus.invoke(mEditText, false);
+		} catch (final SecurityException e) {
+			if (DEBUG) Log.w(TAG, e);
+		} catch (final NoSuchMethodException e) {
+			if (DEBUG) Log.w(TAG, e);
+		} catch (final Exception e) {
+			if (DEBUG) Log.w(TAG, e);
+		}
+		final InputMethodManager imm
+			= (InputMethodManager) mEditText.getContext()
+				.getSystemService(Context.INPUT_METHOD_SERVICE);
+		imm.hideSoftInputFromWindow(mEditText.getWindowToken(), 0);
+	}
+
+//--------------------------------------------------------------------------------
+	protected abstract void onOkClick();
+	protected abstract void onCancelClick();
+
+//--------------------------------------------------------------------------------
+	@Override
+	public void onPress(final int primaryCode) {
+		if (DEBUG) Log.v(TAG, "onPress:");
+	}
+
+	@Override
+	public void onRelease(final int primaryCode) {
+		if (DEBUG) Log.v(TAG, "onRelease:");
+	}
+
+	@Override
+	public void onKey(final int primaryCode, final int[] keyCodes) {
+		if (DEBUG) Log.v(TAG, "onKey:");
+		Editable editable = mEditText.getText();
+		int start = mEditText.getSelectionStart();
+		if (primaryCode == Keyboard.KEYCODE_DELETE) {
+			// 削除(Delete)キー
+			if (editable != null && editable.length() > 0) {
+				if (start > 0) {
+					editable.delete(start - 1, start);
+				}
+			}
+		} else if (primaryCode == Keyboard.KEYCODE_CANCEL) {
+			// キャンセル(ESC)キー
+			hideKeyboard();
+			onCancelClick();
+		} else if (primaryCode == Keyboard.KEYCODE_DONE) {
+			// 完了(ENTER)キー
+			hideKeyboard();
+			onOkClick();
+		} else {
+			// それ以外のキー入力
+			editable.insert(start, Character.toString((char) primaryCode));
+		}
+	}
+
+	@Override
+	public void onText(final CharSequence text) {
+		if (DEBUG) Log.v(TAG, "onText:");
+	}
+
+	@Override
+	public void swipeLeft() {
+		if (DEBUG) Log.v(TAG, "swipeLeft:");
+	}
+
+	@Override
+	public void swipeRight() {
+		if (DEBUG) Log.v(TAG, "swipeRight:");
+	}
+
+	@Override
+	public void swipeDown() {
+		if (DEBUG) Log.v(TAG, "swipeDown:");
+	}
+
+	@Override
+	public void swipeUp() {
+		if (DEBUG) Log.v(TAG, "swipeUp:");
+	}
+
+}
