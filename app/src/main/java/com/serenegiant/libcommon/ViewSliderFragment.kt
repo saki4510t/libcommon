57818fed9e3fa79cd3eb352bd32cfff7e899f9ed
saki4510t <saki4510t@gmail.com>
2020-03-02 21:15:22 +0900

    ViewSliderで四辺からのスライドin/outをテストできるようにレイアウトとViewSlideFragmentでの処理を修正

--- app/src/main/java/com/serenegiant/libcommon/ViewSliderFragment.kt
+++ app/src/main/java/com/serenegiant/libcommon/ViewSliderFragment.kt
@@ -4,20 +4,22 @@ import android.content.Context
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
+import android.view.MotionEvent
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
-import com.serenegiant.system.KeyboardDelegater
 import com.serenegiant.view.ViewSlider
 import com.serenegiant.view.ViewUtils
-import com.serenegiant.widget.KeyboardView
 
 /**
  * ViewSliderテスト用フラグメント
  */
 class ViewSliderFragment : BaseFragment() {
 
-	private var mViewSlider: ViewSlider? = null
+	private var mRootView: View? = null
+	private var mViewSliderLeft: ViewSlider? = null
+	private var mViewSliderTop: ViewSlider? = null
+	private var mViewSliderRight: ViewSlider? = null
+	private var mViewSliderBottom: ViewSlider? = null
 
 	override fun onAttach(context: Context) {
 		super.onAttach(context)
@@ -30,9 +32,9 @@ class ViewSliderFragment : BaseFragment() {
 		if (DEBUG) Log.v(TAG, "onCreateView:")
 		val customInflater
 			= ViewUtils.createCustomLayoutInflater(requireContext(), inflater, R.style.AppTheme_ViewSlider)
-		val rootView = customInflater.inflate(R.layout.fragment_viewslider, container, false)
-		initView(rootView)
-		return rootView;
+		mRootView = customInflater.inflate(R.layout.fragment_viewslider, container, false)
+		initView(mRootView!!)
+		return mRootView;
 	}
 
 	override fun internalOnResume() {
@@ -43,17 +45,70 @@ class ViewSliderFragment : BaseFragment() {
 		super.internalOnPause()
 	}
 
-	fun initView(rootView: View) {
-		mViewSlider = ViewSlider(rootView, R.id.slide_view)
-		rootView.setOnClickListener {
-			var visibility = mViewSlider!!.visibility;
-			if (mViewSlider!!.isVisible) {
-				mViewSlider!!.hide(1000)
-			} else {
-				mViewSlider!!.show(300)
+	private fun initView(rootView: View) {
+		rootView.setOnTouchListener(mOnTouchListener)
+		mViewSliderLeft = ViewSlider(rootView, R.id.slide_view_left)
+		mViewSliderLeft!!.hide(0)
+		mViewSliderLeft!!.targetView.setOnTouchListener(mOnTouchListener)
+
+		mViewSliderTop = ViewSlider(rootView, R.id.slide_view_top)
+		mViewSliderTop!!.hide(0)
+		mViewSliderTop!!.targetView.setOnTouchListener(mOnTouchListener)
+
+		mViewSliderRight = ViewSlider(rootView, R.id.slide_view_right)
+		mViewSliderRight!!.hide(0)
+		mViewSliderRight!!.targetView.setOnTouchListener(mOnTouchListener)
+
+		mViewSliderBottom = ViewSlider(rootView, R.id.slide_view_bottom)
+		mViewSliderBottom!!.hide(0)
+	}
+
+	private val mOnTouchListener = View.OnTouchListener { view, event ->
+		if (DEBUG) Log.v(TAG, "onTouch:${view},${event}")
+		if (event.actionMasked == MotionEvent.ACTION_UP) {
+			if (view == mRootView) {
+				val width = view.width
+				val height = view.height
+				val left = width / 4;
+				val top = height / 4
+				val right = width * 3 / 4
+				val bottom = height * 3 / 4
+				if (event.y < top) {
+					if (mViewSliderTop!!.isVisible) {
+						mViewSliderTop!!.hide()
+					} else {
+						mViewSliderTop!!.show(0)
+					}
+				} else if (event.y > bottom) {
+					if (mViewSliderBottom!!.isVisible) {
+						mViewSliderBottom!!.hide()
+					} else {
+						mViewSliderBottom!!.show(0)
+					}
+				} else if (event.x < left) {
+					if (mViewSliderLeft!!.isVisible) {
+						mViewSliderLeft!!.hide()
+					} else {
+						mViewSliderLeft!!.show(0)
+					}
+				} else if (event.x > right) {
+					if (mViewSliderRight!!.isVisible) {
+						mViewSliderRight!!.hide()
+					} else {
+						mViewSliderRight!!.show(0)
+					}
+				}
+			} else if (view == mViewSliderLeft!!.targetView) {
+				mViewSliderLeft!!.hide()
+			} else if (view == mViewSliderTop!!.targetView) {
+				mViewSliderTop!!.hide()
+			} else if (view == mViewSliderRight!!.targetView) {
+				mViewSliderRight!!.hide()
+			} else if (view == mViewSliderBottom!!.targetView) {
+				mViewSliderBottom!!.hide()
 			}
 		}
-		mViewSlider!!.hide(300)
+		return@OnTouchListener true
 	}
 
 	companion object {
