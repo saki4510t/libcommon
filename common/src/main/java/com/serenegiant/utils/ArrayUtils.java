601c04db9941b48b5dd88ae3da9e6ee9b657c10a
saki4510t <saki4510t@gmail.com>
2020-04-24 21:59:13 +0900

    プリミティブ用のヘルパーメソッドを追加

--- common/src/main/java/com/serenegiant/utils/ArrayUtils.java
+++ common/src/main/java/com/serenegiant/utils/ArrayUtils.java
@@ -25,6 +25,81 @@ public class ArrayUtils {
 	}
 
 	/**
+	 * 指定した値が配列に含まれているかどうかをチェック
+	 * @param values
+	 * @param value
+	 * @return
+	 */
+	public static boolean contains(@NonNull final byte[] values, final byte value) {
+		for (final byte v: values) {
+			if (v == value) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 指定した値が配列に含まれているかどうかをチェック
+	 * @param values
+	 * @param value
+	 * @return
+	 */
+	public static boolean contains(@NonNull final short[] values, final short value) {
+		for (final short v: values) {
+			if (v == value) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 指定した値が配列に含まれているかどうかをチェック
+	 * @param values
+	 * @param value
+	 * @return
+	 */
+	public static boolean contains(@NonNull final int[] values, final int value) {
+		for (final int v: values) {
+			if (v == value) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 指定した値が配列に含まれているかどうかをチェック
+	 * @param values
+	 * @param value
+	 * @return
+	 */
+	public static boolean contains(@NonNull final float[] values, final float value) {
+		for (final float v: values) {
+			if (Float.compare(v, value) == 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * 指定した値が配列に含まれているかどうかをチェック
+	 * @param values
+	 * @param value
+	 * @return
+	 */
+	public static boolean contains(@NonNull final double[] values, final double value) {
+		for (final double v: values) {
+			if (Double.compare(v, value) == 0) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	/**
 	 * System.arraycopyのヘルパークラス
 	 * srcの全部をdstへコピーする
 	 * @param src
