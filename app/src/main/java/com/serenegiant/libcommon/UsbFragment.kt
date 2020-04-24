db689a6c0fd14818145fc3056d720aba48eeb23f
saki4510t <saki4510t@gmail.com>
2020-04-24 20:06:52 +0900

    DeviceFilterの読み込みテスト用にデバッグログ出力を追加

--- app/src/main/java/com/serenegiant/libcommon/UsbFragment.kt
+++ app/src/main/java/com/serenegiant/libcommon/UsbFragment.kt
@@ -44,9 +44,22 @@ class UsbFragment : BaseFragment() {
 		requireActivity().title = getString(R.string.title_usB_monitor)
 		if (mUSBMonitor == null) {
 			mUSBMonitor = USBMonitor(context, mOnDeviceConnectListener)
-			val filters
+			var filters
 				 = DeviceFilter.getDeviceFilters(context, R.xml.device_filter_uvc_exclude)
 			mUSBMonitor!!.setDeviceFilter(filters)
+			if (DEBUG) Log.v(TAG, "onAttach:uvc_exclude=$filters")
+			filters = DeviceFilter.getDeviceFilters(context, R.xml.device_filter_uvc)
+			if (DEBUG) Log.v(TAG, "onAttach:uvc=$filters")
+			filters = DeviceFilter.getDeviceFilters(context, R.xml.device_filter_uac)
+			if (DEBUG) Log.v(TAG, "onAttach:uac=$filters")
+			// API>=28以上でclass,subclassが0になってのでinterfaceClass等を追加した定義の読み込みテスト
+			filters  = DeviceFilter.getDeviceFilters(context, R.xml.device_filter_uvc_exclude_intf)
+			mUSBMonitor!!.setDeviceFilter(filters)
+			if (DEBUG) Log.v(TAG, "onAttach:uvc_exclude_sub=$filters")
+			filters = DeviceFilter.getDeviceFilters(context, R.xml.device_filter_uvc_intf)
+			if (DEBUG) Log.v(TAG, "onAttach:uvc_sub=$filters")
+			filters = DeviceFilter.getDeviceFilters(context, R.xml.device_filter_uac_intf)
+			if (DEBUG) Log.v(TAG, "onAttach:uac_sub=$filters")
 		}
 	}
 
