e04d4d67bba402773d79b9a922a0d9a548d78403
saki4510t <saki4510t@gmail.com>
2020-05-04 04:02:18 +0900

    コメント修正

--- app/src/main/java/com/serenegiant/service/RecordingService.java
+++ app/src/main/java/com/serenegiant/service/RecordingService.java
@@ -56,7 +56,7 @@ public class RecordingService extends BaseService {
 	private static final String TAG = RecordingService.class.getSimpleName();
 
 	/**
-	 * MediaStoreOutputStreamを使って出力するかどうか
+	 * MediaStoreOutputStreamを使って出力するかどうか(isAndroid8以降のみ有効)
 	 */
 	private static final boolean USE_MEDIASTORE_OUTPUT_STREAM = true;
 
