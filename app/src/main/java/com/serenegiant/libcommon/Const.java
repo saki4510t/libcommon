76799bb22f5d8f74962d755a1dc1a45ceb8d0a93
saki4510t <saki4510t@gmail.com>
2020-05-06 19:43:15 +0900

    Kotlinに変換

--- app/src/main/java/com/serenegiant/libcommon/Const.java
+++
@@ -1,9 +0,0 @@
-package com.serenegiant.libcommon;
-
-public class Const {
-	public static final int REQUEST_ACCESS_SD = 34567;
-	public static final String APP_DIR = "libcommon";
-
-	/*package*/
-	static final String PREF_NAME = "com.serenegiant.libcommon";
-}
---
+++ app/src/main/java/com/serenegiant/libcommon/Const.kt
@@ -0,0 +1,9 @@
+package com.serenegiant.libcommon
+
+object Const {
+	const val REQUEST_ACCESS_SD = 34567
+	const val APP_DIR = "libcommon"
+
+	/*package*/
+	const val PREF_NAME = "com.serenegiant.libcommon"
+}
