bb575be454d1506f4b7b97bdc60f2697b3161053
saki4510t <saki4510t@gmail.com>
2020-05-28 16:10:34 +0900

    リスト項目の長押しでSAFパーミッションを開放する処理を実装

--- app/src/main/java/com/serenegiant/libcommon/SAFUtilsFragment.java
+++ app/src/main/java/com/serenegiant/libcommon/SAFUtilsFragment.java
@@ -3,6 +3,7 @@ package com.serenegiant.libcommon;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,7 @@ import android.widget.Toast;
 import com.serenegiant.libcommon.databinding.FragmentSafutilsBinding;
 import com.serenegiant.libcommon.viewmodel.SAFUtilsViewModel;
 import com.serenegiant.utils.SAFUtils;
+import com.serenegiant.widget.ArrayListRecyclerViewAdapter;
 import com.serenegiant.widget.StringsRecyclerViewAdapter;
 
 import java.util.ArrayList;
@@ -22,6 +24,7 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.databinding.DataBindingUtil;
 import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SAFUtilsFragment extends BaseFragment {
 	private static final boolean DEBUG = true;	// set false on production
@@ -97,6 +100,44 @@ public class SAFUtilsFragment extends BaseFragment {
 	private void initView() {
 		mAdapter = new StringsRecyclerViewAdapter(
 			R.layout.list_item_title, new ArrayList<String>());
+		mAdapter.setOnItemClickListener(
+			new ArrayListRecyclerViewAdapter.ArrayListRecyclerViewListener<String>() {
+
+			@Override
+			public void onItemClick(
+				@NonNull final RecyclerView.Adapter<?> parent,
+				@NonNull final View view,
+				final int position, @Nullable final String item) {
+
+			}
+
+			@Override
+			public boolean onItemLongClick(
+				@NonNull final RecyclerView.Adapter<?> parent,
+				@NonNull final View view,
+				final int position, @Nullable final String item) {
+
+				if (!TextUtils.isEmpty(item)) {
+					final String[] v = item.split("@");
+					if (v.length == 2) {
+						try {
+							final int requestCode = Integer.parseInt(v[0]);
+							if (requestCode != 0) {
+								Toast.makeText(requireContext(),
+									"release permission,requestCode=" + requestCode,
+									Toast.LENGTH_SHORT).show();
+								SAFUtils.releaseStorageAccessPermission(requireContext(), requestCode);
+								updateSAFPermissions();
+								return true;
+							}
+						} catch (final NumberFormatException e) {
+							Log.w(TAG, e);
+						}
+					}
+				}
+				return false;
+			}
+		});
 		mBinding.list.setLayoutManager(new LinearLayoutManager(requireContext()));
 		mBinding.list.setEmptyView(mBinding.empty);
 		mBinding.list.setAdapter(mAdapter);
