f941a86a088c7dfb1720fab8b6c68f22fd4efa34
saki4510t <saki4510t@gmail.com>
2020-05-28 17:36:33 +0900

    ContextUtilsクラスを追加

---
+++ common/src/main/java/com/serenegiant/utils/ContextUtils.java
@@ -0,0 +1,51 @@
+package com.serenegiant.utils;
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
+
+/**
+ * Context関係のラッパークラス
+ */
+public class ContextUtils {
+	private ContextUtils() {
+		// インスタンス化をエラーにするためにデフォルトコンストラクタをｐrivateに
+	}
+
+	/**
+	 * ContextCompat.getSystemServiceを呼び出すだけ
+	 * (たぶんAPIレベルをチェックしていないときにnullになる)
+	 * @param context
+	 * @param serviceClass
+	 * @param <T>
+	 * @return
+	 */
+	@Nullable
+	public static <T> T getSystemService(@NonNull Context context, @NonNull Class<T> serviceClass) {
+		return ContextCompat.getSystemService(context, serviceClass);
+	}
+
+	/**
+	 * ContextCompat.getSystemServiceを呼び出して取得した結果がnullならIllegalArgumentExceptionを投げる
+	 * (たぶんAPIレベルをチェックしていないときにnullになる)
+	 * @param context
+	 * @param serviceClass
+	 * @param <T>
+	 * @return
+	 * @throws IllegalArgumentException
+	 */
+	@NonNull
+	public static <T> T requireSystemService(@NonNull Context context, @NonNull Class<T> serviceClass)
+		throws IllegalArgumentException {
+
+		final T result = ContextCompat.getSystemService(context, serviceClass);
+		if (result != null) {
+			return result;
+		} else {
+			throw new IllegalArgumentException();
+		}
+	}
+
+}
