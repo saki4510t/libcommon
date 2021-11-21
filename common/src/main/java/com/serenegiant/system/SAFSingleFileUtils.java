c53efb146ff21394b220bc21f66ab08db223c3a6
saki4510t@gmail.com <saki4510t@gmail.com>
2021-11-21 12:09:38 +0900

    registerForActivityResultとActivityResultCallbackを使うように変更

--- common/src/main/java/com/serenegiant/system/SAFSingleFileUtils.java
+++ common/src/main/java/com/serenegiant/system/SAFSingleFileUtils.java
@@ -8,10 +8,19 @@ import android.net.Uri;
 import android.os.Build;
 import android.provider.DocumentsContract;
 import android.text.TextUtils;
+import android.util.Log;
+import android.util.Pair;
 
 import java.io.FileNotFoundException;
 
+import androidx.activity.ComponentActivity;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
+import androidx.annotation.CallSuper;
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
 
@@ -21,37 +30,187 @@ import androidx.fragment.app.FragmentActivity;
  */
 @TargetApi(Build.VERSION_CODES.KITKAT)
 public class SAFSingleFileUtils {
+	private static final boolean DEBUG = false;	// set false on production
+	private static final String TAG = SAFSingleFileUtils.class.getSimpleName();
 
-	private SAFSingleFileUtils() {
-		// インスタンス化をエラーにするためにデフォルトオンストラクタをprivateに
+	private static final String EXTRA_KEY_REQUEST_ID = "SAFSingleFileUtils.EXTRA_KEY_REQUEST_ID";
+
+	@NonNull
+	private final ActivityResultLauncher<Intent> mLauncher;
+
+	/**
+	 * コンストラクタ
+	 * @param activity
+	 * @param callback
+	 */
+	@RequiresApi(api = Build.VERSION_CODES.KITKAT)
+	public SAFSingleFileUtils(
+		@NonNull final ComponentActivity activity,
+		@NonNull final SAFPermission.SAFCallback callback) {
+
+		mLauncher = activity.registerForActivityResult(new SingleDocument(),
+			new MyActivityResultCallback(callback));
+	}
+
+	/**
+	 * コンストラクタ
+	 * @param fragment
+	 * @param callback
+	 */
+	@RequiresApi(api = Build.VERSION_CODES.KITKAT)
+	public SAFSingleFileUtils(
+		@NonNull final Fragment fragment,
+		@NonNull final SAFPermission.SAFCallback callback) {
+
+		mLauncher = fragment.registerForActivityResult(new SingleDocument(),
+			new MyActivityResultCallback(callback));
+	}
+
+	/**
+	 * ファイル読み込み用のUriを要求
+	 * KITKAT以降で個別のファイル毎にパーミッション要求する場合
+	 * @param id
+	 * @param mimeType
+	 * @throws IllegalArgumentException
+	 */
+	public void requestOpen(final int id, @NonNull final String mimeType) throws IllegalArgumentException {
+		mLauncher.launch(prepareOpenDocumentIntent(id, mimeType));
+	}
+
+	/**
+	 * ファイル保存用のUriを要求
+	 * KITKAT以降で個別のファイル毎にパーミッション要求する場合
+	 * @param id
+	 * @param mimeType
+	 * @param defaultName
+	 * @throws IllegalArgumentException
+	 */
+	public void requestCreate(
+		final int id, @NonNull final String mimeType,
+		@Nullable final String defaultName) throws IllegalArgumentException {
+
+		mLauncher.launch(prepareCreateDocument(id, mimeType, defaultName));
+	}
+
+//--------------------------------------------------------------------------------
+	private static class MyActivityResultCallback implements ActivityResultCallback<Pair<Integer, Uri>> {
+		private static final String TAG = MyActivityResultCallback.class.getSimpleName();
+		@NonNull
+		final SAFPermission.SAFCallback callback;
+
+		private MyActivityResultCallback(
+			@NonNull final SAFPermission.SAFCallback callback) {
+
+			if (DEBUG) Log.v(TAG, "コンストラクタ:");
+			this.callback = callback;
+		}
+
+		@Override
+		public void onActivityResult(final Pair<Integer, Uri> result) {
+			if (DEBUG) Log.v(TAG, "onActivityResult:" + result);
+			if (result != null) {
+				final int requestCode = result.first;
+				final Uri uri = result.second;
+				if (uri != null) {
+					// SingleDocumentのuriを取得できた時
+					callback.onResult(requestCode, uri);
+				} else {
+					// SingleDocumentのuriを取得できなかったとき
+					callback.onFailed(requestCode);
+				}
+			} else {
+				// ActivityResultContractの実装上実際にはresultはnullにならないけど
+				callback.onFailed(0);
+			}
+		}
+	}
+
+	@TargetApi(19)
+	private static class SingleDocument extends ActivityResultContract<Intent, Pair<Integer, Uri>> {
+		private static final String TAG = SingleDocument.class.getSimpleName();
+
+		@Nullable
+		private Intent input;
+
+		@CallSuper
+		@NonNull
+		@Override
+		public Intent createIntent(@NonNull Context context, @Nullable Intent input) {
+			if (DEBUG) Log.v(TAG, "createIntent:" + input);
+			this.input = input;
+			final String action = input != null ? input.getAction() : null;	// 実装上はnullにならないはず
+			final String mimeType = input != null ? input.getType() : null;
+			final String defaultName = input != null ? input.getStringExtra(Intent.EXTRA_TITLE) : null;
+			final Intent intent = new Intent(action != null ? action : Intent.ACTION_OPEN_DOCUMENT);
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O && input != null) {
+				final Uri initialUri = input != null ? input.getData() : null;
+				intent.putExtra(DocumentsContract.EXTRA_INITIAL_URI, initialUri);
+			}
+			if (!TextUtils.isEmpty(mimeType)) {
+				intent.setType(mimeType);
+			}
+			if (!TextUtils.isEmpty(defaultName)) {
+				intent.putExtra(Intent.EXTRA_TITLE, defaultName);
+			}
+			return intent;
+		}
+
+		@Nullable
+		@Override
+		public final SynchronousResult<Pair<Integer, Uri>> getSynchronousResult(@NonNull Context context,
+				@Nullable Intent input) {
+			if (DEBUG) Log.v(TAG, "getSynchronousResult:" + input);
+			this.input = input;
+			return null;
+		}
+
+		@Nullable
+		@Override
+		public final Pair<Integer, Uri> parseResult(final int resultCode, @Nullable Intent intent) {
+			final Uri uri = ((intent!= null) && (resultCode == Activity.RESULT_OK)) ? intent.getData() : null;
+			final int id = input != null ? input.getIntExtra(EXTRA_KEY_REQUEST_ID, 0) : 0;
+			return Pair.create(id, uri);
+		}
+	}
+
+//--------------------------------------------------------------------------------
 	/**
 	 * ファイル読み込み用のUriを要求するヘルパーメソッド
 	 * KITKAT以降で個別のファイル毎にパーミッション要求する場合
-	 * @param mime
+	 * @param id
+	 * @param mimeType
 	 * @return
+	 * @throws IllegalArgumentException
 	 */
-	private static Intent prepareOpenDocumentIntent(@NonNull final String mime) {
-		final Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-		intent.setType(mime);
-		return intent;
+	private static Intent prepareOpenDocumentIntent(
+		final int id, @NonNull final String mimeType) throws IllegalArgumentException {
+		if (TextUtils.isEmpty(mimeType)) {
+			throw new IllegalArgumentException("mime type should not a null/empty");
+		}
+		return new Intent(Intent.ACTION_OPEN_DOCUMENT)
+			.setType(mimeType)
+			.putExtra(EXTRA_KEY_REQUEST_ID, id);
 	}
 
 	/**
 	 * ファイル保存用のUriを要求するヘルパーメソッド
 	 * KITKAT以降で個別のファイル毎にパーミッション要求する場合
-	 * @param mime
+	 * @param id
+	 * @param mimeType
 	 * @param defaultName
 	 * @return
+	 * @throws IllegalArgumentException
 	 */
 	private static Intent prepareCreateDocument(
-		final String mime, final String defaultName) {
+		final int id, @NonNull final String mimeType,
+		@Nullable final String defaultName) throws IllegalArgumentException {
 
-		final Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
-		intent.setType(mime);
-		if (!TextUtils.isEmpty(defaultName)) {
-			intent.putExtra(Intent.EXTRA_TITLE, defaultName);
+		if (TextUtils.isEmpty(mimeType)) {
+			throw new IllegalArgumentException("mime type should not a null/empty");
 		}
-		return intent;
+		return new Intent(Intent.ACTION_OPEN_DOCUMENT)
+			.setType(mimeType)
+			.putExtra(Intent.EXTRA_TITLE, defaultName);
 	}
 
 	/**
@@ -77,16 +236,17 @@ public class SAFSingleFileUtils {
 	 * ファイル読み込み用のUriを要求
 	 * KITKAT以降で個別のファイル毎にパーミッション要求する場合
 	 * @param activity
-	 * @param mime
+	 * @param mimeType
 	 * @param requestCode
+	 * @throws IllegalArgumentException
 	 */
 	@Deprecated
 	public static void requestOpenDocument(
 		@NonNull final Activity activity,
-		final String mime, final int requestCode) {
+		@NonNull final String mimeType, final int requestCode) throws IllegalArgumentException {
 
 		if (BuildCheck.isKitKat()) {
-			activity.startActivityForResult(prepareOpenDocumentIntent(mime), requestCode);
+			activity.startActivityForResult(prepareOpenDocumentIntent(requestCode, mimeType), requestCode);
 		}
 	}
 
@@ -94,16 +254,18 @@ public class SAFSingleFileUtils {
 	 * ファイル読み込み用のUriを要求
 	 * KITKAT以降で個別のファイル毎にパーミッション要求する場合
 	 * @param activity
-	 * @param mime
+	 * @param mimeType
 	 * @param requestCode
+	 * @throws IllegalArgumentException
 	 */
+	@SuppressWarnings("deprecation")
 	@Deprecated
 	public static void requestOpenDocument(
 		@NonNull final FragmentActivity activity,
-		final String mime, final int requestCode) {
+		@NonNull final String mimeType, final int requestCode) throws IllegalArgumentException {
 
 		if (BuildCheck.isKitKat()) {
-			activity.startActivityForResult(prepareOpenDocumentIntent(mime), requestCode);
+			activity.startActivityForResult(prepareOpenDocumentIntent(requestCode, mimeType), requestCode);
 		}
 	}
 
@@ -111,15 +273,16 @@ public class SAFSingleFileUtils {
 	 * ファイル読み込み用のUriを要求
 	 * KITKAT以降で個別のファイル毎にパーミッション要求する場合
 	 * @param fragment
-	 * @param mime
+	 * @param mimeType
 	 * @param requestCode
+	 * @throws IllegalArgumentException
 	 */
 	@Deprecated
 	public static void requestOpenDocument(@NonNull final android.app.Fragment fragment,
-		final String mime, final int requestCode) {
+		@NonNull final String mimeType, final int requestCode) throws IllegalArgumentException {
 
 		if (BuildCheck.isKitKat()) {
-			fragment.startActivityForResult(prepareOpenDocumentIntent(mime), requestCode);
+			fragment.startActivityForResult(prepareOpenDocumentIntent(requestCode, mimeType), requestCode);
 		}
 	}
 
@@ -127,16 +290,18 @@ public class SAFSingleFileUtils {
 	 * ファイル読み込み用のUriを要求
 	 * KITKAT以降で個別のファイル毎にパーミッション要求する場合
 	 * @param fragment
-	 * @param mime
+	 * @param mimeType
 	 * @param requestCode
+	 * @throws IllegalArgumentException
 	 */
+	@SuppressWarnings("deprecation")
 	@Deprecated
 	public static void requestOpenDocument(
 		@NonNull final Fragment fragment,
-		final String mime, final int requestCode) {
+		@NonNull final String mimeType, final int requestCode) throws IllegalArgumentException {
 
 		if (BuildCheck.isKitKat()) {
-			fragment.startActivityForResult(prepareOpenDocumentIntent(mime), requestCode);
+			fragment.startActivityForResult(prepareOpenDocumentIntent(requestCode, mimeType), requestCode);
 		}
 	}
 
@@ -144,16 +309,17 @@ public class SAFSingleFileUtils {
 	 * ファイル保存用のUriを要求
 	 * KITKAT以降で個別のファイル毎にパーミッション要求する場合
 	 * @param activity
-	 * @param mime
+	 * @param mimeType
 	 * @param requestCode
+	 * @throws IllegalArgumentException
 	 */
 	@Deprecated
 	public static void requestCreateDocument(
 		@NonNull final Activity activity,
-		final String mime, final int requestCode) {
+		@NonNull final String mimeType, final int requestCode) throws IllegalArgumentException {
 
 		if (BuildCheck.isKitKat()) {
-			activity.startActivityForResult(prepareCreateDocument(mime, null), requestCode);
+			activity.startActivityForResult(prepareCreateDocument(requestCode, mimeType, null), requestCode);
 		}
 	}
 
@@ -161,17 +327,18 @@ public class SAFSingleFileUtils {
 	 * ファイル保存用のUriを要求
 	 * KITKAT以降で個別のファイル毎にパーミッション要求する場合
 	 * @param activity
-	 * @param mime
+	 * @param mimeType
 	 * @param defaultName
 	 * @param requestCode
+	 * @throws IllegalArgumentException
 	 */
 	@Deprecated
 	public static void requestCreateDocument(
 		@NonNull final Activity activity,
-		final String mime, final String defaultName, final int requestCode) {
+		@NonNull final String mimeType, final String defaultName, final int requestCode) throws IllegalArgumentException {
 
 		if (BuildCheck.isKitKat()) {
-			activity.startActivityForResult(prepareCreateDocument(mime, defaultName), requestCode);
+			activity.startActivityForResult(prepareCreateDocument(requestCode, mimeType, defaultName), requestCode);
 		}
 	}
 
@@ -179,16 +346,18 @@ public class SAFSingleFileUtils {
 	 * ファイル保存用のUriを要求
 	 * KITKAT以降で個別のファイル毎にパーミッション要求する場合
 	 * @param activity
-	 * @param mime
+	 * @param mimeType
 	 * @param requestCode
+	 * @throws IllegalArgumentException
 	 */
+	@SuppressWarnings("deprecation")
 	@Deprecated
 	public static void requestCreateDocument(
 		@NonNull final FragmentActivity activity,
-		final String mime, final int requestCode) {
+		@NonNull final String mimeType, final int requestCode) throws IllegalArgumentException {
 
 		if (BuildCheck.isKitKat()) {
-			activity.startActivityForResult(prepareCreateDocument(mime, null), requestCode);
+			activity.startActivityForResult(prepareCreateDocument(requestCode, mimeType, null), requestCode);
 		}
 	}
 
@@ -196,17 +365,19 @@ public class SAFSingleFileUtils {
 	 * ファイル保存用のUriを要求
 	 * KITKAT以降で個別のファイル毎にパーミッション要求する場合
 	 * @param activity
-	 * @param mime
+	 * @param mimeType
 	 * @param defaultName
 	 * @param requestCode
+	 * @throws IllegalArgumentException
 	 */
+	@SuppressWarnings("deprecation")
 	@Deprecated
 	public static void requestCreateDocument(
 		@NonNull final FragmentActivity activity,
-		final String mime, final String defaultName, final int requestCode) {
+		@NonNull final String mimeType, final String defaultName, final int requestCode) throws IllegalArgumentException {
 
 		if (BuildCheck.isKitKat()) {
-			activity.startActivityForResult(prepareCreateDocument(mime, defaultName), requestCode);
+			activity.startActivityForResult(prepareCreateDocument(requestCode, mimeType, defaultName), requestCode);
 		}
 	}
 
@@ -214,16 +385,17 @@ public class SAFSingleFileUtils {
 	 * ファイル保存用のUriを要求
 	 * KITKAT以降で個別のファイル毎にパーミッション要求する場合
 	 * @param fragment
-	 * @param mime
+	 * @param mimeType
 	 * @param requestCode
+	 * @throws IllegalArgumentException
 	 */
 	@Deprecated
 	public static void requestCreateDocument(
 		@NonNull final android.app.Fragment fragment,
-		final String mime, final int requestCode) {
+		@NonNull final String mimeType, final int requestCode) throws IllegalArgumentException {
 
 		if (BuildCheck.isKitKat()) {
-			fragment.startActivityForResult(prepareCreateDocument(mime, null), requestCode);
+			fragment.startActivityForResult(prepareCreateDocument(requestCode, mimeType, null), requestCode);
 		}
 	}
 
@@ -231,17 +403,18 @@ public class SAFSingleFileUtils {
 	 * ファイル保存用のUriを要求
 	 * KITKAT以降で個別のファイル毎にパーミッション要求する場合
 	 * @param fragment
-	 * @param mime
+	 * @param mimeType
 	 * @param defaultName
 	 * @param requestCode
+	 * @throws IllegalArgumentException
 	 */
 	@Deprecated
 	public static void requestCreateDocument(
 		@NonNull final android.app.Fragment fragment,
-		final String mime, final String defaultName, final int requestCode) {
+		@NonNull final String mimeType, final String defaultName, final int requestCode) throws IllegalArgumentException {
 
 		if (BuildCheck.isKitKat()) {
-			fragment.startActivityForResult(prepareCreateDocument(mime, defaultName), requestCode);
+			fragment.startActivityForResult(prepareCreateDocument(requestCode, mimeType, defaultName), requestCode);
 		}
 	}
 
@@ -249,16 +422,18 @@ public class SAFSingleFileUtils {
 	 * ファイル保存用のUriを要求
 	 * KITKAT以降で個別のファイル毎にパーミッション要求する場合
 	 * @param fragment
-	 * @param mime
+	 * @param mimeType
 	 * @param requestCode
+	 * @throws IllegalArgumentException
 	 */
+	@SuppressWarnings("deprecation")
 	@Deprecated
 	public static void requestCreateDocument(
 		@NonNull final Fragment fragment,
-		final String mime, final int requestCode) {
+		@NonNull final String mimeType, final int requestCode) throws IllegalArgumentException {
 
 		if (BuildCheck.isKitKat()) {
-			fragment.startActivityForResult(prepareCreateDocument(mime, null), requestCode);
+			fragment.startActivityForResult(prepareCreateDocument(requestCode, mimeType, null), requestCode);
 		}
 	}
 
@@ -266,17 +441,19 @@ public class SAFSingleFileUtils {
 	 * ファイル保存用のUriを要求
 	 * KITKAT以降で個別のファイル毎にパーミッション要求する場合
  	 * @param fragment
-	 * @param mime
+	 * @param mimeType
 	 * @param defaultName
 	 * @param requestCode
+	 * @throws IllegalArgumentException
 	 */
+	@SuppressWarnings("deprecation")
 	@Deprecated
 	public static void requestCreateDocument(
 		@NonNull final Fragment fragment,
-		final String mime, final String defaultName, final int requestCode) {
+		@NonNull final String mimeType, final String defaultName, final int requestCode) throws IllegalArgumentException {
 
 		if (BuildCheck.isKitKat()) {
-			fragment.startActivityForResult(prepareCreateDocument(mime, defaultName), requestCode);
+			fragment.startActivityForResult(prepareCreateDocument(requestCode, mimeType, defaultName), requestCode);
 		}
 	}
 
