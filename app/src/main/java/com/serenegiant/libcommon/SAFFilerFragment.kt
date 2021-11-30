fa71566109646834776f1dfa790843405039c2da
saki4510t@gmail.com <saki4510t@gmail.com>
2021-11-30 17:37:10 +0900

    kotlinへ変換

---
+++ app/src/main/java/com/serenegiant/libcommon/SAFFilerFragment.kt
@@ -0,0 +1,123 @@
+package com.serenegiant.libcommon
+
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.databinding.DataBindingUtil
+import androidx.documentfile.provider.DocumentFile
+import androidx.documentfile.provider.SAFRootTreeDocumentFile
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.serenegiant.libcommon.databinding.FragmentSafFilerBinding
+import com.serenegiant.widget.DocumentTreeRecyclerAdapter
+import com.serenegiant.widget.DocumentTreeRecyclerAdapter.DocumentTreeRecyclerAdapterListener
+import java.io.IOException
+
+/*
+* libcommon
+* utility/helper classes for myself
+*
+* Copyright (c) 2014-2021 saki t_saki@serenegiant.com
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+*  You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*  distributed under the License is distributed on an "AS IS" BASIS,
+*  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*  See the License for the specific language governing permissions and
+*  limitations under the License.
+*/ /**
+ * DocumentTreeRecyclerAdapterとSAFRootTreeDocumentFileのテスト用Fragment
+ */
+@RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+class SAFFilerFragment
+/**
+ * デフォルトコンストラクタ
+ */
+	: BaseFragment() {
+	private var mBinding: FragmentSafFilerBinding? = null
+	private var mAdapter: DocumentTreeRecyclerAdapter? = null
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		if (DEBUG) Log.v(TAG, "onCreateView:")
+		mBinding = DataBindingUtil.inflate(
+			inflater,
+			R.layout.fragment_saf_filer, container, false
+		)
+		return mBinding!!.root
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		if (DEBUG) Log.v(TAG, "onViewCreated:")
+		initView()
+	}
+
+	override fun internalOnResume() {
+		super.internalOnResume()
+		if (DEBUG) Log.v(TAG, "internalOnResume:")
+	}
+
+	override fun internalOnPause() {
+		if (DEBUG) Log.v(TAG, "internalOnPause:")
+		super.internalOnPause()
+	}
+	//--------------------------------------------------------------------------------
+	/**
+	 * 表示を初期化
+	 */
+	private fun initView() {
+		mAdapter = DocumentTreeRecyclerAdapter(
+			requireContext(),
+			R.layout.list_item_title,
+			SAFRootTreeDocumentFile.fromContext(requireContext())
+		)
+		mAdapter!!.setListener(
+			object : DocumentTreeRecyclerAdapterListener {
+				override fun onItemClick(
+					parent: RecyclerView.Adapter<*>,
+					view: View,
+					item: DocumentFile
+				) {
+					if (DEBUG) Log.v(TAG, "onItemClick:$item")
+					if (item.isDirectory) {
+						try {
+							mAdapter!!.changeDir(item)
+						} catch (e: IOException) {
+							Log.w(TAG, e)
+						}
+//					} else {
+//						// open specific file
+					}
+				}
+
+				override fun onItemLongClick(
+					parent: RecyclerView.Adapter<*>,
+					view: View,
+					item: DocumentFile
+				): Boolean {
+					if (DEBUG) Log.v(TAG, "onItemLongClick:$item")
+					// show popup menu?
+					return false
+				}
+			})
+		mBinding!!.list.layoutManager = LinearLayoutManager(requireContext())
+		mBinding!!.list.adapter = mAdapter
+	}
+
+	companion object {
+		private const val DEBUG = true // set false on production
+		private val TAG = SAFFilerFragment::class.java.simpleName
+	}
+}
---
+++ app/src/main/java/com/serenegiant/libcommon/SettingsLinkFragment.kt
@@ -0,0 +1,54 @@
+package com.serenegiant.libcommon
+
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.provider.Settings
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.serenegiant.libcommon.databinding.FragmentSettingsLinkBinding
+
+/**
+ * ユーザーがアプリに権限付与をしなかったために機能を実行できない時に
+ * 端末のアプリ設定画面への繊維ボタンを表示するFragment
+ */
+class SettingsLinkFragment : Fragment() {
+	private var mBinding: FragmentSettingsLinkBinding? = null
+	override fun onCreateView(
+		inflater: LayoutInflater,
+		container: ViewGroup?,
+		savedInstanceState: Bundle?
+	): View {
+		mBinding = DataBindingUtil.inflate(
+			inflater,
+			R.layout.fragment_settings_link, container, false
+		)
+		return mBinding!!.root
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+		init()
+	}
+
+	private fun init() {
+		mBinding!!.onClickListener = View.OnClickListener { v -> // 端末のアプリ設定を開く
+			val context = v.context
+			val uriString = "package:" + context.packageName
+			val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS, Uri.parse(uriString))
+			context.startActivity(intent)
+		}
+	}
+
+	companion object {
+		fun newInstance(): SettingsLinkFragment {
+			val fragment = SettingsLinkFragment()
+			val args = Bundle()
+			fragment.arguments = args
+			return fragment
+		}
+	}
+}
