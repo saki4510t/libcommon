af5758299ec397ace70a2814bdc5a73beb19c1e0
saki4510t@gmail.com <saki4510t@gmail.com>
2022-05-15 16:08:29 +0900

    GLTextureの処理ではGLES20とGLES30を区別する必要がないのでes2/es3下に置いていたのを削除してglutilsパッケージ直下に移動

--- common/src/main/java/com/serenegiant/glutils/GLTexture.java
+++ common/src/main/java/com/serenegiant/glutils/GLTexture.java
@@ -1,4 +1,4 @@
-package com.serenegiant.glutils.es2;
+package com.serenegiant.glutils;
 /*
  * libcommon
  * utility/helper classes for myself
@@ -20,37 +20,39 @@ package com.serenegiant.glutils.es2;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Canvas;
 import android.opengl.GLES20;
 import android.opengl.GLUtils;
 import android.opengl.Matrix;
+import android.util.Log;
 
-import com.serenegiant.glutils.IGLSurface;
+import com.serenegiant.glutils.es2.GLHelper;
 
 import java.io.IOException;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Size;
 
-import static com.serenegiant.glutils.ShaderConst.GL_TEXTURE_EXTERNAL_OES;
+import static com.serenegiant.glutils.ShaderConst.*;
 
 /**
  * OpenGL|ESのテクスチャ操作用のヘルパークラス
  */
 public class GLTexture implements IGLSurface {
-//	private static final boolean DEBUG = false;	// FIXME 実働時はfalseにすること
-//	private static final String TAG = "GLTexture";
+	private static final boolean DEBUG = false;	// FIXME 実働時はfalseにすること
+	private static final String TAG = GLTexture.class.getSimpleName();
 
 	private static final boolean DEFAULT_ADJUST_POWER2 = false;
 
-	/* package */final int mTextureTarget;
-	/* package */final int mTextureUnit ;
-	/* package */int mTextureId;
+	private final int TEX_TARGET;
+	private final int TEX_UNIT;
+	private final int FILTER_PARAM;
+	private final boolean ADJUST_POWER2;
+	private int mTextureId;
 	@Size(min=16)
 	@NonNull
-	/* package */final float[] mTexMatrix = new float[16];	// テクスチャ変換行列
-	/* package */int mTexWidth, mTexHeight;
-	/* package */int mImageWidth, mImageHeight;
+	private final float[] mTexMatrix = new float[16];	// テクスチャ変換行列
+	private int mTexWidth, mTexHeight;
+	private int mWidth, mHeight;
 	private int viewPortX, viewPortY, viewPortWidth, viewPortHeight;
 
 	/**
@@ -105,40 +107,11 @@ public class GLTexture implements IGLSurface {
 		final boolean adjust_power2,
 		final int filter_param) {
 //		if (DEBUG) Log.v(TAG, String.format("コンストラクタ(%d,%d)", width, height));
-		mTextureTarget = texTarget;
-		mTextureUnit = texUnit;
-		if (adjust_power2) {
-			// テクスチャのサイズは2の乗数にするとき
-			int w = 1;
-			for (; w < width; w <<= 1) ;
-			int h = 1;
-			for (; h < height; h <<= 1) ;
-			if (mTexWidth != w || mTexHeight != h) {
-				mTexWidth = w;
-				mTexHeight = h;
-			}
-		} else {
-			mTexWidth = width;
-			mTexHeight = height;
-		}
-		mImageWidth = width;
-		mImageHeight = height;
-//		if (DEBUG) Log.v(TAG, String.format("texSize(%d,%d)", mTexWidth, mTexHeight));
-		mTextureId = GLHelper.initTex(mTextureTarget, texUnit, filter_param);
-		// テクスチャのメモリ領域を確保する
-		GLES20.glTexImage2D(mTextureTarget,
-			0,					// ミップマップレベル0(ミップマップしない)
-			GLES20.GL_RGBA,				// 内部フォーマット
-			mTexWidth, mTexHeight,		// サイズ
-			0,					// 境界幅
-			GLES20.GL_RGBA,				// 引き渡すデータのフォーマット
-			GLES20.GL_UNSIGNED_BYTE,	// データの型
-			null);				// ピクセルデータ無し
-		// テクスチャ変換行列を初期化
-		Matrix.setIdentityM(mTexMatrix, 0);
-		mTexMatrix[0] = width / (float)mTexWidth;
-		mTexMatrix[5] = height / (float)mTexHeight;
-		setViewPort(0, 0, mImageWidth, mImageHeight);
+		TEX_TARGET = texTarget;
+		TEX_UNIT = texUnit;
+		FILTER_PARAM = filter_param;
+		ADJUST_POWER2 = adjust_power2;
+		createTexture(width, height);
 //		if (DEBUG) Log.v(TAG, "GLTexture:id=" + mTextureId);
 	}
 
@@ -158,10 +131,7 @@ public class GLTexture implements IGLSurface {
 	@Override
 	public void release() {
 //		if (DEBUG) Log.v(TAG, "release:");
-		if (mTextureId >= 0) {
-			GLHelper.deleteTex(mTextureId);
-			mTextureId = -1;
-		}
+		releaseTexture();
 	}
 
 	/**
@@ -171,8 +141,8 @@ public class GLTexture implements IGLSurface {
 	@Override
 	public void makeCurrent() {
 //		if (DEBUG) Log.v(TAG, "makeCurrent:");
-		GLES20.glActiveTexture(mTextureUnit);	// テクスチャユニットを選択
-		GLES20.glBindTexture(mTextureTarget, mTextureId);
+		GLES20.glActiveTexture(TEX_UNIT);	// テクスチャユニットを選択
+		GLES20.glBindTexture(TEX_TARGET, mTextureId);
 		setViewPort(viewPortX, viewPortY, viewPortWidth, viewPortHeight);
 	}
 
@@ -181,8 +151,8 @@ public class GLTexture implements IGLSurface {
 	 * (ビューポートの設定はしない)
 	 */
 	public void bindTexture() {
-		GLES20.glActiveTexture(mTextureUnit);
-		GLES20.glBindTexture(mTextureTarget, mTextureId);
+		GLES20.glActiveTexture(TEX_UNIT);
+		GLES20.glBindTexture(TEX_TARGET, mTextureId);
 	}
 
 	/**
@@ -209,8 +179,8 @@ public class GLTexture implements IGLSurface {
 	@Override
 	public void swap() {
 //		if (DEBUG) Log.v(TAG, "swap:");
-		GLES20.glActiveTexture(mTextureUnit);	// テクスチャユニットを選択
-		GLES20.glBindTexture(mTextureTarget, 0);
+		GLES20.glActiveTexture(TEX_UNIT);	// テクスチャユニットを選択
+		GLES20.glBindTexture(TEX_TARGET, 0);
 	}
 
 	@Override
@@ -224,7 +194,7 @@ public class GLTexture implements IGLSurface {
 	 */
 	@Override
 	public boolean isOES() {
-		return mTextureTarget == GL_TEXTURE_EXTERNAL_OES;
+		return TEX_TARGET == GL_TEXTURE_EXTERNAL_OES;
 	}
 
 	/**
@@ -233,12 +203,12 @@ public class GLTexture implements IGLSurface {
 	 */
 	@Override
 	public int getTexTarget() {
-		return mTextureTarget;
+		return TEX_TARGET;
 	}
 
 	@Override
 	public int getTexUnit() {
-		return mTextureUnit;
+		return TEX_UNIT;
 	}
 
 	/**
@@ -356,21 +326,67 @@ public class GLTexture implements IGLSurface {
 	 */
 	@Override
 	public void loadBitmap(@NonNull final Bitmap bitmap) {
-		mImageWidth = bitmap.getWidth();	// 読み込んだイメージのサイズを取得
-		mImageHeight = bitmap.getHeight();
-		Bitmap texture = Bitmap.createBitmap(mTexWidth, mTexHeight, Bitmap.Config.ARGB_8888);
-		final Canvas canvas = new Canvas(texture);
-		canvas.drawBitmap(bitmap, 0, 0, null);
-		bitmap.recycle();
-		// テクスチャ座標変換行列を設定(読み込んだイメージサイズがテクスチャサイズにフィットするようにスケール変換)
+		final int width = bitmap.getWidth();
+		final int height = bitmap.getHeight();
+		if ((width > mTexWidth) || (height > mTexHeight)) {
+			mWidth = width;
+			mHeight = height;
+			releaseTexture();
+			createTexture(width, height);
+		}
+		bindTexture();
+		GLUtils.texImage2D(TEX_TARGET, 0, bitmap, 0);
+		GLES20.glBindTexture(TEX_TARGET, 0);
+		// initialize texture matrix
 		Matrix.setIdentityM(mTexMatrix, 0);
-		mTexMatrix[0] = mImageWidth / (float)mTexWidth;
-		mTexMatrix[5] = mImageHeight / (float)mTexHeight;
-//		if (DEBUG) Log.v(TAG, String.format("image(%d,%d),scale(%f,%f)",
-// 			mImageWidth, mImageHeight, mMvpMatrix[0], mMvpMatrix[5]));
-		makeCurrent();
-		GLUtils.texImage2D(mTextureTarget, 0, texture, 0);
-		swap();
-		texture.recycle();
+		mTexMatrix[0] = width / (float)mTexWidth;
+		mTexMatrix[5] = height / (float)mTexHeight;
+		if (DEBUG) Log.v(TAG, String.format("image(%d,%d),scale(%f,%f)",
+			mWidth, mHeight, mTexMatrix[0], mTexMatrix[5]));
 	}
+
+	private void createTexture(final int width, final int height) {
+//		if (DEBUG) Log.v(TAG, String.format("texSize(%d,%d)", mTexWidth, mTexHeight));
+		if (ADJUST_POWER2) {
+			// テクスチャのサイズは2の乗数にするとき
+			int w = 1;
+			for (; w < width; w <<= 1) ;
+			int h = 1;
+			for (; h < height; h <<= 1) ;
+			if (mTexWidth != w || mTexHeight != h) {
+				mTexWidth = w;
+				mTexHeight = h;
+			}
+		} else {
+			mTexWidth = width;
+			mTexHeight = height;
+		}
+		mWidth = width;
+		mHeight = height;
+		mTextureId = GLHelper.initTex(TEX_TARGET, TEX_UNIT, FILTER_PARAM);
+		// テクスチャのメモリ領域を確保する
+		GLES20.glTexImage2D(TEX_TARGET,
+			0,					// ミップマップレベル0(ミップマップしない)
+			GLES20.GL_RGBA,				// 内部フォーマット
+			mTexWidth, mTexHeight,		// サイズ
+			0,					// 境界幅
+			GLES20.GL_RGBA,				// 引き渡すデータのフォーマット
+			GLES20.GL_UNSIGNED_BYTE,	// データの型
+			null);				// ピクセルデータ無し
+		// テクスチャ変換行列を初期化
+		Matrix.setIdentityM(mTexMatrix, 0);
+		mTexMatrix[0] = mWidth / (float)mTexWidth;
+		mTexMatrix[5] = mHeight / (float)mTexHeight;
+		if (DEBUG) Log.v(TAG, String.format("image(%d,%d),scale(%f,%f)",
+			mWidth, mHeight, mTexMatrix[0], mTexMatrix[5]));
+		setViewPort(0, 0, mWidth, mHeight);
+	}
+
+	private void releaseTexture() {
+		if (mTextureId >= 0) {
+			GLHelper.deleteTex(mTextureId);
+			mTextureId = NO_TEXTURE;
+		}
+	}
+
 }
