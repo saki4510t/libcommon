ebc52c46d41bf77cbcb391c32e2c2d80e616c468
saki4510t@gmail.com <saki4510t@gmail.com>
2022-05-17 15:27:23 +0900

    テクスチャターゲットとテクスチャユニットの指定用にアノテーションを作成

--- common/src/main/java/com/serenegiant/glutils/GLConst.java
+++ common/src/main/java/com/serenegiant/glutils/GLConst.java
@@ -18,6 +18,15 @@ package com.serenegiant.glutils;
  *  limitations under the License.
 */
 
+import android.opengl.GLES11Ext;
+import android.opengl.GLES20;
+import android.opengl.GLES30;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import androidx.annotation.IntDef;
+
 public interface GLConst {
 	public static final int GL_TEXTURE_EXTERNAL_OES	= 0x8D65;
 	public static final int GL_TEXTURE_2D           = 0x0DE1;
@@ -33,4 +42,51 @@ public interface GLConst {
 	 * 無効なシェーダープログラム名/シェーダープログラムID
 	 */
 	public  static final int GL_NO_PROGRAM = -1;
+
+	@IntDef({
+		GL_TEXTURE_EXTERNAL_OES,
+		GLES11Ext.GL_TEXTURE_EXTERNAL_OES,
+		GL_TEXTURE_2D,
+		GLES20.GL_TEXTURE_2D,
+		GLES30.GL_TEXTURE_2D,
+	})
+	@Retention(RetentionPolicy.SOURCE)
+	public @interface TexTarget {}
+
+	@IntDef({
+		GLES20.GL_TEXTURE0,
+		GLES20.GL_TEXTURE1,
+		GLES20.GL_TEXTURE2,
+		GLES20.GL_TEXTURE3,
+		GLES20.GL_TEXTURE4,
+		GLES20.GL_TEXTURE5,
+		GLES20.GL_TEXTURE6,
+		GLES20.GL_TEXTURE7,
+		GLES20.GL_TEXTURE8,
+		GLES20.GL_TEXTURE9,
+		GLES20.GL_TEXTURE10,
+		GLES20.GL_TEXTURE11,
+		GLES20.GL_TEXTURE12,
+		GLES20.GL_TEXTURE13,
+		GLES20.GL_TEXTURE14,
+		GLES20.GL_TEXTURE15,
+		GLES20.GL_TEXTURE16,
+		GLES20.GL_TEXTURE17,
+		GLES20.GL_TEXTURE18,
+		GLES20.GL_TEXTURE19,
+		GLES20.GL_TEXTURE20,
+		GLES20.GL_TEXTURE21,
+		GLES20.GL_TEXTURE22,
+		GLES20.GL_TEXTURE23,
+		GLES20.GL_TEXTURE24,
+		GLES20.GL_TEXTURE25,
+		GLES20.GL_TEXTURE26,
+		GLES20.GL_TEXTURE27,
+		GLES20.GL_TEXTURE28,
+		GLES20.GL_TEXTURE29,
+		GLES20.GL_TEXTURE30,
+		GLES20.GL_TEXTURE31,
+	})
+	@Retention(RetentionPolicy.SOURCE)
+	public @interface TexUnit {}
 }
