42f73eab50b7241f41704b8c1c22e5b381b465c8
saki4510t@gmail.com <saki4510t@gmail.com>
2022-05-17 10:48:23 +0900

    入れ替えしやすいように最後の要素の後ろにもカンマを追加

--- common/src/main/java/com/serenegiant/glutils/GLDrawer2D.java
+++ common/src/main/java/com/serenegiant/glutils/GLDrawer2D.java
@@ -51,13 +51,13 @@ public abstract class GLDrawer2D {
 		1.0f, 1.0f,		// 右上
 		-1.0f, 1.0f,	// 左上
 		1.0f, -1.0f,	// 右下
-		-1.0f, -1.0f	// 左下
+		-1.0f, -1.0f,	// 左下
 	};
 	protected static final float[] DEFAULT_TEXCOORD = {
 		1.0f, 1.0f,		// 右上
 		0.0f, 1.0f,		// 左上
 		1.0f, 0.0f,		// 右下
-		0.0f, 0.0f		// 左下
+		0.0f, 0.0f,		// 左下
 	};
 	protected static final int FLOAT_SZ = Float.SIZE / 8;
 
