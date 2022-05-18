9b794417ac00025bfc33d259700759831d84f50b
saki4510t@gmail.com <saki4510t@gmail.com>
2022-05-18 21:25:39 +0900

    StaticTextureSourceとSurfaceReaderのテストを追加

---
+++ common/src/androidTest/java/com/serenegiant/common/SurfaceReaderTest.java
@@ -0,0 +1,127 @@
+package com.serenegiant.common;
+/*
+ * libcommon
+ * utility/helper classes for myself
+ *
+ * Copyright (c) 2014-2022 saki t_saki@serenegiant.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+*/
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.view.Surface;
+
+import com.serenegiant.glutils.SurfaceReader;
+import com.serenegiant.graphics.BitmapHelper;
+import com.serenegiant.utils.HandlerThreadHandler;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+import androidx.annotation.NonNull;
+import androidx.test.core.app.ApplicationProvider;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import static com.serenegiant.common.TestUtils.bitMapEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(AndroidJUnit4.class)
+public class SurfaceReaderTest {
+	private static final String TAG = SurfaceReaderTest.class.getSimpleName();
+
+	private static final int WIDTH = 100;
+	private static final int HEIGHT = 100;
+
+	@Before
+	public void prepare() {
+		final Context context = ApplicationProvider.getApplicationContext();
+	}
+
+	@After
+	public void cleanUp() {
+		final Context context = ApplicationProvider.getApplicationContext();
+	}
+
+	@Test
+	public void surfaceReader() {
+		final Bitmap original = BitmapHelper.makeCheckBitmap(
+			WIDTH, HEIGHT, 15, 12, Bitmap.Config.ARGB_8888);
+//		dump(bitmap);
+
+		final Semaphore sem = new Semaphore(0);
+		final AtomicInteger cnt = new AtomicInteger();
+		final SurfaceReader reader = new SurfaceReader(WIDTH, HEIGHT, Bitmap.Config.ARGB_8888, 2);
+		final AtomicReference<Bitmap> result = new AtomicReference<>();
+		reader.setOnImageAvailableListener(new SurfaceReader.OnImageAvailableListener() {
+			@Override
+			public void onImageAvailable(@NonNull final SurfaceReader reader) {
+				final Bitmap bitmap = reader.acquireLatestImage();
+				if (bitmap != null) {
+					try {
+						if (cnt.incrementAndGet() >= 5) {
+							if (sem.availablePermits() == 0) {
+								result.set(Bitmap.createBitmap(bitmap));
+								sem.release();
+							}
+						}
+					} finally {
+						reader.recycle(bitmap);
+					}
+				}
+			}
+		}, HandlerThreadHandler.createHandler());
+
+		final Surface surface = reader.getSurface();
+		assertNotNull(surface);
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				final Rect inOutDirty = new Rect();
+				for (int i = 0; i < 10; i++) {
+					final Canvas canvas = surface.lockCanvas(inOutDirty);
+					if (canvas != null) {
+						try {
+							canvas.drawBitmap(original, 0, 0, null);
+							Thread.sleep(10);
+						} catch (InterruptedException e) {
+							e.printStackTrace();
+						} finally {
+							surface.unlockCanvasAndPost(canvas);
+						}
+					}
+				}
+			}
+		}).start();
+		try {
+			assertTrue(sem.tryAcquire(1000, TimeUnit.MILLISECONDS));
+			final Bitmap b = result.get();
+//			dump(b);
+			assertNotNull(b);
+			// 元のビットマップと同じかどうかを検証
+			assertTrue(bitMapEquals(original, b));
+		} catch (final InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
