f88a75e2ab861aea65ad511c991068f15386d6d7
saki4510t@gmail.com <saki4510t@gmail.com>
2022-05-23 14:37:50 +0900

    OpenGLInfoが呼び出し元のスレッド上でEGL|GLコンテキストを生成したのを止めてワーカースレッド上で実行するように変更

--- common/src/main/java/com/serenegiant/gl/OpenGLInfo.java
+++ common/src/main/java/com/serenegiant/gl/OpenGLInfo.java
@@ -21,6 +21,8 @@ package com.serenegiant.gl;
 import java.nio.IntBuffer;
 import java.util.Arrays;
 import java.util.Locale;
+import java.util.concurrent.Semaphore;
+import java.util.concurrent.TimeUnit;
 
 import javax.microedition.khronos.egl.EGL10;
 
@@ -44,134 +46,146 @@ public class OpenGLInfo {
 	private static final int EGL_CLIENT_APIS                    = 0x308D;
 
 	/**
-	 * 現在のスレッド上にGLコンテキストを生成してアクセスするのですでにGLコンテキストが存在するスレッド上で実行するときは注意
+	 * OpenGL|ESとEGLの情報取得をJSONObjectとして取得する
 	 * @return
 	 * @throws JSONException
 	 */
 	public static JSONObject get() throws JSONException {
-		JSONObject result = new JSONObject();
+		final JSONObject result = new JSONObject();
 		try {
-			// FIXME GLContextに変える
-	    	final EGLBase egl = EGLBase.createFrom(3, null, false, 0, false);
-	    	final EGLBase.IEglSurface dummy = egl.createOffscreen(1, 1);
-	    	try {
-		    	final IntBuffer val = IntBuffer.allocate(2);
-		    	// GLコンテキストの情報を取得
-		    	JSONObject glinfo = new JSONObject();
-		    	try {
-		    		glinfo.put("GL_VENDOR", GLES20.glGetString(GLES20.GL_VENDOR));
-		    	} catch (Exception e) {
-		    		glinfo.put("GL_VENDOR", e.getMessage());
-		    	}
-		    	try {
-		    		glinfo.put("GL_VERSION", GLES20.glGetString(GLES20.GL_VERSION));
-		    	} catch (Exception e) {
-		    		glinfo.put("GL_VERSION", e.getMessage());
-		    	}
-		    	try {
-			    	glinfo.put("GL_RENDERER", GLES20.glGetString(GLES20.GL_RENDERER));
-		    	} catch (Exception e) {
-		    		glinfo.put("GL_RENDERER", e.getMessage());
-		    	}
-		    	try {
-			        GLES20.glGetIntegerv(GLES20.GL_MAX_VERTEX_ATTRIBS, val);
-			        glinfo.put("GL_MAX_VERTEX_ATTRIBS", val.get(0));
-		    	} catch (Exception e) {
-		    		glinfo.put("GL_MAX_VERTEX_ATTRIBS", e.getMessage());
-		    	}
-		    	try {
-			        GLES20.glGetIntegerv(GLES20.GL_MAX_VERTEX_UNIFORM_VECTORS, val);
-			        glinfo.put("GL_MAX_VERTEX_UNIFORM_VECTORS", val.get(0));
-		    	} catch (Exception e) {
-		    		glinfo.put("GL_MAX_VERTEX_UNIFORM_VECTORS", e.getMessage());
-		    	}
-		    	try {
-			        GLES20.glGetIntegerv(GLES20.GL_MAX_VARYING_VECTORS, val);
-			        glinfo.put("GL_MAX_VARYING_VECTORS", val.get(0));
-		    	} catch (Exception e) {
-		    		glinfo.put("GL_MAX_VARYING_VECTORS", e.getMessage());
-		    	}
-		    	try {
-			        GLES20.glGetIntegerv(GLES20.GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS, val);
-			        glinfo.put("GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS", val.get(0));
-		    	} catch (Exception e) {
-		    		glinfo.put("GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS", e.getMessage());
-		    	}
-		    	try {
-			        GLES20.glGetIntegerv(GLES20.GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS, val);
-			        glinfo.put("GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS", val.get(0));
-		    	} catch (Exception e) {
-		    		glinfo.put("GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS", e.getMessage());
-		    	}
-		    	try {
-			        GLES20.glGetIntegerv(GLES20.GL_MAX_TEXTURE_IMAGE_UNITS, val);
-			        glinfo.put("GL_MAX_TEXTURE_IMAGE_UNITS", val.get(0));
-		    	} catch (Exception e) {
-		    		glinfo.put("GL_MAX_TEXTURE_IMAGE_UNITS", e.getMessage());
-		    	}
-		    	try {
-			        GLES20.glGetIntegerv(GLES20.GL_MAX_FRAGMENT_UNIFORM_VECTORS, val);
-			        glinfo.put("GL_MAX_FRAGMENT_UNIFORM_VECTORS", val.get(0));
-		    	} catch (Exception e) {
-		    		glinfo.put("GL_MAX_FRAGMENT_UNIFORM_VECTORS", e.getMessage());
-		    	}
-		    	try {
-			        GLES20.glGetIntegerv(GLES20.GL_MAX_CUBE_MAP_TEXTURE_SIZE, val);
-			        glinfo.put("GL_MAX_CUBE_MAP_TEXTURE_SIZE", val.get(0));
-		    	} catch (Exception e) {
-		    		glinfo.put("GL_MAX_CUBE_MAP_TEXTURE_SIZE", e.getMessage());
-		    	}
-		    	try {
-			        GLES20.glGetIntegerv(GLES20.GL_MAX_RENDERBUFFER_SIZE, val);
-			        glinfo.put("GL_MAX_RENDERBUFFER_SIZE", val.get(0));
-		    	} catch (Exception e) {
-		    		glinfo.put("GL_MAX_RENDERBUFFER_SIZE", e.getMessage());
-		    	}
-		    	try {
-			        GLES20.glGetIntegerv(GLES20.GL_MAX_TEXTURE_SIZE, val);
-			        glinfo.put("GL_MAX_TEXTURE_SIZE", val.get(0));
-		    	} catch (Exception e) {
-		    		glinfo.put("GL_MAX_TEXTURE_SIZE", e.getMessage());
-		    	}
-		    	try {
-			        GLES20.glGetIntegerv(GLES20.GL_MAX_VIEWPORT_DIMS, val);
-			        glinfo.put("GL_MAX_VIEWPORT_DIMS", String.format(Locale.US, "%d x %d", val.get(0), val.get(1)));
-		    	} catch (Exception e) {
-		    		glinfo.put("GL_MAX_VIEWPORT_DIMS", e.getMessage());
-		    	}
-		    	try {
-			        glinfo.put("GL_EXTENSIONS", formatExtensions(GLES20.glGetString(GLES20.GL_EXTENSIONS)));
-		    	} catch (Exception e) {
-		    		glinfo.put("GL_EXTENSIONS", e.getMessage());
-		    	}
-		        result.put("GL_INFO", glinfo);
-		        // EGLの情報取得
-		    	JSONObject eglinfo = new JSONObject();
-		    	try {
-		    		eglinfo.put("EGL_VENDOR", egl.queryString(EGL10.EGL_VENDOR));
-		    	} catch (Exception e) {
-		    		glinfo.put("EGL_VENDOR", e.getMessage());
-		    	}
-		    	try {
-		    		eglinfo.put("EGL_VERSION", egl.queryString(EGL10.EGL_VERSION));
-		    	} catch (Exception e) {
-		    		glinfo.put("EGL_VERSION", e.getMessage());
-		    	}
-		    	try {
-		    		eglinfo.put("EGL_CLIENT_APIS", egl.queryString(EGL_CLIENT_APIS));
-		    	} catch (Exception e) {
-		    		glinfo.put("EGL_CLIENT_APIS", e.getMessage());
-		    	}
-		    	try {
-		    		eglinfo.put("EGL_EXTENSIONS:", formatExtensions(egl.queryString(EGL10.EGL_EXTENSIONS)));
-		    	} catch (Exception e) {
-		    		glinfo.put("EGL_EXTENSIONS", e.getMessage());
-		    	}
-		        result.put("EGL_INFO", eglinfo);
-	    	} finally {
-		        dummy.release();
-		        egl.release();
-	    	}
+			final GLContext context = new GLContext(3, null, 0);
+			final Semaphore sem = new Semaphore(0);
+			new Thread(() -> {
+				context.initialize();
+				try {
+					final IntBuffer val = IntBuffer.allocate(2);
+					// GLコンテキストの情報を取得
+					final JSONObject glinfo = new JSONObject();
+					try {
+						glinfo.put("GL_VENDOR", GLES20.glGetString(GLES20.GL_VENDOR));
+					} catch (Exception e) {
+						glinfo.put("GL_VENDOR", e.getMessage());
+					}
+					try {
+						glinfo.put("GL_VERSION", GLES20.glGetString(GLES20.GL_VERSION));
+					} catch (Exception e) {
+						glinfo.put("GL_VERSION", e.getMessage());
+					}
+					try {
+						glinfo.put("GL_RENDERER", GLES20.glGetString(GLES20.GL_RENDERER));
+					} catch (Exception e) {
+						glinfo.put("GL_RENDERER", e.getMessage());
+					}
+					try {
+						GLES20.glGetIntegerv(GLES20.GL_MAX_VERTEX_ATTRIBS, val);
+						glinfo.put("GL_MAX_VERTEX_ATTRIBS", val.get(0));
+					} catch (Exception e) {
+						glinfo.put("GL_MAX_VERTEX_ATTRIBS", e.getMessage());
+					}
+					try {
+						GLES20.glGetIntegerv(GLES20.GL_MAX_VERTEX_UNIFORM_VECTORS, val);
+						glinfo.put("GL_MAX_VERTEX_UNIFORM_VECTORS", val.get(0));
+					} catch (Exception e) {
+						glinfo.put("GL_MAX_VERTEX_UNIFORM_VECTORS", e.getMessage());
+					}
+					try {
+						GLES20.glGetIntegerv(GLES20.GL_MAX_VARYING_VECTORS, val);
+						glinfo.put("GL_MAX_VARYING_VECTORS", val.get(0));
+					} catch (Exception e) {
+						glinfo.put("GL_MAX_VARYING_VECTORS", e.getMessage());
+					}
+					try {
+						GLES20.glGetIntegerv(GLES20.GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS, val);
+						glinfo.put("GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS", val.get(0));
+					} catch (Exception e) {
+						glinfo.put("GL_MAX_COMBINED_TEXTURE_IMAGE_UNITS", e.getMessage());
+					}
+					try {
+						GLES20.glGetIntegerv(GLES20.GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS, val);
+						glinfo.put("GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS", val.get(0));
+					} catch (Exception e) {
+						glinfo.put("GL_MAX_VERTEX_TEXTURE_IMAGE_UNITS", e.getMessage());
+					}
+					try {
+						GLES20.glGetIntegerv(GLES20.GL_MAX_TEXTURE_IMAGE_UNITS, val);
+						glinfo.put("GL_MAX_TEXTURE_IMAGE_UNITS", val.get(0));
+					} catch (Exception e) {
+						glinfo.put("GL_MAX_TEXTURE_IMAGE_UNITS", e.getMessage());
+					}
+					try {
+						GLES20.glGetIntegerv(GLES20.GL_MAX_FRAGMENT_UNIFORM_VECTORS, val);
+						glinfo.put("GL_MAX_FRAGMENT_UNIFORM_VECTORS", val.get(0));
+					} catch (Exception e) {
+						glinfo.put("GL_MAX_FRAGMENT_UNIFORM_VECTORS", e.getMessage());
+					}
+					try {
+						GLES20.glGetIntegerv(GLES20.GL_MAX_CUBE_MAP_TEXTURE_SIZE, val);
+						glinfo.put("GL_MAX_CUBE_MAP_TEXTURE_SIZE", val.get(0));
+					} catch (Exception e) {
+						glinfo.put("GL_MAX_CUBE_MAP_TEXTURE_SIZE", e.getMessage());
+					}
+					try {
+						GLES20.glGetIntegerv(GLES20.GL_MAX_RENDERBUFFER_SIZE, val);
+						glinfo.put("GL_MAX_RENDERBUFFER_SIZE", val.get(0));
+					} catch (Exception e) {
+						glinfo.put("GL_MAX_RENDERBUFFER_SIZE", e.getMessage());
+					}
+					try {
+						GLES20.glGetIntegerv(GLES20.GL_MAX_TEXTURE_SIZE, val);
+						glinfo.put("GL_MAX_TEXTURE_SIZE", val.get(0));
+					} catch (Exception e) {
+						glinfo.put("GL_MAX_TEXTURE_SIZE", e.getMessage());
+					}
+					try {
+						GLES20.glGetIntegerv(GLES20.GL_MAX_VIEWPORT_DIMS, val);
+						glinfo.put("GL_MAX_VIEWPORT_DIMS", String.format(Locale.US, "%d x %d", val.get(0), val.get(1)));
+					} catch (Exception e) {
+						glinfo.put("GL_MAX_VIEWPORT_DIMS", e.getMessage());
+					}
+					try {
+						glinfo.put("GL_EXTENSIONS", formatExtensions(GLES20.glGetString(GLES20.GL_EXTENSIONS)));
+					} catch (Exception e) {
+						glinfo.put("GL_EXTENSIONS", e.getMessage());
+					}
+					result.put("GL_INFO", glinfo);
+					// EGLの情報取得
+					final EGLBase egl = context.getEgl();
+					final JSONObject eglinfo = new JSONObject();
+					try {
+						eglinfo.put("EGL_VENDOR", egl.queryString(EGL10.EGL_VENDOR));
+					} catch (Exception e) {
+						glinfo.put("EGL_VENDOR", e.getMessage());
+					}
+					try {
+						eglinfo.put("EGL_VERSION", egl.queryString(EGL10.EGL_VERSION));
+					} catch (Exception e) {
+						glinfo.put("EGL_VERSION", e.getMessage());
+					}
+					try {
+						eglinfo.put("EGL_CLIENT_APIS", egl.queryString(EGL_CLIENT_APIS));
+					} catch (Exception e) {
+						glinfo.put("EGL_CLIENT_APIS", e.getMessage());
+					}
+					try {
+						eglinfo.put("EGL_EXTENSIONS:", formatExtensions(egl.queryString(EGL10.EGL_EXTENSIONS)));
+					} catch (Exception e) {
+						glinfo.put("EGL_EXTENSIONS", e.getMessage());
+					}
+					result.put("EGL_INFO", eglinfo);
+				} catch (final JSONException e) {
+					try {
+						result.put("EXCEPTION", e.getMessage());
+					} catch (JSONException ex) {
+						// ignore
+					}
+				} finally {
+					context.release();
+					sem.release();
+				}
+			}).start();
+			if (!sem.tryAcquire(3000, TimeUnit.MILLISECONDS)) {
+				throw new RuntimeException("Failed to get GL info");
+			}
 		} catch (Exception e) {
 			result.put("EXCEPTION", e.getMessage());
 		}
