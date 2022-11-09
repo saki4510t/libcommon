c6b74271b471584222c8790f6dd6ea26f9512d05
saki4510t@gmail.com <saki4510t@gmail.com>
2022-11-09 17:00:03 +0900

    OpenGLの映像ソースからGLBitmapImageReaderとGLImageReceiverでBitmapを取得するテストを追加

--- common/src/androidTest/java/com/serenegiant/common/GLImageReceiverTest.java
+++ common/src/androidTest/java/com/serenegiant/common/GLImageReceiverTest.java
@@ -22,8 +22,13 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.view.Surface;
 
+import com.serenegiant.gl.GLManager;
+import com.serenegiant.glpipeline.GLPipeline;
+import com.serenegiant.glpipeline.ImageSourcePipeline;
+import com.serenegiant.glpipeline.SurfaceRendererPipeline;
 import com.serenegiant.glutils.GLImageReceiver;
 import com.serenegiant.glutils.GLBitmapImageReader;
+import com.serenegiant.glutils.IMirror;
 import com.serenegiant.glutils.ImageReader;
 import com.serenegiant.graphics.BitmapHelper;
 import com.serenegiant.utils.HandlerThreadHandler;
@@ -63,7 +68,7 @@ public class GLImageReceiverTest {
 	}
 
 	@Test
-	public void surfaceReader() {
+	public void surfaceReadTest() {
 		final Bitmap original = BitmapHelper.makeCheckBitmap(
 			WIDTH, HEIGHT, 15, 12, Bitmap.Config.ARGB_8888);
 //		dump(bitmap);
@@ -97,6 +102,60 @@ public class GLImageReceiverTest {
 		assertNotNull(surface);
 
 		inputImagesAsync(original, surface, 10);
+
+		try {
+			assertTrue(sem.tryAcquire(1000, TimeUnit.MILLISECONDS));
+			final Bitmap b = result.get();
+//			dump(b);
+			assertNotNull(b);
+			// 元のビットマップと同じかどうかを検証
+			assertTrue(bitmapEquals(original, b));
+		} catch (final InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void pipelineReader() {
+		final Bitmap original = BitmapHelper.makeCheckBitmap(
+			WIDTH, HEIGHT, 15, 12, Bitmap.Config.ARGB_8888);
+//		dump(bitmap);
+
+		final GLManager manager = new GLManager();
+		final ImageSourcePipeline source = new ImageSourcePipeline(manager, original, null);
+		final SurfaceRendererPipeline renderer = new SurfaceRendererPipeline(manager);
+		// OpenGLの描画を経由するとビットマップが上下反転してしまうのであらかじめ上下判定設定を適用
+		renderer.setMirror(IMirror.MIRROR_VERTICAL);
+		GLPipeline.append(source, renderer);
+
+		final Semaphore sem = new Semaphore(0);
+		final AtomicReference<Bitmap> result = new AtomicReference<>();
+		final GLBitmapImageReader reader
+			= new GLBitmapImageReader(WIDTH, HEIGHT, Bitmap.Config.ARGB_8888, 2);
+		reader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener<Bitmap>() {
+			final AtomicInteger cnt = new AtomicInteger();
+			@Override
+			public void onImageAvailable(@NonNull final ImageReader<Bitmap> reader) {
+				final Bitmap bitmap = reader.acquireLatestImage();
+				if (bitmap != null) {
+					try {
+						if (cnt.incrementAndGet() >= 5) {
+							if (sem.availablePermits() == 0) {
+								result.set(Bitmap.createBitmap(bitmap));
+								sem.release();
+							}
+						}
+					} finally {
+						reader.recycle(bitmap);
+					}
+				}
+			}
+		}, HandlerThreadHandler.createHandler());
+
+		final GLImageReceiver receiver = new GLImageReceiver(WIDTH, HEIGHT, reader);
+		final Surface surface = receiver.getSurface();
+		assertNotNull(surface);
+		renderer.setSurface(surface);
 
 		try {
 			assertTrue(sem.tryAcquire(1000, TimeUnit.MILLISECONDS));
