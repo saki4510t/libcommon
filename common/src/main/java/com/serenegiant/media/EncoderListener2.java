bf85f2b25dafa95ac2da06e02fd36aff20d8f05b
saki4510t@gmail.com <saki4510t@gmail.com>
2022-12-18 15:37:04 +0900

    互換性維持のためのヘルパーメソッドを追加

--- common/src/main/java/com/serenegiant/media/EncoderListener2.java
+++ common/src/main/java/com/serenegiant/media/EncoderListener2.java
@@ -47,4 +47,39 @@ public interface EncoderListener2 extends EncoderListener {
 	 * @param e
 	 */
 	public void onError(@NonNull final Throwable e);
+
+	/**
+	 * すでにEncoderListener2であればそのまま返す
+	 * EncoderListenerであればEncoderListener2にラップして返す
+	 * @param listener
+	 * @return
+	 */
+	@NonNull
+	public static EncoderListener2 wrap(@NonNull final EncoderListener listener) {
+		if (listener instanceof EncoderListener2) {
+			return (EncoderListener2) listener;
+		} else {
+			return new EncoderListener2() {
+				@Override
+				public void onStartEncode(@NonNull final Encoder encoder, @Nullable final Surface source, final boolean mayFail) {
+					listener.onStartEncode(encoder, source, 0, mayFail);
+				}
+
+				@Override
+				public void onStopEncode(@NonNull final Encoder encoder) {
+					listener.onStopEncode(encoder);
+				}
+
+				@Override
+				public void onDestroy(@NonNull final Encoder encoder) {
+					listener.onDestroy(encoder);
+				}
+
+				@Override
+				public void onError(@NonNull final Throwable t) {
+					listener.onError(t);
+				}
+			};
+		}
+	}
 }
