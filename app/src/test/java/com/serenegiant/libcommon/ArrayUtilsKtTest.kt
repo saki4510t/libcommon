b532edb0afda139b0dcccbfcd861d9e3d0be8064
saki4510t <saki4510t@gmail.com>
2024-06-08 15:12:12 +0900

    配列とプリミティブ型の変換を行う拡張関数とそのテストを追加

---
+++ app/src/test/java/com/serenegiant/libcommon/ArrayUtilsKtTest.kt
@@ -0,0 +1,401 @@
+package com.serenegiant.libcommon
+/*
+ * libcommon
+ * utility/helper classes for myself
+ *
+ * Copyright (c) 2014-2024 saki t_saki@serenegiant.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+*/
+
+import com.serenegiant.utils.ArrayUtilsKt.toByteArray
+import com.serenegiant.utils.ArrayUtilsKt.toDouble
+import com.serenegiant.utils.ArrayUtilsKt.toFloat
+import com.serenegiant.utils.ArrayUtilsKt.toInt
+import com.serenegiant.utils.ArrayUtilsKt.toLong
+import com.serenegiant.utils.ArrayUtilsKt.toShort
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import java.nio.ByteOrder
+import kotlin.math.pow
+
+class ArrayUtilsKtTest {
+	@Test
+	fun primitiveToByteArrayBigEndian() {
+		val shortBytes = 0x1234.toShort().toByteArray()
+		assertEquals(Short.SIZE_BYTES, shortBytes.size)
+		assertEquals(0x12.toByte(), shortBytes[0])
+		assertEquals(0x34.toByte(), shortBytes[1])
+
+		val intBytes = 0x12345678.toByteArray()
+		assertEquals(Int.SIZE_BYTES, intBytes.size)
+		assertEquals(0x12.toByte(), intBytes[0])
+		assertEquals(0x34.toByte(), intBytes[1])
+		assertEquals(0x56.toByte(), intBytes[2])
+		assertEquals(0x78.toByte(), intBytes[3])
+
+		val longBytes = 0x123456789abcdef0.toByteArray()
+		assertEquals(Long.SIZE_BYTES, longBytes.size)
+		assertEquals(0x12.toByte(), longBytes[0])
+		assertEquals(0x34.toByte(), longBytes[1])
+		assertEquals(0x56.toByte(), longBytes[2])
+		assertEquals(0x78.toByte(), longBytes[3])
+		assertEquals(0x9a.toByte(), longBytes[4])
+		assertEquals(0xbc.toByte(), longBytes[5])
+		assertEquals(0xde.toByte(), longBytes[6])
+		assertEquals(0xf0.toByte(), longBytes[7])
+
+		val floatBytes = 1234.5677f.toByteArray()
+		assertEquals(Float.SIZE_BYTES, floatBytes.size)
+		assertEquals(0x44.toByte(), floatBytes[0])
+		assertEquals(0x9a.toByte(), floatBytes[1])
+		assertEquals(0x52.toByte(), floatBytes[2])
+		assertEquals(0x2b.toByte(), floatBytes[3])
+
+		val doubleBytes = 1234.56789.toByteArray()
+		assertEquals(Double.SIZE_BYTES, doubleBytes.size)
+		assertEquals(0x40.toByte(), doubleBytes[0])
+		assertEquals(0x93.toByte(), doubleBytes[1])
+		assertEquals(0x4a.toByte(), doubleBytes[2])
+		assertEquals(0x45.toByte(), doubleBytes[3])
+		assertEquals(0x84.toByte(), doubleBytes[4])
+		assertEquals(0xf4.toByte(), doubleBytes[5])
+		assertEquals(0xc6.toByte(), doubleBytes[6])
+		assertEquals(0xe7.toByte(), doubleBytes[7])
+	}
+
+	@Test
+	fun primitiveToByteArrayLittleEndian() {
+		val shortBytes = 0x1234.toShort().toByteArray(ByteOrder.LITTLE_ENDIAN)
+		assertEquals(Short.SIZE_BYTES, shortBytes.size)
+		assertEquals(0x12.toByte(), shortBytes[1])
+		assertEquals(0x34.toByte(), shortBytes[0])
+
+		val intBytes = 0x12345678.toByteArray(ByteOrder.LITTLE_ENDIAN)
+		assertEquals(Int.SIZE_BYTES, intBytes.size)
+		assertEquals(0x12.toByte(), intBytes[3])
+		assertEquals(0x34.toByte(), intBytes[2])
+		assertEquals(0x56.toByte(), intBytes[1])
+		assertEquals(0x78.toByte(), intBytes[0])
+
+		val longBytes = 0x123456789abcdef0.toByteArray(ByteOrder.LITTLE_ENDIAN)
+		assertEquals(Long.SIZE_BYTES, longBytes.size)
+		assertEquals(0x12.toByte(), longBytes[7])
+		assertEquals(0x34.toByte(), longBytes[6])
+		assertEquals(0x56.toByte(), longBytes[5])
+		assertEquals(0x78.toByte(), longBytes[4])
+		assertEquals(0x9a.toByte(), longBytes[3])
+		assertEquals(0xbc.toByte(), longBytes[2])
+		assertEquals(0xde.toByte(), longBytes[1])
+		assertEquals(0xf0.toByte(), longBytes[0])
+
+		val floatBytes = 1234.5677f.toByteArray(ByteOrder.LITTLE_ENDIAN)
+		assertEquals(Float.SIZE_BYTES, floatBytes.size)
+		assertEquals(0x44.toByte(), floatBytes[3])
+		assertEquals(0x9a.toByte(), floatBytes[2])
+		assertEquals(0x52.toByte(), floatBytes[1])
+		assertEquals(0x2b.toByte(), floatBytes[0])
+
+		val doubleBytes = 1234.56789.toByteArray(ByteOrder.LITTLE_ENDIAN)
+		assertEquals(Double.SIZE_BYTES, doubleBytes.size)
+		assertEquals(0x40.toByte(), doubleBytes[7])
+		assertEquals(0x93.toByte(), doubleBytes[6])
+		assertEquals(0x4a.toByte(), doubleBytes[5])
+		assertEquals(0x45.toByte(), doubleBytes[4])
+		assertEquals(0x84.toByte(), doubleBytes[3])
+		assertEquals(0xf4.toByte(), doubleBytes[2])
+		assertEquals(0xc6.toByte(), doubleBytes[1])
+		assertEquals(0xe7.toByte(), doubleBytes[0])
+	}
+
+	@Test
+	fun primitiveArrayToByteArrayBigEndian() {
+		val shortBytes = shortArrayOf(
+			0x1234.toShort(),
+			0x5678.toShort(),
+		).toByteArray()
+		assertEquals(Short.SIZE_BYTES * 2, shortBytes.size)
+		assertEquals(0x12.toByte(), shortBytes[0])
+		assertEquals(0x34.toByte(), shortBytes[1])
+		assertEquals(0x56.toByte(), shortBytes[2])
+		assertEquals(0x78.toByte(), shortBytes[3])
+
+		val intBytes = intArrayOf(
+			0x12345678.toInt(),
+			0x9abcdef0.toInt(),
+		).toByteArray()
+		assertEquals(Int.SIZE_BYTES * 2, intBytes.size)
+		assertEquals(0x12.toByte(), intBytes[0])
+		assertEquals(0x34.toByte(), intBytes[1])
+		assertEquals(0x56.toByte(), intBytes[2])
+		assertEquals(0x78.toByte(), intBytes[3])
+		assertEquals(0x9a.toByte(), intBytes[4])
+		assertEquals(0xbc.toByte(), intBytes[5])
+		assertEquals(0xde.toByte(), intBytes[6])
+		assertEquals(0xf0.toByte(), intBytes[7])
+
+		val longBytes = longArrayOf(
+			0x123456789abcdef0,
+			0x0fedcba987654321,
+		).toByteArray()
+		assertEquals(Long.SIZE_BYTES * 2, longBytes.size)
+		assertEquals(0x12.toByte(), longBytes[0])
+		assertEquals(0x34.toByte(), longBytes[1])
+		assertEquals(0x56.toByte(), longBytes[2])
+		assertEquals(0x78.toByte(), longBytes[3])
+		assertEquals(0x9a.toByte(), longBytes[4])
+		assertEquals(0xbc.toByte(), longBytes[5])
+		assertEquals(0xde.toByte(), longBytes[6])
+		assertEquals(0xf0.toByte(), longBytes[7])
+		assertEquals(0x0f.toByte(), longBytes[8])
+		assertEquals(0xed.toByte(), longBytes[9])
+		assertEquals(0xcb.toByte(), longBytes[10])
+		assertEquals(0xa9.toByte(), longBytes[11])
+		assertEquals(0x87.toByte(), longBytes[12])
+		assertEquals(0x65.toByte(), longBytes[13])
+		assertEquals(0x43.toByte(), longBytes[14])
+		assertEquals(0x21.toByte(), longBytes[15])
+
+		val floatBytes = floatArrayOf(
+			1234.5677f,
+			1234.5677f,
+		).toByteArray()
+		assertEquals(Float.SIZE_BYTES * 2, floatBytes.size)
+		assertEquals(0x44.toByte(), floatBytes[0])
+		assertEquals(0x9a.toByte(), floatBytes[1])
+		assertEquals(0x52.toByte(), floatBytes[2])
+		assertEquals(0x2b.toByte(), floatBytes[3])
+		assertEquals(0x44.toByte(), floatBytes[4])
+		assertEquals(0x9a.toByte(), floatBytes[5])
+		assertEquals(0x52.toByte(), floatBytes[6])
+		assertEquals(0x2b.toByte(), floatBytes[7])
+
+		val doubleBytes = doubleArrayOf(
+			1234.56789,
+			1234.56789,
+		).toByteArray()
+		assertEquals(Double.SIZE_BYTES * 2, doubleBytes.size)
+		assertEquals(0x40.toByte(), doubleBytes[0])
+		assertEquals(0x93.toByte(), doubleBytes[1])
+		assertEquals(0x4a.toByte(), doubleBytes[2])
+		assertEquals(0x45.toByte(), doubleBytes[3])
+		assertEquals(0x84.toByte(), doubleBytes[4])
+		assertEquals(0xf4.toByte(), doubleBytes[5])
+		assertEquals(0xc6.toByte(), doubleBytes[6])
+		assertEquals(0xe7.toByte(), doubleBytes[7])
+		assertEquals(0x40.toByte(), doubleBytes[8])
+		assertEquals(0x93.toByte(), doubleBytes[9])
+		assertEquals(0x4a.toByte(), doubleBytes[10])
+		assertEquals(0x45.toByte(), doubleBytes[11])
+		assertEquals(0x84.toByte(), doubleBytes[12])
+		assertEquals(0xf4.toByte(), doubleBytes[13])
+		assertEquals(0xc6.toByte(), doubleBytes[14])
+		assertEquals(0xe7.toByte(), doubleBytes[15])
+	}
+
+	@Test
+	fun primitiveArrayToByteArrayLittleEndian() {
+		val shortBytes = shortArrayOf(
+			0x1234.toShort(),
+			0x5678.toShort(),
+		).toByteArray(ByteOrder.LITTLE_ENDIAN)
+		assertEquals(Short.SIZE_BYTES * 2, shortBytes.size)
+		assertEquals(0x12.toByte(), shortBytes[1])
+		assertEquals(0x34.toByte(), shortBytes[0])
+		assertEquals(0x56.toByte(), shortBytes[3])
+		assertEquals(0x78.toByte(), shortBytes[2])
+
+		val intBytes = intArrayOf(
+			0x12345678.toInt(),
+			0x9abcdef0.toInt(),
+		).toByteArray(ByteOrder.LITTLE_ENDIAN)
+		assertEquals(Int.SIZE_BYTES * 2, intBytes.size)
+		assertEquals(0x12.toByte(), intBytes[3])
+		assertEquals(0x34.toByte(), intBytes[2])
+		assertEquals(0x56.toByte(), intBytes[1])
+		assertEquals(0x78.toByte(), intBytes[0])
+		assertEquals(0x9a.toByte(), intBytes[7])
+		assertEquals(0xbc.toByte(), intBytes[6])
+		assertEquals(0xde.toByte(), intBytes[5])
+		assertEquals(0xf0.toByte(), intBytes[4])
+
+		val longBytes = longArrayOf(
+			0x123456789abcdef0,
+			0x0fedcba987654321,
+		).toByteArray(ByteOrder.LITTLE_ENDIAN)
+		assertEquals(Long.SIZE_BYTES * 2, longBytes.size)
+		assertEquals(0x12.toByte(), longBytes[7])
+		assertEquals(0x34.toByte(), longBytes[6])
+		assertEquals(0x56.toByte(), longBytes[5])
+		assertEquals(0x78.toByte(), longBytes[4])
+		assertEquals(0x9a.toByte(), longBytes[3])
+		assertEquals(0xbc.toByte(), longBytes[2])
+		assertEquals(0xde.toByte(), longBytes[1])
+		assertEquals(0xf0.toByte(), longBytes[0])
+		assertEquals(0x0f.toByte(), longBytes[15])
+		assertEquals(0xed.toByte(), longBytes[14])
+		assertEquals(0xcb.toByte(), longBytes[13])
+		assertEquals(0xa9.toByte(), longBytes[12])
+		assertEquals(0x87.toByte(), longBytes[11])
+		assertEquals(0x65.toByte(), longBytes[10])
+		assertEquals(0x43.toByte(), longBytes[9])
+		assertEquals(0x21.toByte(), longBytes[8])
+
+		val floatBytes = floatArrayOf(
+			1234.5677f,
+			1234.5677f,
+		).toByteArray(ByteOrder.LITTLE_ENDIAN)
+		assertEquals(Float.SIZE_BYTES * 2, floatBytes.size)
+		assertEquals(0x44.toByte(), floatBytes[3])
+		assertEquals(0x9a.toByte(), floatBytes[2])
+		assertEquals(0x52.toByte(), floatBytes[1])
+		assertEquals(0x2b.toByte(), floatBytes[0])
+		assertEquals(0x44.toByte(), floatBytes[7])
+		assertEquals(0x9a.toByte(), floatBytes[6])
+		assertEquals(0x52.toByte(), floatBytes[5])
+		assertEquals(0x2b.toByte(), floatBytes[4])
+
+		val doubleBytes = doubleArrayOf(
+			1234.56789,
+			1234.56789,
+		).toByteArray(ByteOrder.LITTLE_ENDIAN)
+		assertEquals(Double.SIZE_BYTES * 2, doubleBytes.size)
+		assertEquals(0x40.toByte(), doubleBytes[7])
+		assertEquals(0x93.toByte(), doubleBytes[6])
+		assertEquals(0x4a.toByte(), doubleBytes[5])
+		assertEquals(0x45.toByte(), doubleBytes[4])
+		assertEquals(0x84.toByte(), doubleBytes[3])
+		assertEquals(0xf4.toByte(), doubleBytes[2])
+		assertEquals(0xc6.toByte(), doubleBytes[1])
+		assertEquals(0xe7.toByte(), doubleBytes[0])
+		assertEquals(0x40.toByte(), doubleBytes[15])
+		assertEquals(0x93.toByte(), doubleBytes[14])
+		assertEquals(0x4a.toByte(), doubleBytes[13])
+		assertEquals(0x45.toByte(), doubleBytes[12])
+		assertEquals(0x84.toByte(), doubleBytes[11])
+		assertEquals(0xf4.toByte(), doubleBytes[10])
+		assertEquals(0xc6.toByte(), doubleBytes[9])
+		assertEquals(0xe7.toByte(), doubleBytes[8])
+	}
+
+	@Test
+	fun byteArrayToXXXTest() {
+		val shortValue = byteArrayOf(
+			0x12.toByte(),
+			0x34.toByte(),
+		).toShort()
+		assertEquals(0x1234.toShort(), shortValue)
+
+		val intValue = byteArrayOf(
+			0x12.toByte(),
+			0x34.toByte(),
+			0x56.toByte(),
+			0x78.toByte(),
+		).toInt()
+		assertEquals(0x12345678, intValue)
+
+		val longValue = byteArrayOf(
+			0x12.toByte(),
+			0x34.toByte(),
+			0x56.toByte(),
+			0x78.toByte(),
+			0x9a.toByte(),
+			0xbc.toByte(),
+			0xde.toByte(),
+			0xf0.toByte(),
+		).toLong()
+		assertEquals(0x123456789abcdef0, longValue)
+
+		val floatValue = byteArrayOf(
+			0x44.toByte(),
+			0x9a.toByte(),
+			0x52.toByte(),
+			0x2b.toByte(),
+		).toFloat()
+		assertEquals(1234.5677f, floatValue)
+
+		val doubleValue = byteArrayOf(
+			0x40.toByte(),
+			0x93.toByte(),
+			0x4a.toByte(),
+			0x45.toByte(),
+			0x84.toByte(),
+			0xf4.toByte(),
+			0xc6.toByte(),
+			0xe7.toByte(),
+		).toDouble()
+		assertEquals(1234.56789, doubleValue, EPS)
+	}
+
+	@Test
+	fun byteArrayToXXXWithOffsetTest() {
+		val shortValue = byteArrayOf(
+			0x00, 0x00, 0x00,
+			0x12.toByte(),
+			0x34.toByte(),
+		).toShort(3)
+		assertEquals(0x1234.toShort(), shortValue)
+
+		val intValue = byteArrayOf(
+			0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00,
+			0x12.toByte(),
+			0x34.toByte(),
+			0x56.toByte(),
+			0x78.toByte(),
+		).toInt(6)
+		assertEquals(0x12345678, intValue)
+
+		val longValue = byteArrayOf(
+			0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00,
+			0x00, 0x00, 0x00,
+			0x12.toByte(),
+			0x34.toByte(),
+			0x56.toByte(),
+			0x78.toByte(),
+			0x9a.toByte(),
+			0xbc.toByte(),
+			0xde.toByte(),
+			0xf0.toByte(),
+		).toLong(9)
+		assertEquals(0x123456789abcdef0, longValue)
+
+		val floatValue = byteArrayOf(
+			0x12, 0x34, 0x56, 0x78, 0x00,
+			0x44.toByte(),
+			0x9a.toByte(),
+			0x52.toByte(),
+			0x2b.toByte(),
+		).toFloat(5)
+		assertEquals(1234.5677f, floatValue)
+
+		val doubleValue = byteArrayOf(
+			0x12, 0x34, 0x56, 0x78, 0x00,
+			0x12, 0x34, 0x56, 0x78, 0x00,
+			0x40.toByte(),
+			0x93.toByte(),
+			0x4a.toByte(),
+			0x45.toByte(),
+			0x84.toByte(),
+			0xf4.toByte(),
+			0xc6.toByte(),
+			0xe7.toByte(),
+		).toDouble(10)
+		assertEquals(1234.56789, doubleValue, EPS)
+	}
+
+	companion object {
+		private val EPS = 10.0.pow((java.lang.Float.MIN_EXPONENT + 2).toDouble())
+	}
+}
---
+++ common/src/main/java/com/serenegiant/utils/ArrayUtilsKt.kt
@@ -0,0 +1,209 @@
+package com.serenegiant.utils
+/*
+ * libcommon
+ * utility/helper classes for myself
+ *
+ * Copyright (c) 2014-2024 saki t_saki@serenegiant.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+*/
+
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
+
+object ArrayUtilsKt {
+	//--------------------------------------------------------------------------------
+	// xxx.toByteArray
+	//--------------------------------------------------------------------------------
+	fun Short.toByteArray(byteOrder: ByteOrder = ByteOrder.BIG_ENDIAN): ByteArray =
+		ByteBuffer.allocate(Short.SIZE_BYTES).order(byteOrder).putShort(this).array()
+
+	fun Int.toByteArray(byteOrder: ByteOrder = ByteOrder.BIG_ENDIAN): ByteArray =
+		ByteBuffer.allocate(Int.SIZE_BYTES).order(byteOrder).putInt(this).array()
+
+	fun Long.toByteArray(byteOrder: ByteOrder = ByteOrder.BIG_ENDIAN): ByteArray =
+		ByteBuffer.allocate(Long.SIZE_BYTES).order(byteOrder).putLong(this).array()
+
+	fun Float.toByteArray(byteOrder: ByteOrder = ByteOrder.BIG_ENDIAN): ByteArray =
+		ByteBuffer.allocate(Float.SIZE_BYTES).order(byteOrder).putFloat(this).array()
+
+	fun Double.toByteArray(byteOrder: ByteOrder = ByteOrder.BIG_ENDIAN): ByteArray =
+		ByteBuffer.allocate(Double.SIZE_BYTES).order(byteOrder).putDouble(this).array()
+
+	fun ShortArray.toByteArray(byteOrder: ByteOrder = ByteOrder.BIG_ENDIAN): ByteArray {
+		val buf = ByteBuffer.allocate(Short.SIZE_BYTES * size)
+			.order(byteOrder)
+		forEach { value ->
+			buf.putShort(value)
+		}
+		return buf.array()
+	}
+
+	fun IntArray.toByteArray(byteOrder: ByteOrder = ByteOrder.BIG_ENDIAN): ByteArray {
+		val buf = ByteBuffer.allocate(Int.SIZE_BYTES * size)
+			.order(byteOrder)
+		forEach { value ->
+			buf.putInt(value)
+		}
+		return buf.array()
+	}
+
+	fun LongArray.toByteArray(byteOrder: ByteOrder = ByteOrder.BIG_ENDIAN): ByteArray {
+		val buf = ByteBuffer.allocate(Long.SIZE_BYTES * size)
+			.order(byteOrder)
+		forEach { value ->
+			buf.putLong(value)
+		}
+		return buf.array()
+	}
+
+	fun FloatArray.toByteArray(byteOrder: ByteOrder = ByteOrder.BIG_ENDIAN): ByteArray {
+		val buf = ByteBuffer.allocate(Float.SIZE_BYTES * size)
+			.order(byteOrder)
+		forEach { value ->
+			buf.putFloat(value)
+		}
+		return buf.array()
+	}
+
+	fun DoubleArray.toByteArray(byteOrder: ByteOrder = ByteOrder.BIG_ENDIAN): ByteArray {
+		val buf = ByteBuffer.allocate(Double.SIZE_BYTES * size)
+			.order(byteOrder)
+		forEach { value ->
+			buf.putDouble(value)
+		}
+		return buf.array()
+	}
+
+	//--------------------------------------------------------------------------------
+	// ByteArray.toXXX
+	//--------------------------------------------------------------------------------
+	fun ByteArray.toShort(offset: Int = 0, byteOrder: ByteOrder = ByteOrder.BIG_ENDIAN): Short =
+		ByteBuffer.wrap(copyOfRange(offset, offset + Short.SIZE_BYTES)).order(byteOrder).short
+
+	fun ByteArray.toInt(offset: Int = 0, byteOrder: ByteOrder = ByteOrder.BIG_ENDIAN): Int =
+		ByteBuffer.wrap(copyOfRange(offset, offset + Int.SIZE_BYTES)).order(byteOrder).int
+
+	fun ByteArray.toLong(offset: Int = 0, byteOrder: ByteOrder = ByteOrder.BIG_ENDIAN): Long =
+		ByteBuffer.wrap(copyOfRange(offset, offset + Long.SIZE_BYTES)).order(byteOrder).long
+
+	fun ByteArray.toFloat(offset: Int = 0, byteOrder: ByteOrder = ByteOrder.BIG_ENDIAN): Float =
+		ByteBuffer.wrap(copyOfRange(offset, offset + Float.SIZE_BYTES)).order(byteOrder).float
+
+	fun ByteArray.toDouble(offset: Int = 0, byteOrder: ByteOrder = ByteOrder.BIG_ENDIAN): Double =
+		ByteBuffer.wrap(copyOfRange(offset, offset + Double.SIZE_BYTES)).order(byteOrder).double
+
+	private val ZERO_PADDING = "0".repeat(32)
+	/**
+	 * Intを指定した文字数の2進数文字列に変換する
+	 */
+	fun Int.toBinString(numDigit: Int): String {
+		return "$ZERO_PADDING${Integer.toBinaryString(this)}".takeLast(numDigit)
+	}
+
+	/**
+	 * バイト配列を16進数文字列に変換する
+	 */
+	fun ByteArray.toHexString(): String {
+		val builder = StringBuilder()
+		for (byte in this) {
+			builder.append("%02X".format(byte.toInt()))
+		}
+		return builder.toString()
+	}
+
+	/**
+	 * 指定したパターンに一致する先頭位置を検索する
+	 * @param pattern
+	 * @param startFrom
+	 * @param lastIndex
+	 */
+	fun ByteArray.findFirstBy(pattern: ByteArray, startFrom: Int = 0, lastIndex: Int = size - 1): Int {
+		if (pattern.isEmpty()) throw IllegalArgumentException("non-empty byte sequence is required")
+		if (startFrom < 0) throw IllegalArgumentException("startFrom must be non-negative")
+		var matchOffset = 0
+		var start = startFrom
+		var offset = startFrom
+		while (offset <= lastIndex) {
+			if (this[offset] == pattern[matchOffset]) {
+				if (matchOffset++ == 0) start = offset
+				if (matchOffset == pattern.size) return start
+			} else {
+				matchOffset = 0
+			}
+			offset++
+		}
+		return -1
+	}
+
+	fun ByteArray.toInt2(offset: Int = 0): Int {
+		if ((offset < 0) || (offset + 2 > this.size)) {
+			throw IndexOutOfBoundsException()
+		}
+		return this[offset + 1].toUByte().toInt() +
+			(this[offset].toUByte().toInt() shl 8)
+	}
+
+	fun ByteArray.toInt3(offset: Int = 0): Int {
+		if ((offset < 0) || (offset + 4 > this.size)) {
+			throw IndexOutOfBoundsException()
+		}
+		return this[offset + 2].toUByte().toInt() +
+			(this[offset + 1].toUByte().toInt() shl 8) +
+			(this[offset].toUByte().toInt() shl 16)
+	}
+
+	fun ByteArray.toInt(offset: Int = 0): Int {
+		if ((offset < 0) || (offset + 4 > this.size)) {
+			throw IndexOutOfBoundsException()
+		}
+		return this[offset + 3].toUByte().toInt() +
+			(this[offset + 2].toUByte().toInt() shl 8) +
+			(this[offset + 1].toUByte().toInt() shl 16) +
+			(this[offset].toUByte().toInt() shl 24)
+	}
+
+	fun ByteArray.setInt2(value: Int, offset: Int = 0): ByteArray {
+		if ((offset < 0) || (offset + 2 > this.size)) {
+			throw IndexOutOfBoundsException()
+		}
+		this[offset    ] = (value shr 8).toByte()
+		this[offset + 1] = value.toByte()
+		return this
+	}
+
+	fun ByteArray.setInt3(value: Int, offset: Int = 0): ByteArray {
+		if ((offset < 0) || (offset + 3 > this.size)) {
+			throw IndexOutOfBoundsException()
+		}
+		this[offset    ] = (value shr 16).toByte()
+		this[offset + 1] = (value shr 8).toByte()
+		this[offset + 2] = value.toByte()
+		return this
+	}
+
+	fun ByteArray.setInt(value: Int, offset: Int = 0): ByteArray {
+		if ((offset < 0) || (offset + 4 > this.size)) {
+			throw IndexOutOfBoundsException()
+		}
+		this[offset    ] = (value shr 24).toByte()
+		this[offset + 1] = (value shr 16).toByte()
+		this[offset + 2] = (value shr 8).toByte()
+		this[offset + 3] = value.toByte()
+		return this
+	}
+
+	fun ByteArray.calcBcc(): Int {
+		return (this.fold(0) { v, value -> (v xor (value.toInt() and 0xff)) } and 0xff)
+	}
+}
