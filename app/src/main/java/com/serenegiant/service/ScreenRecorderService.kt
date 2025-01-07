f6e68f8701c879b2b17706c77c525cf620f430cc
saki4510t <saki4510t@gmail.com>
2025-01-07 13:50:57 +0900

    Happy 2025

--- app/src/main/java/com/serenegiant/service/ScreenRecorderService.kt
+++ app/src/main/java/com/serenegiant/service/ScreenRecorderService.kt
@@ -3,7 +3,7 @@ package com.serenegiant.service
  * ScreenRecordingSample
  * Sample project to capture and save audio from internal mic and video from screen as MPEG4 file.
  *
- * Copyright (c) 2015-2024 saki t_saki@serenegiant.com
+ * Copyright (c) 2015-2025 saki t_saki@serenegiant.com
  *
  * File name: ScreenRecorderService.java
  *
