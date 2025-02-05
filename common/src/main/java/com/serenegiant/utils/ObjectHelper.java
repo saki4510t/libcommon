602bb718f79176085d39643374faa9137085a6f9
saki4510t <saki4510t@gmail.com>
2025-02-05 22:38:47 +0900

    asBooleanでnullとブランク文字列はfalseと見なすように変更、kotlinの予約後と被るのでvalをvalueへリネーム

--- common/src/main/java/com/serenegiant/utils/ObjectHelper.java
+++ common/src/main/java/com/serenegiant/utils/ObjectHelper.java
@@ -31,30 +31,33 @@ public class ObjectHelper {
 
 	/**
 	 * booleanへ変換
-	 * @param val
+	 * @param value
 	 * @param defaultValue
 	 * @return
 	 */
-	public static boolean asBoolean(final Object val, final boolean defaultValue) {
-		if (val instanceof Boolean) {
-			return (Boolean)val;
-		} else if (val instanceof Byte) {
-			return ((Byte)val) != 0;
-		} else if (val instanceof Short) {
-			return ((Short)val) != 0;
-		} else if (val instanceof Integer) {
-			return ((Integer)val) != 0;
-		} else if (val instanceof Long) {
-			return ((Long)val) != 0;
-		} else if (val instanceof Float) {
-			return ((Float)val) != 0;
-		} else if (val instanceof Double) {
-			return ((Double)val) != 0;
-		} else if (val instanceof Number) {
-			return ((Number)val).doubleValue() != 0;
-		} else if (val instanceof String) {
-			if (!TextUtils.isEmpty((String)val)) {
-				final String v = (String)val;
+	public static boolean asBoolean(final Object value, final boolean defaultValue) {
+		// XXX char(文字)の処理を入れた方が良い？intに暗黙のキャストされるから問題ない？
+		if (value instanceof Boolean) {
+			return (Boolean)value;
+		} else if (value instanceof Byte) {
+			return ((Byte)value) != 0;
+		} else if (value instanceof Short) {
+			return ((Short)value) != 0;
+		} else if (value instanceof Integer) {
+			return ((Integer)value) != 0;
+		} else if (value instanceof Long) {
+			return ((Long)value) != 0;
+		} else if (value instanceof Float) {
+			return ((Float)value) != 0;
+		} else if (value instanceof Double) {
+			return ((Double)value) != 0;
+		} else if (value instanceof Number) {
+			return ((Number)value).doubleValue() != 0;
+		} else if (value instanceof String) {
+			// 空文字列だけではなく空白文字列だけもデフォルト値にする
+			final String trimmedValue = ((String) value).trim();
+			if (!TextUtils.isEmpty(trimmedValue)) {
+				final String v = trimmedValue;
 				// 16進文字列かも
 				if (v.startsWith("0x") || v.startsWith("0X")) {
 					try {
@@ -81,38 +84,39 @@ public class ObjectHelper {
 				} catch (final Exception e) {
 					//
 				}
-				return  Boolean.parseBoolean((String)val);
+				return Boolean.parseBoolean((String)value);
 			}
 		}
+
 		return defaultValue;
 	}
 
 	/**
 	 * byteへ変換
-	 * @param val
+	 * @param value
 	 * @param defaultValue
 	 * @return
 	 */
-	public static byte asByte(final Object val, final byte defaultValue) {
-		if (val instanceof Boolean) {
-			return (Boolean)val ? (byte)1 : (byte)0;
-		} else if (val instanceof Byte) {
-			return ((Byte)val);
-		} else if (val instanceof Short) {
-			return ((Short)val).byteValue();
-		} else if (val instanceof Integer) {
-			return ((Integer)val).byteValue();
-		} else if (val instanceof Long) {
-			return ((Long)val).byteValue();
-		} else if (val instanceof Float) {
-			return ((Float)val).byteValue();
-		} else if (val instanceof Double) {
-			return ((Double)val).byteValue();
-		} else if (val instanceof Number) {
-			return ((Number)val).byteValue();
-		} else if (val instanceof String) {
-			if (!TextUtils.isEmpty((String)val)) {
-				final String v = (String)val;
+	public static byte asByte(final Object value, final byte defaultValue) {
+		if (value instanceof Boolean) {
+			return (Boolean)value ? (byte)1 : (byte)0;
+		} else if (value instanceof Byte) {
+			return ((Byte)value);
+		} else if (value instanceof Short) {
+			return ((Short)value).byteValue();
+		} else if (value instanceof Integer) {
+			return ((Integer)value).byteValue();
+		} else if (value instanceof Long) {
+			return ((Long)value).byteValue();
+		} else if (value instanceof Float) {
+			return ((Float)value).byteValue();
+		} else if (value instanceof Double) {
+			return ((Double)value).byteValue();
+		} else if (value instanceof Number) {
+			return ((Number)value).byteValue();
+		} else if (value instanceof String) {
+			if (!TextUtils.isEmpty((String)value)) {
+				final String v = (String)value;
 				// 16進文字列かも
 				if (v.startsWith("0x") || v.startsWith("0X")) {
 					try {
@@ -139,7 +143,7 @@ public class ObjectHelper {
 				} catch (final Exception e) {
 					//
 				}
-				return  Boolean.parseBoolean((String)val) ? (byte)1 : (byte)0;
+				return  Boolean.parseBoolean((String)value) ? (byte)1 : (byte)0;
 			}
 		}
 		return defaultValue;
@@ -147,30 +151,30 @@ public class ObjectHelper {
 
 	/**
 	 * shortへ変換
-	 * @param val
+	 * @param value
 	 * @param defaultValue
 	 * @return
 	 */
-	public static short asShort(final Object val, final short defaultValue) {
-		if (val instanceof Boolean) {
-			return (Boolean)val ? (short)1 : (short)0;
-		} else if (val instanceof Byte) {
-			return ((Byte)val);
-		} else if (val instanceof Short) {
-			return ((Short)val);
-		} else if (val instanceof Integer) {
-			return ((Integer)val).shortValue();
-		} else if (val instanceof Long) {
-			return ((Long)val).shortValue();
-		} else if (val instanceof Float) {
-			return ((Float)val).shortValue();
-		} else if (val instanceof Double) {
-			return ((Double)val).shortValue();
-		} else if (val instanceof Number) {
-			return ((Number)val).shortValue();
-		} else if (val instanceof String) {
-			if (!TextUtils.isEmpty((String)val)) {
-				final String v = (String)val;
+	public static short asShort(final Object value, final short defaultValue) {
+		if (value instanceof Boolean) {
+			return (Boolean)value ? (short)1 : (short)0;
+		} else if (value instanceof Byte) {
+			return ((Byte)value);
+		} else if (value instanceof Short) {
+			return ((Short)value);
+		} else if (value instanceof Integer) {
+			return ((Integer)value).shortValue();
+		} else if (value instanceof Long) {
+			return ((Long)value).shortValue();
+		} else if (value instanceof Float) {
+			return ((Float)value).shortValue();
+		} else if (value instanceof Double) {
+			return ((Double)value).shortValue();
+		} else if (value instanceof Number) {
+			return ((Number)value).shortValue();
+		} else if (value instanceof String) {
+			if (!TextUtils.isEmpty((String)value)) {
+				final String v = (String)value;
 				// 16進文字列かも
 				if (v.startsWith("0x") || v.startsWith("0X")) {
 					try {
@@ -197,7 +201,7 @@ public class ObjectHelper {
 				} catch (final Exception e) {
 					//
 				}
-				return  Boolean.parseBoolean((String)val) ? (short)1 : (short)0;
+				return  Boolean.parseBoolean((String)value) ? (short)1 : (short)0;
 			}
 		}
 		return defaultValue;
@@ -205,30 +209,30 @@ public class ObjectHelper {
 
 	/**
 	 * intへ変換
-	 * @param val
+	 * @param value
 	 * @param defaultValue
 	 * @return
 	 */
-	public static int asInt(final Object val, final int defaultValue) {
-		if (val instanceof Boolean) {
-			return ((Boolean)val) ? 1 : 0;
-		} else if (val instanceof Byte) {
-			return ((Byte)val);
-		} else if (val instanceof Short) {
-			return ((Short)val);
-		} else if (val instanceof Integer) {
-			return (Integer)val;
-		} else if (val instanceof Long) {
-			return ((Long)val).intValue();
-		} else if (val instanceof Float) {
-			return ((Float)val).intValue();
-		} else if (val instanceof Double) {
-			return ((Double)val).intValue();
-		} else if (val instanceof Number) {
-			return ((Number)val).intValue();
-		} else if (val instanceof String) {
-			if (!TextUtils.isEmpty((String)val)) {
-				final String v = (String)val;
+	public static int asInt(final Object value, final int defaultValue) {
+		if (value instanceof Boolean) {
+			return ((Boolean)value) ? 1 : 0;
+		} else if (value instanceof Byte) {
+			return ((Byte)value);
+		} else if (value instanceof Short) {
+			return ((Short)value);
+		} else if (value instanceof Integer) {
+			return (Integer)value;
+		} else if (value instanceof Long) {
+			return ((Long)value).intValue();
+		} else if (value instanceof Float) {
+			return ((Float)value).intValue();
+		} else if (value instanceof Double) {
+			return ((Double)value).intValue();
+		} else if (value instanceof Number) {
+			return ((Number)value).intValue();
+		} else if (value instanceof String) {
+			if (!TextUtils.isEmpty((String)value)) {
+				final String v = (String)value;
 				// 16進文字列かも
 				if (v.startsWith("0x") || v.startsWith("0X")) {
 					try {
@@ -255,7 +259,7 @@ public class ObjectHelper {
 				} catch (final Exception e) {
 					//
 				}
-				return Boolean.parseBoolean((String)val) ? 1 : 0;
+				return Boolean.parseBoolean((String)value) ? 1 : 0;
 			}
 		}
 		return defaultValue;
@@ -263,30 +267,30 @@ public class ObjectHelper {
 
 	/**
 	 * longへ変換
-	 * @param val
+	 * @param value
 	 * @param defaultValue
 	 * @return
 	 */
-	public static long asLong(final Object val, final long defaultValue) {
-		if (val instanceof Boolean) {
-			return ((Boolean)val) ? 1 : 0;
-		} else if (val instanceof Byte) {
-			return ((Byte)val);
-		} else if (val instanceof Short) {
-			return ((Short)val);
-		} else if (val instanceof Integer) {
-			return (Integer)val;
-		} else if (val instanceof Long) {
-			return (Long)val;
-		} else if (val instanceof Float) {
-			return ((Float)val).longValue();
-		} else if (val instanceof Double) {
-			return ((Double)val).longValue();
-		} else if (val instanceof Number) {
-			return ((Number)val).longValue();
-		} else if (val instanceof String) {
-			if (!TextUtils.isEmpty((String)val)) {
-				final String v = (String)val;
+	public static long asLong(final Object value, final long defaultValue) {
+		if (value instanceof Boolean) {
+			return ((Boolean)value) ? 1 : 0;
+		} else if (value instanceof Byte) {
+			return ((Byte)value);
+		} else if (value instanceof Short) {
+			return ((Short)value);
+		} else if (value instanceof Integer) {
+			return (Integer)value;
+		} else if (value instanceof Long) {
+			return (Long)value;
+		} else if (value instanceof Float) {
+			return ((Float)value).longValue();
+		} else if (value instanceof Double) {
+			return ((Double)value).longValue();
+		} else if (value instanceof Number) {
+			return ((Number)value).longValue();
+		} else if (value instanceof String) {
+			if (!TextUtils.isEmpty((String)value)) {
+				final String v = (String)value;
 				// 16進文字列かも
 				if (v.startsWith("0x") || v.startsWith("0X")) {
 					try {
@@ -307,7 +311,7 @@ public class ObjectHelper {
 				} catch (final Exception e2) {
 					//
 				}
-				return Boolean.parseBoolean((String)val) ? 1 : 0;
+				return Boolean.parseBoolean((String)value) ? 1 : 0;
 			}
 		}
 		return defaultValue;
@@ -315,30 +319,30 @@ public class ObjectHelper {
 
 	/**
 	 * floatへ変換
-	 * @param val
+	 * @param value
 	 * @param defaultValue
 	 * @return
 	 */
-	public static float asFloat(final Object val, final float defaultValue) {
-		if (val instanceof Boolean) {
-			return ((Boolean)val) ? 1 : 0;
-		} else if (val instanceof Byte) {
-			return ((Byte)val);
-		} else if (val instanceof Short) {
-			return ((Short)val);
-		} else if (val instanceof Integer) {
-			return (Integer)val;
-		} else if (val instanceof Long) {
-			return ((Long)val).floatValue();
-		} else if (val instanceof Float) {
-			return (Float)val;
-		} else if (val instanceof Double) {
-			return ((Double)val).floatValue();
-		} else if (val instanceof Number) {
-			return ((Number)val).floatValue();
-		} else if (val instanceof String) {
-			if (!TextUtils.isEmpty((String)val)) {
-				final String v = (String)val;
+	public static float asFloat(final Object value, final float defaultValue) {
+		if (value instanceof Boolean) {
+			return ((Boolean)value) ? 1 : 0;
+		} else if (value instanceof Byte) {
+			return ((Byte)value);
+		} else if (value instanceof Short) {
+			return ((Short)value);
+		} else if (value instanceof Integer) {
+			return (Integer)value;
+		} else if (value instanceof Long) {
+			return ((Long)value).floatValue();
+		} else if (value instanceof Float) {
+			return (Float)value;
+		} else if (value instanceof Double) {
+			return ((Double)value).floatValue();
+		} else if (value instanceof Number) {
+			return ((Number)value).floatValue();
+		} else if (value instanceof String) {
+			if (!TextUtils.isEmpty((String)value)) {
+				final String v = (String)value;
 				// 16進文字列かも
 				if (v.startsWith("0x") || v.startsWith("0X")) {
 					try {
@@ -359,7 +363,7 @@ public class ObjectHelper {
 				} catch (final Exception e2) {
 					//
 				}
-				return Boolean.parseBoolean((String)val) ? 1 : 0;
+				return Boolean.parseBoolean((String)value) ? 1 : 0;
 			}
 		}
 		return defaultValue;
@@ -367,30 +371,30 @@ public class ObjectHelper {
 
 	/**
 	 * doubleへ変換
-	 * @param val
+	 * @param value
 	 * @param defaultValue
 	 * @return
 	 */
-	public static double asDouble(final Object val, final double defaultValue) {
-		if (val instanceof Boolean) {
-			return ((Boolean)val) ? 1 : 0;
-		} else if (val instanceof Byte) {
-			return ((Byte)val);
-		} else if (val instanceof Short) {
-			return ((Short)val);
-		} else if (val instanceof Integer) {
-			return (Integer)val;
-		} else if (val instanceof Long) {
-			return ((Long)val).doubleValue();
-		} else if (val instanceof Float) {
-			return (Float)val;
-		} else if (val instanceof Double) {
-			return (Double)val;
-		} else if (val instanceof Number) {
-			return ((Number)val).doubleValue();
-		} else if (val instanceof String) {
-			if (!TextUtils.isEmpty((String)val)) {
-				final String v = (String)val;
+	public static double asDouble(final Object value, final double defaultValue) {
+		if (value instanceof Boolean) {
+			return ((Boolean)value) ? 1 : 0;
+		} else if (value instanceof Byte) {
+			return ((Byte)value);
+		} else if (value instanceof Short) {
+			return ((Short)value);
+		} else if (value instanceof Integer) {
+			return (Integer)value;
+		} else if (value instanceof Long) {
+			return ((Long)value).doubleValue();
+		} else if (value instanceof Float) {
+			return (Float)value;
+		} else if (value instanceof Double) {
+			return (Double)value;
+		} else if (value instanceof Number) {
+			return ((Number)value).doubleValue();
+		} else if (value instanceof String) {
+			if (!TextUtils.isEmpty((String)value)) {
+				final String v = (String)value;
 				// 16進文字列かも
 				if (v.startsWith("0x") || v.startsWith("0X")) {
 					try {
@@ -411,7 +415,7 @@ public class ObjectHelper {
 				} catch (final Exception e2) {
 					//
 				}
-				return Boolean.parseBoolean((String)val) ? 1 : 0;
+				return Boolean.parseBoolean((String)value) ? 1 : 0;
 			}
 		}
 		return defaultValue;
