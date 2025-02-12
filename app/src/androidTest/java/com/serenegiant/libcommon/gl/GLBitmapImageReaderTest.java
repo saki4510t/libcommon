2b99d7c30c07093e9146f88422249dd7e166d4d2
saki4510t <saki4510t@gmail.com>
2025-02-12 19:58:29 +0900

    GLBitmapImageReaderのテストを単独クラスへ移動

--- app/src/androidTest/java/com/serenegiant/libcommon/gl/GLBitmapImageReaderTest.java
+++ app/src/androidTest/java/com/serenegiant/libcommon/gl/GLBitmapImageReaderTest.java
@@ -16,20 +16,17 @@ package com.serenegiant.libcommon.gl;
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
-*/
+ */
 
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.util.Log;
 import android.view.Surface;
 
 import com.serenegiant.gl.GLManager;
-import com.serenegiant.glutils.GLImageReceiver;
 import com.serenegiant.glutils.GLBitmapImageReader;
+import com.serenegiant.glutils.GLImageReceiver;
 import com.serenegiant.glutils.ImageReader;
-import com.serenegiant.glutils.ImageTextureSource;
 import com.serenegiant.graphics.BitmapHelper;
-import com.serenegiant.math.Fraction;
 import com.serenegiant.utils.HandlerThreadHandler;
 
 import org.junit.After;
@@ -51,8 +48,8 @@ import static com.serenegiant.libcommon.TestUtils.*;
 import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
-public class ImageTextureSourceTest {
-	private static final String TAG = ImageTextureSourceTest.class.getSimpleName();
+public class GLBitmapImageReaderTest {
+	private static final String TAG = GLBitmapImageReaderTest.class.getSimpleName();
 
 	private static final int WIDTH = 100;
 	private static final int HEIGHT = 100;
@@ -82,113 +79,26 @@ public class ImageTextureSourceTest {
 	}
 
 	@Test
-	public void imageTextureSourceTest() {
+	public void gLBitmapImageReaderTest() {
 		final Bitmap original = BitmapHelper.makeCheckBitmap(
 			WIDTH, HEIGHT, 15, 12, Bitmap.Config.ARGB_8888);
 //		dump(bitmap);
 
 		final GLManager manager = mManager;
 
-		// 映像受け取り用にGLImageReceiverを生成
-		final Semaphore sem = new Semaphore(0);
-		final AtomicReference<Bitmap> result = new AtomicReference<>();
-		final Surface surface = createGLImageReceiverSurface(
-			manager, WIDTH, HEIGHT, MAX_FRAMES, sem, result);
-		assertNotNull(surface);
-
-		// 映像ソース用にImageTextureSourceを生成
-		final ImageTextureSource source = new ImageTextureSource(manager, original, new Fraction(30));
-		// 映像受け取り用Surfaceをセット
-		source.setSurface(surface);
-		try {
-			assertTrue(sem.tryAcquire(MAX_WAIT_MS, TimeUnit.MILLISECONDS));
-			final Bitmap resultBitmap = result.get();
-			assertNotNull(resultBitmap);
-			// 元のビットマップと同じかどうかを検証
-			assertTrue(bitmapEquals(original, resultBitmap));
-		} catch (final InterruptedException e) {
-			fail();
-		}
-	}
-
-	@Test
-	public void frameRate5Test() {
-		frameRate(mManager, 5);
-	}
-
-	@Test
-	public void frameRate10Test() {
-		frameRate(mManager, 10);
-	}
-
-	@Test
-	public void frameRate15Test() {
-		frameRate(mManager, 15);
-	}
-
-	@Test
-	public void frameRate20Test() {
-		frameRate(mManager, 20);
-	}
-
-	@Test
-	public void frameRate24Test() {
-		frameRate(mManager, 24);
-	}
-
-	@Test
-	public void frameRate30Test() {
-		frameRate(mManager, 30);
-	}
-
-	@Test
-	public void frameRate33Test() {
-		frameRate(mManager, 33);
-	}
-
-	@Test
-	public void frameRate35Test() {
-		frameRate(mManager, 35);
-	}
-
-	@Test
-	public void frameRate45Test() {
-		frameRate(mManager, 45);
-	}
-
-	@Test
-	public void frameRate50Test() {
-		frameRate(mManager, 50);
-	}
-
-	@Test
-	public void frameRate60Test() {
-		frameRate(mManager, 60);
-	}
-
-	/**
-	 * フレームレートを指定して想定通りのフレームレート±10%になるかどうかを確認
-	 * @param manager
-	 * @param requestFps
-	 */
-	private static void frameRate(@NonNull final GLManager manager, final int requestFps) {
-		final Bitmap original = BitmapHelper.makeCheckBitmap(
-			WIDTH, HEIGHT, 15, 12, Bitmap.Config.ARGB_8888);
-//		dump(bitmap);
-
 		// 映像受け取り用にSurfaceReaderを生成
 		final Semaphore sem = new Semaphore(0);
 		final AtomicReference<Bitmap> result = new AtomicReference<>();
-		final AtomicInteger numFrames = new AtomicInteger();
 		final GLBitmapImageReader reader
-			= new GLBitmapImageReader(WIDTH, HEIGHT, Bitmap.Config.ARGB_8888, 2);
+			= new GLBitmapImageReader(WIDTH, HEIGHT, Bitmap.Config.ARGB_8888, MAX_FRAMES);
 		reader.setOnImageAvailableListener(new ImageReader.OnImageAvailableListener<Bitmap>() {
+			final AtomicInteger cnt = new AtomicInteger();
 			@Override
 			public void onImageAvailable(@NonNull final ImageReader<Bitmap> reader) {
 				final Bitmap bitmap = reader.acquireLatestImage();
 				if (bitmap != null) {
 					try {
-						if (numFrames.incrementAndGet() == MAX_FRAMES) {
+						if (cnt.incrementAndGet() == MAX_FRAMES) {
 							result.set(Bitmap.createBitmap(bitmap));
 							sem.release();
 						}
@@ -202,26 +112,15 @@ public class ImageTextureSourceTest {
 		final GLImageReceiver receiver = new GLImageReceiver(WIDTH, HEIGHT, reader);
 		final Surface surface = receiver.getSurface();
 		assertNotNull(surface);
+		inputImagesAsync(original, surface, MAX_FRAMES);
 
-		// 映像ソース用にImageTextureSourceを生成
-		final ImageTextureSource source = new ImageTextureSource(manager, original, new Fraction(requestFps));
-		// 映像受け取り用Surfaceをセット
-		source.setSurface(surface);
 		try {
-			final long startTimeNs = System.nanoTime();
 			assertTrue(sem.tryAcquire(MAX_WAIT_MS, TimeUnit.MILLISECONDS));
-			final long endTimeNs = System.nanoTime();
-			final int n = numFrames.get();
-			final float fps = (n * 1000000000f) / (endTimeNs - startTimeNs);
-			Log.i(TAG, "numFrames=" + n);
-			Log.i(TAG, "fps=" + fps + "/" + requestFps);
 			final Bitmap b = result.get();
 //			dump(b);
 			assertNotNull(b);
 			// 元のビットマップと同じかどうかを検証
 			assertTrue(bitmapEquals(original, b));
-			// フレームレートが指定値の±10%以内にはいっているかどうか
-			assertTrue((fps > requestFps * 0.90f) && (fps < requestFps * 1.1f));
 		} catch (final InterruptedException e) {
 			fail();
 		}
