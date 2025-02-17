79e3d8a5b0efd5110687474a8a95640a7398aff9
saki4510t <saki4510t@gmail.com>
2025-02-18 04:20:25 +0900

    EffectPipelineTestへも映像ソースがGL_TEXTURE_EXTERNAL_OESテクスチャの場合のテストを追加、いくつかパスしない

--- app/src/androidTest/java/com/serenegiant/libcommon/glpipeline/EffectPipelineTest.java
+++ app/src/androidTest/java/com/serenegiant/libcommon/glpipeline/EffectPipelineTest.java
@@ -21,11 +21,14 @@ package com.serenegiant.libcommon.glpipeline;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.util.Log;
+import android.view.Surface;
 
 import com.serenegiant.gl.GLManager;
 import com.serenegiant.glpipeline.EffectPipeline;
 import com.serenegiant.glpipeline.GLPipeline;
+import com.serenegiant.glpipeline.GLPipelineSurfaceSource;
 import com.serenegiant.glpipeline.ImageSourcePipeline;
+import com.serenegiant.glpipeline.SurfaceSourcePipeline;
 import com.serenegiant.graphics.BitmapHelper;
 
 import org.junit.After;
@@ -35,18 +38,17 @@ import org.junit.runner.RunWith;
 
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
-import static com.serenegiant.libcommon.TestUtils.bitmapEquals;
-import static com.serenegiant.libcommon.TestUtils.createImageReceivePipeline;
-import static com.serenegiant.libcommon.TestUtils.validatePipelineOrder;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static com.serenegiant.libcommon.TestUtils.*;
+import static org.junit.Assert.*;
 
 @RunWith(AndroidJUnit4.class)
 public class EffectPipelineTest {
@@ -80,6 +82,7 @@ public class EffectPipelineTest {
 
 	/**
 	 * EffectPipelineが動作するかどうかを検証
+	 * 映像ソースがImageSourcePipelineなのでGL_TEXTURE_2D
 	 * (FIXME 個別の映像効果付与が想定通りかどうかは未検証)
 	 * Bitmap → ImageSourcePipeline
 	 * 			→ EffectPipeline
@@ -97,17 +100,17 @@ public class EffectPipelineTest {
 		// 映像ソースを生成
 		final ImageSourcePipeline source = new ImageSourcePipeline(manager, original, null);
 
-		final EffectPipeline effectPipeline = new EffectPipeline(manager);
+		final EffectPipeline pipeline1 = new EffectPipeline(manager);
 
 		final Semaphore sem = new Semaphore(0);
 		final AtomicReference<Bitmap> result = new AtomicReference<>();
 		final AtomicInteger cnt = new AtomicInteger();
 		final GLPipeline proxy = createImageReceivePipeline(WIDTH, HEIGHT, NUM_FRAMES, sem, result, cnt);
 
-		source.setPipeline(effectPipeline);
-		effectPipeline.setPipeline(proxy);
+		source.setPipeline(pipeline1);
+		pipeline1.setPipeline(proxy);
 
-		assertTrue(validatePipelineOrder(source, source, effectPipeline, proxy));
+		assertTrue(validatePipelineOrder(source, source, pipeline1, proxy));
 
 		try {
 			// 30fpsなので約1秒以内に抜けてくるはず(多少の遅延・タイムラグを考慮して少し長めに)
@@ -122,6 +125,7 @@ public class EffectPipelineTest {
 
 	/**
 	 * EffectPipelineを2つ連結処理したときに想定通りに動作になるかどうかを検証
+	 * 映像ソースがImageSourcePipelineなのでGL_TEXTURE_2D
 	 * (FIXME 個別の映像効果付与が想定通りかどうかは未検証)
 	 * Bitmap → ImageSourcePipeline
 	 * 			→ EffectPipeline
@@ -140,19 +144,19 @@ public class EffectPipelineTest {
 		// 映像ソースを生成
 		final ImageSourcePipeline source = new ImageSourcePipeline(manager, original, null);
 
-		final EffectPipeline effectPipeline1 = new EffectPipeline(manager);
-		final EffectPipeline effectPipeline2 = new EffectPipeline(manager);
+		final EffectPipeline pipeline1 = new EffectPipeline(manager);
+		final EffectPipeline pipeline2 = new EffectPipeline(manager);
 
 		final Semaphore sem = new Semaphore(0);
 		final AtomicReference<Bitmap> result = new AtomicReference<>();
 		final AtomicInteger cnt = new AtomicInteger();
 		final GLPipeline proxy = createImageReceivePipeline(WIDTH, HEIGHT, NUM_FRAMES, sem, result, cnt);
 
-		source.setPipeline(effectPipeline1);
-		effectPipeline1.setPipeline(effectPipeline2);
-		effectPipeline2.setPipeline(proxy);
+		source.setPipeline(pipeline1);
+		pipeline1.setPipeline(pipeline2);
+		pipeline2.setPipeline(proxy);
 
-		assertTrue(validatePipelineOrder(source, source, effectPipeline1, effectPipeline2, proxy));
+		assertTrue(validatePipelineOrder(source, source, pipeline1, pipeline2, proxy));
 
 		try {
 			// 30fpsなので約1秒以内に抜けてくるはず(多少の遅延・タイムラグを考慮して少し長めに)
@@ -169,6 +173,7 @@ public class EffectPipelineTest {
 
 	/**
 	 * EffectPipelineを3つ連結処理したときに想定通りに動作になるかどうかを検証
+	 * 映像ソースがImageSourcePipelineなのでGL_TEXTURE_2D
 	 * (FIXME 個別の映像効果付与が想定通りかどうかは未検証)
 	 * Bitmap → ImageSourcePipeline
 	 * 			→ EffectPipeline
@@ -188,21 +193,21 @@ public class EffectPipelineTest {
 		// 映像ソースを生成
 		final ImageSourcePipeline source = new ImageSourcePipeline(manager, original, null);
 
-		final EffectPipeline effectPipeline1 = new EffectPipeline(manager);
-		final EffectPipeline effectPipeline2 = new EffectPipeline(manager);
-		final EffectPipeline effectPipeline3 = new EffectPipeline(manager);
+		final EffectPipeline pipeline1 = new EffectPipeline(manager);
+		final EffectPipeline pipeline2 = new EffectPipeline(manager);
+		final EffectPipeline pipeline3 = new EffectPipeline(manager);
 
 		final Semaphore sem = new Semaphore(0);
 		final AtomicReference<Bitmap> result = new AtomicReference<>();
 		final AtomicInteger cnt = new AtomicInteger();
 		final GLPipeline proxy = createImageReceivePipeline(WIDTH, HEIGHT, NUM_FRAMES, sem, result, cnt);
 
-		source.setPipeline(effectPipeline1);
-		effectPipeline1.setPipeline(effectPipeline2);
-		effectPipeline2.setPipeline(effectPipeline3);
-		effectPipeline3.setPipeline(proxy);
+		source.setPipeline(pipeline1);
+		pipeline1.setPipeline(pipeline2);
+		pipeline2.setPipeline(pipeline3);
+		pipeline3.setPipeline(proxy);
 
-		assertTrue(validatePipelineOrder(source, source, effectPipeline1, effectPipeline2, effectPipeline3, proxy));
+		assertTrue(validatePipelineOrder(source, source, pipeline1, pipeline2, pipeline3, proxy));
 
 		try {
 			// 30fpsなので約1秒以内に抜けてくるはず(多少の遅延・タイムラグを考慮して少し長めに)
@@ -210,6 +215,228 @@ public class EffectPipelineTest {
 			assertEquals(NUM_FRAMES, cnt.get());
 			final Bitmap resultBitmap = result.get();
 //			dump(resultBitmap);
+			assertTrue(bitmapEquals(original, resultBitmap, true));
+		} catch (final InterruptedException e) {
+			Log.d(TAG, "interrupted", e);
+		}
+	}
+
+	/**
+	 * EffectPipelineが動作するかどうかを検証
+	 * SurfaceSourcePipelineからの映像ソースなのでGL_TEXTURE_EXTERNAL_OESテクスチャ
+	 * Bitmap → inputImagesAsync
+	 * 				↓
+	 * 				→ (Surface) → SurfaceSourcePipeline
+	 * 								 → EffectPipeline
+	 * 								 	→ ProxyPipeline → テクスチャ読み取り
+	 */
+	@Test
+	public void EffectPipelineOESTest1() {
+		// テストに使用するビットマップを生成
+		final Bitmap original = BitmapHelper.makeCheckBitmap(
+			WIDTH, HEIGHT, 15, 12, Bitmap.Config.ARGB_8888);
+//		dump(original);
+
+		final GLManager manager = mManager;
+
+		final Semaphore sem = new Semaphore(0);
+		final AtomicReference<Bitmap> result = new AtomicReference<>();
+		final AtomicInteger cnt = new AtomicInteger();
+		final GLPipeline proxy = createImageReceivePipeline(WIDTH, HEIGHT, NUM_FRAMES, sem, result, cnt);
+
+		// 映像ソースを生成
+		final SurfaceSourcePipeline source = new SurfaceSourcePipeline(
+			manager, WIDTH, HEIGHT,
+			new GLPipelineSurfaceSource.PipelineSourceCallback() {
+				@Override
+				public void onCreate(@NonNull final Surface surface) {
+					Log.v(TAG, "videoSourcePipelineTest#onCreate:" + surface);
+					sem.release();
+				}
+
+				@Override
+				public void onDestroy() {
+					Log.v(TAG, "videoSourcePipelineTest#onDestroy:");
+				}
+			});
+		try {
+			// Surfaceの生成を待機
+			assertTrue(sem.tryAcquire(1200, TimeUnit.MILLISECONDS));
+		} catch (InterruptedException e) {
+			fail();
+		}
+		final Surface inputSurface = source.getInputSurface();
+		assertNotNull(inputSurface);
+
+		// 検証するEffectPipelineを生成
+		final EffectPipeline pipeline1 = new EffectPipeline(manager);
+
+		GLPipeline.append(source, pipeline1);
+		GLPipeline.append(source, proxy);
+		assertTrue(validatePipelineOrder(source, source, pipeline1, proxy));
+
+		// 実際の映像はSurfaceを経由して映像を書き込む
+		final AtomicBoolean requestStop = new AtomicBoolean();
+		inputImagesAsync(original, inputSurface, NUM_FRAMES + 2, requestStop);
+
+		try {
+			assertTrue(sem.tryAcquire(NUM_FRAMES * 50L, TimeUnit.MILLISECONDS));
+			requestStop.set(true);
+			source.release();
+//			dump(result);
+			assertEquals(NUM_FRAMES, cnt.get());
+			final Bitmap resultBitmap = result.get();
+			assertNotNull(resultBitmap);
+			assertTrue(bitmapEquals(original, resultBitmap, true));
+		} catch (final InterruptedException e) {
+			Log.d(TAG, "interrupted", e);
+		}
+	}
+
+	/**
+	 * EffectPipelineが動作するかどうかを検証
+	 * SurfaceSourcePipelineからの映像ソースなのでGL_TEXTURE_EXTERNAL_OESテクスチャ
+	 * Bitmap → inputImagesAsync
+	 * 				↓
+	 * 				→ (Surface) → SurfaceSourcePipeline
+	 * 								 → EffectPipeline → EffectPipeline
+	 * 								 	→ ProxyPipeline → テクスチャ読み取り
+	 */
+	@Test
+	public void drawerPipelineOESTest2() {
+		// テストに使用するビットマップを生成
+		final Bitmap original = BitmapHelper.makeCheckBitmap(
+			WIDTH, HEIGHT, 15, 12, Bitmap.Config.ARGB_8888);
+//		dump(original);
+
+		final GLManager manager = mManager;
+
+		final Semaphore sem = new Semaphore(0);
+		final AtomicReference<Bitmap> result = new AtomicReference<>();
+		final AtomicInteger cnt = new AtomicInteger();
+		final GLPipeline proxy = createImageReceivePipeline(WIDTH, HEIGHT, NUM_FRAMES, sem, result, cnt);
+
+		// 映像ソースを生成
+		final SurfaceSourcePipeline source = new SurfaceSourcePipeline(
+			manager, WIDTH, HEIGHT,
+			new GLPipelineSurfaceSource.PipelineSourceCallback() {
+				@Override
+				public void onCreate(@NonNull final Surface surface) {
+					Log.v(TAG, "videoSourcePipelineTest#onCreate:" + surface);
+					sem.release();
+				}
+
+				@Override
+				public void onDestroy() {
+					Log.v(TAG, "videoSourcePipelineTest#onDestroy:");
+				}
+			});
+		try {
+			// Surfaceの生成を待機
+			assertTrue(sem.tryAcquire(1200, TimeUnit.MILLISECONDS));
+		} catch (InterruptedException e) {
+			fail();
+		}
+		final Surface inputSurface = source.getInputSurface();
+		assertNotNull(inputSurface);
+
+		// 検証するEffectPipelineを生成
+		final EffectPipeline pipeline1 = new EffectPipeline(manager);
+		final EffectPipeline pipeline2 = new EffectPipeline(manager);
+
+		GLPipeline.append(source, pipeline1);
+		GLPipeline.append(source, pipeline2);
+		GLPipeline.append(source, proxy);
+		assertTrue(validatePipelineOrder(source, source, pipeline1, pipeline2, proxy));
+
+		// 実際の映像はSurfaceを経由して映像を書き込む
+		final AtomicBoolean requestStop = new AtomicBoolean();
+		inputImagesAsync(original, inputSurface, NUM_FRAMES + 2, requestStop);
+
+		try {
+			assertTrue(sem.tryAcquire(NUM_FRAMES * 50L, TimeUnit.MILLISECONDS));
+			requestStop.set(true);
+			source.release();
+//			dump(result);
+			assertEquals(NUM_FRAMES, cnt.get());
+			final Bitmap resultBitmap = result.get();
+			assertNotNull(resultBitmap);
+			assertTrue(bitmapEquals(original, resultBitmap, true));
+		} catch (final InterruptedException e) {
+			Log.d(TAG, "interrupted", e);
+		}
+	}
+
+	/**
+	 * EffectPipelineが動作するかどうかを検証
+	 * SurfaceSourcePipelineからの映像ソースなのでGL_TEXTURE_EXTERNAL_OESテクスチャ
+	 * Bitmap → inputImagesAsync
+	 * 				↓
+	 * 				→ (Surface) → SurfaceSourcePipeline
+	 * 								 → EffectPipeline → EffectPipeline → EffectPipeline
+	 * 								 	→ ProxyPipeline → テクスチャ読み取り
+	 */
+	@Test
+	public void drawerPipelineOESTest3() {
+		// テストに使用するビットマップを生成
+		final Bitmap original = BitmapHelper.makeCheckBitmap(
+			WIDTH, HEIGHT, 15, 12, Bitmap.Config.ARGB_8888);
+//		dump(original);
+
+		final GLManager manager = mManager;
+
+		final Semaphore sem = new Semaphore(0);
+		final AtomicReference<Bitmap> result = new AtomicReference<>();
+		final AtomicInteger cnt = new AtomicInteger();
+		final GLPipeline proxy = createImageReceivePipeline(WIDTH, HEIGHT, NUM_FRAMES, sem, result, cnt);
+
+		// 映像ソースを生成
+		final SurfaceSourcePipeline source = new SurfaceSourcePipeline(
+			manager, WIDTH, HEIGHT,
+			new GLPipelineSurfaceSource.PipelineSourceCallback() {
+				@Override
+				public void onCreate(@NonNull final Surface surface) {
+					Log.v(TAG, "videoSourcePipelineTest#onCreate:" + surface);
+					sem.release();
+				}
+
+				@Override
+				public void onDestroy() {
+					Log.v(TAG, "videoSourcePipelineTest#onDestroy:");
+				}
+			});
+		try {
+			// Surfaceの生成を待機
+			assertTrue(sem.tryAcquire(1200, TimeUnit.MILLISECONDS));
+		} catch (InterruptedException e) {
+			fail();
+		}
+		final Surface inputSurface = source.getInputSurface();
+		assertNotNull(inputSurface);
+
+		// 検証するEffectPipelineを生成
+		final EffectPipeline pipeline1 = new EffectPipeline(manager);
+		final EffectPipeline pipeline2 = new EffectPipeline(manager);
+		final EffectPipeline pipeline3 = new EffectPipeline(manager);
+
+		GLPipeline.append(source, pipeline1);
+		GLPipeline.append(source, pipeline2);
+		GLPipeline.append(source, pipeline3);
+		GLPipeline.append(source, proxy);
+		assertTrue(validatePipelineOrder(source, source, pipeline1, pipeline2, pipeline3, proxy));
+
+		// 実際の映像はSurfaceを経由して映像を書き込む
+		final AtomicBoolean requestStop = new AtomicBoolean();
+		inputImagesAsync(original, inputSurface, NUM_FRAMES + 2, requestStop);
+
+		try {
+			assertTrue(sem.tryAcquire(NUM_FRAMES * 50L, TimeUnit.MILLISECONDS));
+			requestStop.set(true);
+			source.release();
+//			dump(result);
+			assertEquals(NUM_FRAMES, cnt.get());
+			final Bitmap resultBitmap = result.get();
+			assertNotNull(resultBitmap);
 			assertTrue(bitmapEquals(original, resultBitmap, true));
 		} catch (final InterruptedException e) {
 			Log.d(TAG, "interrupted", e);
