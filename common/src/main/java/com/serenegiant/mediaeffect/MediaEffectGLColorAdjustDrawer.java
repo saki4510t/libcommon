1c097d452c9a1c089b73a898676fd3d023c69dbc
saki4510t <saki4510t@gmail.com>
2025-06-02 16:36:10 +0900

    MediaEffectGLColorAdjustDrawerに4x4色変換行列の処理を追加

--- common/src/main/java/com/serenegiant/mediaeffect/MediaEffectGLColorAdjustDrawer.java
+++ common/src/main/java/com/serenegiant/mediaeffect/MediaEffectGLColorAdjustDrawer.java
@@ -19,42 +19,74 @@ package com.serenegiant.mediaeffect;
 */
 
 import android.opengl.GLES20;
+import android.opengl.Matrix;
+
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.Size;
 
 import static com.serenegiant.gl.ShaderConst.*;
 
 /**
+ * 色調整可能なMediaEffectGLDrawer.MediaEffectSingleDrawer実装
  * MediaEffectSingleDrawerを継承しているので、使用できるテクスチャは1つだけ
  */
 public class MediaEffectGLColorAdjustDrawer
 	extends MediaEffectGLDrawer.MediaEffectSingleDrawer {
 
-	private int muColorAdjustLoc;		// 色調整
-	private float mColorAdjust;
+	private final int muColorAdjustLoc;		// 色調整
+	private final int muColorMatrixLoc;		// 4x4色変換行列
+	private float mColorAdjust;				// 色調整オフセット
+	@Size(min=16)
+	@NonNull
+	protected final float[] mColorMatrix = new float[16];
 
 	public MediaEffectGLColorAdjustDrawer(final String fss) {
 		this(false, VERTEX_SHADER_ES2, fss);
 	}
 
-	public MediaEffectGLColorAdjustDrawer(final boolean isOES,
-										  final String fss) {
+	public MediaEffectGLColorAdjustDrawer(
+		final boolean isOES, final String fss) {
 
 		this(isOES, VERTEX_SHADER_ES2, fss);
 	}
 
-	public MediaEffectGLColorAdjustDrawer(final boolean isOES,
-										  final String vss, final String fss) {
+	public MediaEffectGLColorAdjustDrawer(
+		final boolean isOES, final String vss, final String fss) {
 
 		super(isOES, vss, fss);
-		muColorAdjustLoc = GLES20.glGetUniformLocation(getProgram(), "uColorAdjust");
-		if (muColorAdjustLoc < 0) {
-			muColorAdjustLoc = -1;
+		int uColorAdjust = GLES20.glGetUniformLocation(getProgram(), "uColorAdjust");
+		if (uColorAdjust < 0) {
+			uColorAdjust = -1;
 		}
+		muColorAdjustLoc = uColorAdjust;
+		int muColorMatrix = GLES20.glGetUniformLocation(getProgram(), "uColorMatrixLoc");
+		if (muColorMatrix < 0) {
+			muColorMatrix = -1;
+		}
+		muColorMatrixLoc = muColorMatrix;
+		Matrix.setIdentityM(mColorMatrix, 0);
 	}
 
 	public void setColorAdjust(final float adjust) {
 		synchronized (mSync) {
 			mColorAdjust = adjust;
+		}
+	}
+
+	/**
+	 * 色変換行列をセット
+	 * 指定したcolorMatrixがnullまたは要素数が16+offsetよりも小さい場合は単位行列になる
+	 * @param colorMatrix 色変換行列
+	 * @param offset
+	 */
+	public void setColorMatrix(@Nullable @Size(min=16) final float[] colorMatrix, final int offset) {
+		synchronized (mSync) {
+			if ((colorMatrix != null) && (colorMatrix.length >= 16 + offset)) {
+				System.arraycopy(colorMatrix, offset, mColorMatrix, 0, mColorMatrix.length);
+			} else {
+				Matrix.setIdentityM(mColorMatrix, 0);
+			}
 		}
 	}
 
@@ -68,6 +100,9 @@ public class MediaEffectGLColorAdjustDrawer
 			synchronized (mSync) {
 				GLES20.glUniform1f(muColorAdjustLoc, mColorAdjust);
 			}
+		}
+		if (muColorMatrixLoc >= 0) {
+			GLES20.glUniformMatrix4fv(muColorMatrixLoc, 1, false, mColorMatrix, 0);
 		}
 	}
 }
