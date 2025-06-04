eff212f18f792a5f663122e9b1347d9542a417e3
saki4510t <saki4510t@gmail.com>
2025-06-04 09:50:04 +0900

    MediaEffectGLCannynとGLEffectで同じCANNYエフェクトのフラグメントシェーダーを定義していたのをGLEffectだけで定義するように修正

--- common/src/main/java/com/serenegiant/mediaeffect/MediaEffectGLCanny.java
+++ common/src/main/java/com/serenegiant/mediaeffect/MediaEffectGLCanny.java
@@ -20,7 +20,7 @@ package com.serenegiant.mediaeffect;
 
 import android.util.Log;
 
-import static com.serenegiant.gl.ShaderConst.*;
+import static com.serenegiant.gl.GLEffect.FRAGMENT_SHADER_CANNY_ES2;
 
 /**
  * Cannyエッジ検出フィルタ
@@ -34,99 +34,8 @@ public class MediaEffectGLCanny extends MediaEffectGLBase {
 	private static final boolean DEBUG = false;
 	private static final String TAG = "MediaEffectGLCanny";
 
-	private static final String FRAGMENT_SHADER_BASE = SHADER_VERSION_ES2 +
-		"""
-		%s
-		#define KERNEL_SIZE3x3 %s
-		precision highp float;
-		varying       vec2 vTextureCoord;
-		uniform %s    sTexture;
-		uniform float uKernel[18];
-		uniform vec2  uTexOffset[KERNEL_SIZE3x3];
-		uniform float uColorAdjust;
-		const float lowerThreshold = 0.4;	// lowerとupperの値を入れ替えると白黒反転する
-		const float upperThreshold = 0.8;
-		void main() {
-			vec4 magdir = texture2D(sTexture, vTextureCoord);
-			vec2 offset = ((magdir.gb * 2.0) - 1.0) * uTexOffset[8];
-			float first = texture2D(sTexture, vTextureCoord + offset).r;
-			float second = texture2D(sTexture, vTextureCoord - offset).r;
-			float multiplier = step(first, magdir.r);
-			multiplier = multiplier * step(second, magdir.r);
-			float threshold = smoothstep(lowerThreshold, upperThreshold, magdir.r);
-			multiplier = multiplier * threshold;
-			gl_FragColor = vec4(multiplier, multiplier, multiplier, 1.0);
-		}
-		""";
-//		"void main() {\n" +
-//		"    vec4 magdir = texture2D(sTexture, vTextureCoord);\n" +
-//		"    float a = 0.5 / sin(3.14159 / 8.0); \n" +	// eight directions on grid
-//		"    vec2 alpha = vec2(a);\n" +
-//		"    vec2 offset = floor(alpha.xx * magdir.xy / magdir.zz);\n" +
-//		"    vec4 fwdneighbour, backneighbour;\n" +
-//		"    fwdneighbour = texture2D(sTexture, vTextureCoord + offset);\n" +
-//		"    backneighbour = texture2D(sTexture, vTextureCoord + offset);\n" +
-//		"    vec4 colorO;\n" +
-//		"    if (fwdneighbour.z > magdir.z || backneighbour.z > magdir.z)\n" +
-//		"        colorO = vec4(0.0, 0.0, 0.0, 0.0);\n" +	// not an edgel
-//		"    else\n" +
-//		"        colorO = vec4(1.0, 1.0, 1.0, 1.0);\n" +	// is an edgel
-//		"    if (magdir.z < uColorAdjust)\n" +
-//		"        colorO  = vec4(0.0, 0.0, 0.0, 0.0);\n" +	// thresholding
-//		"    gl_FragColor = colorO;\n" +
-//		"}\n";
-//----
-//		"const float threshold = 0.2;\n" +
-//		"const vec2 unshift = vec2(1.0 / 256.0, 1.0);\n" +
-//		"const float atan0   = 0.414213;\n" +
-//		"const float atan45  = 2.414213;\n" +
-//		"const float atan90  = -2.414213;\n" +
-//		"const float atan135 = -0.414213;\n" +
-//		"vec2 atanForCanny(float x) {\n" +
-//		"    if (x < atan0 && x > atan135) {\n" +
-//		"        return vec2(1.0, 0.0);\n" +
-//		"    }\n" +
-//		"    if (x < atan90 && x > atan45) {\n" +
-//		"        return vec2(0.0, 1.0);\n" +
-//		"    }\n" +
-//		"    if (x > atan135 && x < atan90) {\n" +
-//		"        return vec2(-1.0, 1.0);\n" +
-//		"    }\n" +
-//		"    return vec2(1.0, 1.0);\n" +
-//		"}\n" +
-//		"vec4 cannyEdge(vec2 coords) {\n" +
-//		"    vec4 color = texture2D(sTexture, coords);\n" +
-//		"    color.z = dot(color.zw, unshift);\n" +
-//		"    if (color.z > threshold) {\n" +
-//		"        color.x -= 0.5;\n" +
-//		"        color.y -= 0.5;\n" +
-//		"        vec2 offset = atanForCanny(color.y / color.x);\n" +
-//		"        offset.x *= uTexOffset[7];\n" +
-//		"        offset.y *= uTexOffset[8];\n" +
-//		"        vec4 forward  = texture2D(sTexture, coords + offset);\n" +
-//		"        vec4 backward = texture2D(sTexture, coords - offset);\n" +
-//		"        forward.z  = dot(forward.zw, unshift);\n" +
-//		"        backward.z = dot(backward.zw, unshift);\n" +
-//		"        if (forward.z >= color.z ||\n" +
-//		"            backward.z >= color.z) {\n" +
-//		"            return vec4(0.0, 0.0, 0.0, 1.0);\n" +
-//		"        } else {\n" +
-//		"            color.x += 0.5; color.y += 0.5;\n" +
-//		"            return vec4(1.0, color.x, color.y, 1.0);\n" +
-//		"        }\n" +
-//		"    }\n" +
-//		"    return vec4(0.0, 0.0, 0.0, 1.0);\n" +
-//		"}\n" +
-//		"void main() {\n" +
-//		"    gl_FragColor = cannyEdge(vTextureCoord);\n" +
-//		"}\n";
-	private static final String FRAGMENT_SHADER
-		= String.format(FRAGMENT_SHADER_BASE, HEADER_2D, KERNEL_SIZE3x3, SAMPLER_2D);
-	private static final String FRAGMENT_SHADER_EXT
-		= String.format(FRAGMENT_SHADER_BASE, HEADER_OES_ES2, KERNEL_SIZE3x3, SAMPLER_OES);
-
 	public MediaEffectGLCanny() {
-		super(new MediaEffectGLKernel3x3Drawer(false, FRAGMENT_SHADER));
+		super(new MediaEffectGLKernel3x3Drawer(false, FRAGMENT_SHADER_CANNY_ES2));
 		if (DEBUG) Log.v(TAG, "コンストラクタ:");
 	}
 
