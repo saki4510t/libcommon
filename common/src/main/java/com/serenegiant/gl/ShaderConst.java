c6f20144519ae61be55355c9c4c4d60e294729c1
saki4510t <saki4510t@gmail.com>
2025-06-05 11:33:09 +0900

    改行とインデントを修正

--- common/src/main/java/com/serenegiant/gl/ShaderConst.java
+++ common/src/main/java/com/serenegiant/gl/ShaderConst.java
@@ -132,27 +132,27 @@ public class ShaderConst implements GLConst {
 	 * RGBをHSVに変換
 	 * {R[0.0-1.0], G[0.0-1.0], B([0.0-1.0]} => {H[0.0-1.0], S[0.0-1.0], V[0.0-1.0]}
 	 */
-	public static final String FUNC_RGB2HSV
-		= """
+	public static final String FUNC_RGB2HSV =
+		"""
 		vec3 rgb2hsv(vec3 c) {
-		vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
-		vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
-		vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
-		float d = q.x - min(q.w, q.y);
-		float e = 1.0e-10;
-		return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
+			vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
+			vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+			vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
+			float d = q.x - min(q.w, q.y);
+			float e = 1.0e-10;
+			return vec3(abs(q.z + (q.w - q.y) / (6.0 * d + e)), d / (q.x + e), q.x);
 		}
 		""";
 	/**
 	 * HSVをRGBに変換
 	 * {H[0.0-1.0], S[0.0-1.0], V[0.0-1.0]} => {R[0.0-1.0], G[0.0-1.0], B([0.0-1.0]}
 	 */
-	public static final String FUNC_HSV2RGB
-		= """
+	public static final String FUNC_HSV2RGB =
+		"""
 		vec3 hsv2rgb(vec3 c) {
-		vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-		vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-		return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+			vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
+			vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
+			return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
 		}
 		""";
 
@@ -162,11 +162,11 @@ public class ShaderConst implements GLConst {
 	 * 変換係数との内積を計算するだけ
 	 * 係数は(0.2125, 0.7154, 0.0721)
 	 */
-	public static final String FUNC_GET_INTENSITY
-		= """
+	public static final String FUNC_GET_INTENSITY =
+		"""
 		const highp vec3 luminanceWeighting = vec3(0.2125, 0.7154, 0.0721);
 		highp float getIntensity(vec3 c) {
-		return dot(c.rgb, luminanceWeighting);
+			return dot(c.rgb, luminanceWeighting);
 		}
 		""";
 
