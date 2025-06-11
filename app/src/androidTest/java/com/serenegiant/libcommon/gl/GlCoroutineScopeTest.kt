b2ce02a944dcd3238df18d4c95a8ab7354a2b3fc
saki4510t <saki4510t@gmail.com>
2025-06-11 13:44:31 +0900

    EGL/GL|ESコンテキストを保持するシングルスレッド上で実行するためのCoroutineScope(GLCoroutineScope)を実装

---
+++ app/src/androidTest/java/com/serenegiant/libcommon/gl/GlCoroutineScopeTest.kt
@@ -0,0 +1,56 @@
+package com.serenegiant.libcommon.gl
+/*
+ * libcommon
+ * utility/helper classes for myself
+ *
+ * Copyright (c) 2014-2025 saki t_saki@serenegiant.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+*/
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.serenegiant.gl.GLInfo
+import com.serenegiant.gl.glCoroutineScope
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.runBlocking
+import org.json.JSONObject
+import org.junit.Assert
+import org.junit.Assert.fail
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * GLCoroutineScopeのテスト
+ */
+@RunWith(AndroidJUnit4::class)
+class GlCoroutineScopeTest {
+	@Test
+	fun glCoroutineScopeTest1() {
+		val scope = glCoroutineScope()
+		runBlocking(scope.coroutineContext) {
+			try {
+				val info = GLInfo.getOnGL(scope.egl)
+//				Log.i("GlCoroutineScopeTest", info.toString())
+				val glInfo = info.get("GL_INFO") as JSONObject?
+				Assert.assertNotNull(glInfo)
+				Assert.assertNotNull(glInfo!!.get("GL_VERSION"))
+				val eglInfo = info.get("EGL_INFO") as JSONObject?
+				Assert.assertNotNull(eglInfo)
+				Assert.assertNotNull(eglInfo!!.get("EGL_VERSION"))
+			} catch (e: Exception) {
+				fail(e.message)
+			}
+		}
+		scope.cancel()
+	}
+}
---
+++ common/src/main/java/com/serenegiant/gl/GLCoroutineScope.kt
@@ -0,0 +1,80 @@
+package com.serenegiant.gl
+/*
+ * libcommon
+ * utility/helper classes for myself
+ *
+ * Copyright (c) 2014-2025 saki t_saki@serenegiant.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+*/
+
+import android.util.Log
+import com.serenegiant.egl.EGLBase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.asCoroutineDispatcher
+import kotlinx.coroutines.awaitCancellation
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import java.util.concurrent.Executors
+import kotlin.coroutines.CoroutineContext
+
+private const val DEBUG = false
+private val TAG = GLCoroutineScope::class.java.simpleName
+
+abstract class GLCoroutineScope internal constructor(): CoroutineScope {
+	abstract val glContext: GLContext
+	val egl: EGLBase
+		get() = glContext.egl
+
+	val isGLES3: Boolean
+		get() = glContext.isGLES3
+
+	fun makeDefault() {
+		glContext.makeDefault()
+	}
+
+	fun swap() {
+		glContext.swap()
+	}
+}
+
+private class GLCoroutineScopeImpl(
+	override val glContext: GLContext,
+	override val coroutineContext: CoroutineContext
+) : GLCoroutineScope() {
+	init {
+		runBlocking(coroutineContext) {
+			if (DEBUG) Log.v(TAG, "initialize gl context")
+			glContext.initialize()
+		}
+		launch {
+			try {
+				awaitCancellation()
+			} finally {
+				if (DEBUG) Log.v(TAG, "release gl context")
+				glContext.release()
+			}
+		}
+	}
+}
+
+/**
+ * EGL/GL|ESコンテキストを保持するシングルスレッド上で実行するためのCoroutineScopeを取得する
+ */
+fun glCoroutineScope(): GLCoroutineScope {
+	val executor = Executors.newSingleThreadExecutor()
+	return GLCoroutineScopeImpl(
+		GLContext(),
+		SupervisorJob() + executor.asCoroutineDispatcher())
+}
