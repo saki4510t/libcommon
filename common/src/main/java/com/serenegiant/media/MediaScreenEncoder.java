6f450bee8cc19de89429d19445351f8e0ae099bf
saki4510t <saki4510t@gmail.com>
2025-06-12 12:09:24 +0900

    EglTaskの代わりにGLManagerを使うように変更…変更前も変更後も録画結果が真っ黒になる、いつからだろ？

--- common/src/main/java/com/serenegiant/media/MediaScreenEncoder.java
+++ common/src/main/java/com/serenegiant/media/MediaScreenEncoder.java
@@ -28,34 +28,67 @@ import android.media.projection.MediaProjection;
 import android.opengl.GLES20;
 import android.os.Build;
 import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.Surface;
 
 import com.serenegiant.egl.EGLBase;
-import com.serenegiant.egl.EglTask;
 import com.serenegiant.gl.GLDrawer2D;
+import com.serenegiant.gl.GLManager;
 import com.serenegiant.gl.GLUtils;
-import com.serenegiant.utils.HandlerThreadHandler;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
+import androidx.annotation.WorkerThread;
 
 /**
  * MediaProjectionからの映像をエンコードするためのAbstractVideoEncoder実装
  */
 @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
 public class MediaScreenEncoder extends AbstractVideoEncoder {
-	private static final boolean DEBUG = false;    // set false on production
+	private static final boolean DEBUG = true;    // set false on production
 	private static final String TAG = MediaScreenEncoder.class.getSimpleName();
 
 	// parameters for recording
 	private static final String MIME = MediaCodecUtils.MIME_VIDEO_AVC;
 
-	private MediaProjection mMediaProjection;
+	private static final int REQUEST_DRAW = 1;
+
+	@NonNull
+	private final GLManager mGLManager;
+	@NonNull
+	private final Handler mGLHandler;
+	@NonNull
+	private final MediaProjection mMediaProjection;
 	private final int mDensity;
+	/**
+	 * MediaCodecの映像エンコーダーへの映像入力用Surface
+	 */
+	@Nullable
 	private Surface mInputSurface;
-	private final Handler mHandler;
+	@Nullable
+	private VirtualDisplay display;
+	private int mTexId;
+	/**
+	 * VirtualDisplayからの映像受け取り用
+	 */
+	@Nullable
+	private SurfaceTexture mSourceTexture;
+	/**
+	 * VirtualDisplayからの映像受け取り用
+	 */
+	@Nullable
+	private Surface mSourceSurface;
+	/**
+	 * MediaCodecの映像エンコーダーへGL|ESで描画するためのIEglSurface実装
+	 */
+	@Nullable
+	private EGLBase.IEglSurface mEncoderSurface;
+	@Nullable
+	private GLDrawer2D mDrawer;
+	@NonNull
+	private final float[] mTexMatrix = new float[16];
 
 	/**
 	 * コンストラクタ
@@ -71,9 +104,15 @@ public class MediaScreenEncoder extends AbstractVideoEncoder {
 		final int density) {
 
 		super(MIME, recorder, listener);
+		mGLManager = new GLManager();
+		mGLHandler = mGLManager.createGLHandler(new Handler.Callback() {
+			@Override
+			public boolean handleMessage(@NonNull final Message msg) {
+				return onHandleMessage(msg);
+			}
+		});
 		mMediaProjection = projection;
 		mDensity = density;
-		mHandler = HandlerThreadHandler.createHandler(TAG);
 	}
 
 	@Override
@@ -110,7 +149,12 @@ public class MediaScreenEncoder extends AbstractVideoEncoder {
 		mInputSurface = mediaCodec.createInputSurface();    // API >= 18
 		mediaCodec.start();
 		final MediaReaper reaper = new MediaReaper.VideoReaper(mediaCodec, listener, mWidth, mHeight);
-		new Thread(mDrawTask, DrawTask.class.getSimpleName()).start();
+		mGLManager.runOnGLThread(new Runnable() {
+			@Override
+			public void run() {
+				startOnGL();
+			}
+		});
 		return new Encoder(mediaCodec, reaper, false);
 	}
 
@@ -120,8 +164,9 @@ public class MediaScreenEncoder extends AbstractVideoEncoder {
 	@Override
 	public void release() {
 		if (DEBUG) Log.d(TAG, "release:");
-		super.release();
 		mInputSurface = null;
+		mGLManager.release();
+		super.release();
 	}
 
 	@Override
@@ -133,152 +178,237 @@ public class MediaScreenEncoder extends AbstractVideoEncoder {
 	@Override
 	public void stop() {
 		if (DEBUG) Log.d(TAG, "stop:");
-		mDrawTask.release();
+		mGLManager.runOnGLThread(new Runnable() {
+			@Override
+			public void run() {
+				stopOnGL();
+			}
+		});
 		super.stop();
 	}
 
-	private volatile boolean requestDraw;
-	@NonNull
-	private final DrawTask mDrawTask = new DrawTask(null, 0);
-
-	/**
-	 * MediaProjectionのVirtualDisplayから受け取った映像を
-	 * MediaCodecの映像エンコーダーの入力Surfaceへ転送するためのEglTask実装
-	 */
-	private final class DrawTask extends EglTask {
-		private VirtualDisplay display;
-		private long intervals;
-		private int mTexId;
-		private SurfaceTexture mSourceTexture;
-		private Surface mSourceSurface;
-		private EGLBase.IEglSurface mEncoderSurface;
-		private GLDrawer2D mDrawer;
-		private final float[] mTexMatrix = new float[16];
-
-		public DrawTask(@Nullable final EGLBase.IContext<?> sharedContext, final int flags) {
-			super(sharedContext, flags);
+	@WorkerThread
+	private boolean onHandleMessage(@NonNull final Message msg) {
+		boolean result = true;
+		if (msg.what == REQUEST_DRAW) {
+			if (isReady()) {
+				mSourceTexture.updateTexImage();
+				mSourceTexture.getTransformMatrix(mTexMatrix);
+				// SurfaceTextureで受け取った画像をMediaCodecの入力用Surfaceへ描画する
+				mEncoderSurface.makeCurrent();
+				mDrawer.draw(GLES20.GL_TEXTURE0, mTexId, mTexMatrix, 0);
+				mEncoderSurface.swap();
+				// EGL保持用のオフスクリーンに描画しないとハングアップする機種の為のworkaround
+				mGLManager.makeDefault();
+				GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+				GLES20.glFlush();
+				frameAvailableSoon();
+			}
+		} else {
+			if (DEBUG) Log.v(TAG, "onHandleMessage:" + msg);
+			result = false;
 		}
-
-		@Override
-		protected void onStart() {
-			if (DEBUG) Log.d(TAG, String.format("DrawTask#onStart:(%dx%d)", mWidth, mHeight));
-			mDrawer = GLDrawer2D.create(isGLES3(), true);
-			mTexId = GLUtils.initTex(mDrawer.texTarget, GLES20.GL_TEXTURE0, GLES20.GL_NEAREST);
-			mSourceTexture = new SurfaceTexture(mTexId);
-			mSourceTexture.setDefaultBufferSize(mWidth, mHeight);    // これを入れないと映像が取れない
-			mSourceSurface = new Surface(mSourceTexture);
-			mSourceTexture.setOnFrameAvailableListener(mOnFrameAvailableListener, mHandler);
-			mEncoderSurface = getEgl().createFromSurface(mInputSurface);
-
-			if (DEBUG) Log.d(TAG, "DrawTask#onStart:setup VirtualDisplay");
-			intervals = (long) (1000f / mFramerate);
-			display = mMediaProjection.createVirtualDisplay(
-				"Capturing Display",
-				mWidth, mHeight, mDensity,
-				DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
-				mSourceSurface, mCallback, mHandler);
-			if (DEBUG) Log.v(TAG, "DrawTask#onStart:screen capture loop:display=" + display);
-			// 録画タスクを起床
-			queueEvent(mDrawTask);
-		}
-
-		@Override
-		protected void onStop() {
-			if (DEBUG) Log.v(TAG, "DrawTask#onStop:");
-			if (mDrawer != null) {
-				mDrawer.release();
-				mDrawer = null;
-			}
-			if (mSourceSurface != null) {
-				mSourceSurface.release();
-				mSourceSurface = null;
-			}
-			if (mSourceTexture != null) {
-				mSourceTexture.release();
-				mSourceTexture = null;
-			}
-			if (mEncoderSurface != null) {
-				mEncoderSurface.release();
-				mEncoderSurface = null;
-			}
-			makeCurrent();
-			if (display != null) {
-				if (DEBUG) Log.v(TAG, "DrawTask#onStop:release VirtualDisplay");
-				display.release();
-			}
-			if (DEBUG) Log.v(TAG, "DrawTask#onStop:tear down MediaProjection");
-			if (mMediaProjection != null) {
-				mMediaProjection.stop();
-				mMediaProjection = null;
-			}
-		}
-
-		@Override
-		protected boolean onError(final Throwable e) {
-			if (DEBUG) Log.w(TAG, "DrawTask.onError:", e);
-			return false;
-		}
-
-		@Override
-		protected Object processRequest(final int request, final int arg1, final int arg2, final Object obj) {
-			return null;
-		}
-
-		// TextureSurfaceで映像を受け取った際のコールバックリスナー
-		private final SurfaceTexture.OnFrameAvailableListener mOnFrameAvailableListener
-			= new SurfaceTexture.OnFrameAvailableListener() {
-			@Override
-			public void onFrameAvailable(final SurfaceTexture surfaceTexture) {
-//				if (DEBUG) Log.v(TAG, "DrawTask#onFrameAvailable:mIsRecording=" + mIsRecording);
-				if (isEncoding()) {
-					synchronized (mSync) {
-						requestDraw = true;
-						mSync.notify();
-					}
-				}
-			}
-		};
-
-		private final Runnable mDrawTask = new Runnable() {
-			@Override
-			public void run() {
-//				if (DEBUG) Log.v(TAG, "DrawTask#draw:");
-				boolean localRequestDraw;
-				synchronized (mSync) {
-					localRequestDraw = requestDraw;
-					if (!localRequestDraw) {
-						try {
-							mSync.wait(intervals);
-							localRequestDraw = requestDraw;
-							requestDraw = false;
-						} catch (final InterruptedException e) {
-							return;
-						}
-					}
-				}
-				if (isReady()) {
-					if (localRequestDraw) {
-						mSourceTexture.updateTexImage();
-						mSourceTexture.getTransformMatrix(mTexMatrix);
-					}
-					// SurfaceTextureで受け取った画像をMediaCodecの入力用Surfaceへ描画する
-					mEncoderSurface.makeCurrent();
-					mDrawer.draw(GLES20.GL_TEXTURE0, mTexId, mTexMatrix, 0);
-					mEncoderSurface.swap();
-					// EGL保持用のオフスクリーンに描画しないとハングアップする機種の為のworkaround
-					makeCurrent();
-					GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
-					GLES20.glFlush();
-					frameAvailableSoon();
-					queueEvent(this);
-				} else {
-					releaseSelf();
-				}
-//				if (DEBUG) Log.v(TAG, "DrawTask#draw:finished");
-			}
-		};
-
+		return result;
 	}
+
+	@WorkerThread
+	private void startOnGL() {
+		if (DEBUG) Log.d(TAG, String.format("startOnGL:(%dx%d)", mWidth, mHeight));
+		mDrawer = GLDrawer2D.create(mGLManager.isGLES3(), true);
+		mTexId = GLUtils.initTex(mDrawer.texTarget, GLES20.GL_TEXTURE0, GLES20.GL_NEAREST);
+		mSourceTexture = new SurfaceTexture(mTexId);
+		mSourceTexture.setDefaultBufferSize(mWidth, mHeight);    // これを入れないと映像が取れない
+		mSourceSurface = new Surface(mSourceTexture);
+		mSourceTexture.setOnFrameAvailableListener(mOnFrameAvailableListener, mGLHandler);
+		mEncoderSurface = mGLManager.getEgl().createFromSurface(mInputSurface);
+
+		if (DEBUG) Log.d(TAG, "startOnGL:setup VirtualDisplay");
+		display = mMediaProjection.createVirtualDisplay(
+			"Capturing Display",
+			mWidth, mHeight, mDensity,
+			DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+			mSourceSurface, mCallback, mGLHandler);
+		if (DEBUG) Log.v(TAG, "startOnGL:screen capture loop:display=" + display);
+	}
+
+	@WorkerThread
+	private void stopOnGL() {
+		if (DEBUG) Log.v(TAG, "stopOnGL:");
+		if (mDrawer != null) {
+			mDrawer.release();
+			mDrawer = null;
+		}
+		if (mSourceSurface != null) {
+			mSourceSurface.release();
+			mSourceSurface = null;
+		}
+		if (mSourceTexture != null) {
+			mSourceTexture.release();
+			mSourceTexture = null;
+		}
+		if (mEncoderSurface != null) {
+			mEncoderSurface.release();
+			mEncoderSurface = null;
+		}
+		mGLManager.makeDefault();
+		if (display != null) {
+			if (DEBUG) Log.v(TAG, "stopOnGL:release VirtualDisplay");
+			display.release();
+		}
+		if (DEBUG) Log.v(TAG, "stopOnGL:tear down MediaProjection");
+		mMediaProjection.stop();
+	}
+
+	// TextureSurfaceで映像を受け取った際のコールバックリスナー
+	private final SurfaceTexture.OnFrameAvailableListener mOnFrameAvailableListener
+		= new SurfaceTexture.OnFrameAvailableListener() {
+		@Override
+		public void onFrameAvailable(final SurfaceTexture surfaceTexture) {
+			if (DEBUG) Log.v(TAG, "onFrameAvailable:isEncoding=" + isEncoding());
+			if (isEncoding()) {
+				mGLHandler.sendEmptyMessage(REQUEST_DRAW);
+			}
+		}
+	};
+
+//	/**
+//	 * MediaProjectionのVirtualDisplayから受け取った映像を
+//	 * MediaCodecの映像エンコーダーの入力Surfaceへ転送するためのEglTask実装
+//	 */
+//	private final class DrawTask extends EglTask {
+//		private VirtualDisplay display;
+//		private long intervals;
+//		private int mTexId;
+//		private SurfaceTexture mSourceTexture;
+//		private Surface mSourceSurface;
+//		private EGLBase.IEglSurface mEncoderSurface;
+//		private GLDrawer2D mDrawer;
+//		private final float[] mTexMatrix = new float[16];
+//
+//		public DrawTask(@Nullable final EGLBase.IContext<?> sharedContext, final int flags) {
+//			super(sharedContext, flags);
+//		}
+//
+//		@Override
+//		protected void onStart() {
+//			if (DEBUG) Log.d(TAG, String.format("DrawTask#onStart:(%dx%d)", mWidth, mHeight));
+//			mDrawer = GLDrawer2D.create(isGLES3(), true);
+//			mTexId = GLUtils.initTex(mDrawer.texTarget, GLES20.GL_TEXTURE0, GLES20.GL_NEAREST);
+//			mSourceTexture = new SurfaceTexture(mTexId);
+//			mSourceTexture.setDefaultBufferSize(mWidth, mHeight);    // これを入れないと映像が取れない
+//			mSourceSurface = new Surface(mSourceTexture);
+//			mSourceTexture.setOnFrameAvailableListener(mOnFrameAvailableListener, mHandler);
+//			mEncoderSurface = getEgl().createFromSurface(mInputSurface);
+//
+//			if (DEBUG) Log.d(TAG, "DrawTask#onStart:setup VirtualDisplay");
+//			intervals = (long) (1000f / mFramerate);
+//			display = mMediaProjection.createVirtualDisplay(
+//				"Capturing Display",
+//				mWidth, mHeight, mDensity,
+//				DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
+//				mSourceSurface, mCallback, mHandler);
+//			if (DEBUG) Log.v(TAG, "DrawTask#onStart:screen capture loop:display=" + display);
+//			// 録画タスクを起床
+//			queueEvent(mDrawTask);
+//		}
+//
+//		@Override
+//		protected void onStop() {
+//			if (DEBUG) Log.v(TAG, "DrawTask#onStop:");
+//			if (mDrawer != null) {
+//				mDrawer.release();
+//				mDrawer = null;
+//			}
+//			if (mSourceSurface != null) {
+//				mSourceSurface.release();
+//				mSourceSurface = null;
+//			}
+//			if (mSourceTexture != null) {
+//				mSourceTexture.release();
+//				mSourceTexture = null;
+//			}
+//			if (mEncoderSurface != null) {
+//				mEncoderSurface.release();
+//				mEncoderSurface = null;
+//			}
+//			makeCurrent();
+//			if (display != null) {
+//				if (DEBUG) Log.v(TAG, "DrawTask#onStop:release VirtualDisplay");
+//				display.release();
+//			}
+//			if (DEBUG) Log.v(TAG, "DrawTask#onStop:tear down MediaProjection");
+//			if (mMediaProjection != null) {
+//				mMediaProjection.stop();
+//				mMediaProjection = null;
+//			}
+//		}
+//
+//		@Override
+//		protected boolean onError(final Throwable e) {
+//			if (DEBUG) Log.w(TAG, "DrawTask.onError:", e);
+//			return false;
+//		}
+//
+//		@Override
+//		protected Object processRequest(final int request, final int arg1, final int arg2, final Object obj) {
+//			return null;
+//		}
+//
+//		// TextureSurfaceで映像を受け取った際のコールバックリスナー
+//		private final SurfaceTexture.OnFrameAvailableListener mOnFrameAvailableListener
+//			= new SurfaceTexture.OnFrameAvailableListener() {
+//			@Override
+//			public void onFrameAvailable(final SurfaceTexture surfaceTexture) {
+////				if (DEBUG) Log.v(TAG, "DrawTask#onFrameAvailable:mIsRecording=" + mIsRecording);
+//				if (isEncoding()) {
+//					synchronized (mSync) {
+//						requestDraw = true;
+//						mSync.notify();
+//					}
+//				}
+//			}
+//		};
+//
+//		private final Runnable mDrawTask = new Runnable() {
+//			@Override
+//			public void run() {
+////				if (DEBUG) Log.v(TAG, "DrawTask#draw:");
+//				boolean localRequestDraw;
+//				synchronized (mSync) {
+//					localRequestDraw = requestDraw;
+//					if (!localRequestDraw) {
+//						try {
+//							mSync.wait(intervals);
+//							localRequestDraw = requestDraw;
+//							requestDraw = false;
+//						} catch (final InterruptedException e) {
+//							return;
+//						}
+//					}
+//				}
+//				if (isReady()) {
+//					if (localRequestDraw) {
+//						mSourceTexture.updateTexImage();
+//						mSourceTexture.getTransformMatrix(mTexMatrix);
+//					}
+//					// SurfaceTextureで受け取った画像をMediaCodecの入力用Surfaceへ描画する
+//					mEncoderSurface.makeCurrent();
+//					mDrawer.draw(GLES20.GL_TEXTURE0, mTexId, mTexMatrix, 0);
+//					mEncoderSurface.swap();
+//					// EGL保持用のオフスクリーンに描画しないとハングアップする機種の為のworkaround
+//					makeCurrent();
+//					GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT);
+//					GLES20.glFlush();
+//					frameAvailableSoon();
+//					queueEvent(this);
+//				} else {
+//					releaseSelf();
+//				}
+////				if (DEBUG) Log.v(TAG, "DrawTask#draw:finished");
+//			}
+//		};
+//
+//	}
 
 	/**
 	 * MediaProjection#createVirtualDisplayへ引き渡すVirtualDisplay.Callbackコールバックオブジェクト
