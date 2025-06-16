89704ccd514127d068c68113000aa1efefa5e94f
saki4510t <saki4510t@gmail.com>
2025-06-16 11:43:13 +0900

    定数を関数外へ移動

--- common/src/main/java/com/serenegiant/gl/ShaderConst.java
+++ common/src/main/java/com/serenegiant/gl/ShaderConst.java
@@ -136,9 +136,9 @@ public class ShaderConst implements GLConst {
 	 */
 	public static final String FUNC_RGB2HSV =
 		"""
+		const vec4 KRGB = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
 		vec3 rgb2hsv(vec3 c) {
-			vec4 K = vec4(0.0, -1.0 / 3.0, 2.0 / 3.0, -1.0);
-			vec4 p = mix(vec4(c.bg, K.wz), vec4(c.gb, K.xy), step(c.b, c.g));
+			vec4 p = mix(vec4(c.bg, KRGB.wz), vec4(c.gb, KRGB.xy), step(c.b, c.g));
 			vec4 q = mix(vec4(p.xyw, c.r), vec4(c.r, p.yzx), step(p.x, c.r));
 			float d = q.x - min(q.w, q.y);
 			float e = 1.0e-10;
@@ -151,10 +151,10 @@ public class ShaderConst implements GLConst {
 	 */
 	public static final String FUNC_HSV2RGB =
 		"""
+		const vec4 KHSV = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
 		vec3 hsv2rgb(vec3 c) {
-			vec4 K = vec4(1.0, 2.0 / 3.0, 1.0 / 3.0, 3.0);
-			vec3 p = abs(fract(c.xxx + K.xyz) * 6.0 - K.www);
-			return c.z * mix(K.xxx, clamp(p - K.xxx, 0.0, 1.0), c.y);
+			vec3 p = abs(fract(c.xxx + KHSV.xyz) * 6.0 - KHSV.www);
+			return c.z * mix(KHSV.xxx, clamp(p - KHSV.xxx, 0.0, 1.0), c.y);
 		}
 		""";
 
